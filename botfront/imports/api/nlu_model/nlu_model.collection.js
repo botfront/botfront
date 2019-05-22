e6209e8ba0f5ba073150947b2d76c1be89df7b7a
Matthieu Joannon <mattjoannon@gmail.com>
2019-05-22 16:55:38 -0400

    feat(roles): made nlu_models pub secure

--- botfront/imports/api/nlu_model/nlu_model.collection.js
+++ botfront/imports/api/nlu_model/nlu_model.collection.js
@@ -1,9 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Mongo } from 'meteor/mongo';
+
 import { can, getScopesForUser } from '../../lib/scopes';
 import { Projects } from '../project/project.collection';
 import { NLUModelSchema } from './nlu_model.schema';
+import { getProjectIdFromModelId } from '../../lib/utils';
 
 export const NLUModels = new Mongo.Collection('nlu_models');
 
@@ -23,16 +25,33 @@ Meteor.startup(() => {
 if (Meteor.isServer) {
     Meteor.publish('nlu_models', function (modelId) {
         check(modelId, String);
-        const isAllowed = () => {
-            if (can('global-admin')) return true;
-            const projectIds = getScopesForUser(this.userId, ['nlu-data:r', 'nlu-meta:r', 'nlu-model:x']);
-            const models = Projects.find({ _id: { $in: projectIds } }, { fields: { nlu_models: 1 } }).fetch();
-            const modelIdArrays = models.map(m => m.nlu_models);
-            const modelIds = [].concat(...modelIdArrays);
-            return modelIds.includes(modelId);
+        if (can('global-admin')) return NLUModels.find({ _id: modelId });
+
+        const projectId = getProjectIdFromModelId(modelId);
+
+        const baseModelInfos = {
+            name: 1, language: 1, instance: 1, published: 1, training_data: 1,
         };
+
+        if (can(['nlu-viewer'], projectId)) return NLUModels.find({ _id: modelId });
         
-        return isAllowed() ? NLUModels.find({ _id: modelId }) : this.ready();
+        if (can(['nlu-data:r'], projectId)) {
+            return NLUModels.find({ _id: modelId }, {
+                fields: {
+                    ...baseModelInfos, training_data: 1, intents: 1, chitchat_intents: 1,
+                },
+            });
+        }
+
+        if (can(['nlu-meta:r'], projectId)) {
+            return NLUModels.find({ _id: modelId }, {
+                fields: {
+                    ...baseModelInfos, config: 1,
+                },
+            });
+        }
+
+        throw new Meteor.Error('401', 'Not authorized');
     });
 
     // his publication is here to get a lTist of accessible models
