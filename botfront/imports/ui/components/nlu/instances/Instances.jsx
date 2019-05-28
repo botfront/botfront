0b918b24cd400a0d67d725e4ead48690fe6805f5
Palashx <jainpalash94@gmail.com>
2019-05-28 17:45:51 -0400

    fix(delete instance): removed the delete instance bug

--- botfront/imports/ui/components/nlu/instances/Instances.jsx
+++ botfront/imports/ui/components/nlu/instances/Instances.jsx
@@ -12,7 +12,6 @@ import Alert from 'react-s-alert';
 
 import { Instances as InstancesCollection } from '../../../../api/instances/instances.collection';
 import SelectField from '../../form_fields/SelectField';
-import { can } from '../../../../lib/scopes';
 
 const NONE = -2;
 const NEW_INSTANCE = -1;
@@ -23,13 +22,14 @@ class Instances extends React.Component {
         this.state = {
             confirmOpen: false,
             editing: NONE,
+            deleting: NONE,
         };
     }
 
     createOrUpdateInstance = (instance) => {
         const method = instance._id ? 'instance.update' : 'instance.insert';
         Meteor.call(method, instance, (err) => {
-            this.setState({ editing: NONE, confirmOpen: false });
+            this.setState({ editing: NONE, deleting: NONE, confirmOpen: false });
             if (err) Alert.error(`Error: ${err.reason}`, { position: 'top-right', timeout: 'none' });
         });
     };
@@ -37,26 +37,16 @@ class Instances extends React.Component {
     deleteInstance = (instance) => {
         const { projectId } = this.props;
         Meteor.call('instance.remove', instance._id, projectId, (err) => {
-            this.setState({ editing: NONE, confirmOpen: false });
+            this.setState({ editing: NONE, deleting: NONE, confirmOpen: false });
             if (err) Alert.error(`Error: ${err.reason}`, { position: 'top-right', timeout: 'none' });
         });
     };
 
     getType = instance => (instance.type ? instance.type.map(type => <Label content={type} />) : <></>);
-
-    renderReady = (instances, editing) => instances.map((instance, index) => (editing === index ? this.renderAddOrEditInstance(instance, index) : this.renderInstance(instance, index)));
     
     renderInstance = (instance, index) => {
-        const { confirmOpen } = this.state;
         return (
             <Card key={index}>
-                <Confirm
-                    open={confirmOpen}
-                    header={`Delete instance ${instance.name}?`}
-                    content='This cannot be undone!'
-                    onCancel={() => this.setState({ editing: NONE, confirmOpen: false })}
-                    onConfirm={() => this.deleteInstance(instance)}
-                />
                 <Card.Content>
                     <Card.Header>{instance.name}</Card.Header>
                     <Card.Meta>{this.getType(instance)}</Card.Meta>
@@ -64,8 +54,8 @@ class Instances extends React.Component {
                 </Card.Content>
                 <Card.Content extra>
                     <Button.Group basic floated='right'>
-                        <Button primary onClick={() => this.setState({ editing: index })} icon='edit' data-cy='edit-instance' />
-                        <Button color='red' onClick={() => this.setState({ confirmOpen: true })} icon='trash' />
+                        <Button primary onClick={() => this.setState({ editing: index, deleting: NONE })} icon='edit' data-cy='edit-instance' />
+                        <Button color='red' onClick={() => this.setState({ editing: NONE, deleting: index, confirmOpen: true })} icon='trash' />
                     </Button.Group>
                 </Card.Content>
             </Card>
@@ -77,12 +67,56 @@ class Instances extends React.Component {
         callback();
     };
 
-    renderAddOrEditInstance(instance) {
+    renderEditOrDeleteInstance(instance) {
+        const { projectId } = this.props;
+        const { confirmOpen, editing } = this.state;
+        if (editing !== NONE && editing !== NEW_INSTANCE) {
+            return (
+                <Card raised key={instance._id}>
+                    <Card.Content extra>
+                        <Label attached='top right' as='a' icon='close' onClick={() => this.setState({ editing: NONE })} />
+                        <AutoForm
+                            schema={InstancesCollection.simpleSchema()}
+                            model={instance}
+                            onSubmit={i => this.createOrUpdateInstance(i)}
+                            onValidate={this.onValidate}
+                        >
+                            <HiddenField name='projectId' value={projectId} />
+                            <AutoField name='name' label='Instance name' />
+                            <SelectField name='type' label='Type' data-cy='type-selector' />
+                            <AutoField name='host' />
+                            <AutoField name='token' label='Token' />
+
+                            <ErrorsField />
+                            <SubmitField value='Save instance' className='primary' data-cy='save-instance' />
+                        </AutoForm>
+                    </Card.Content>
+                </Card>
+            );
+        }
+        return (
+            <Confirm
+                open={confirmOpen}
+                header={`Delete instance ${instance.name}?`}
+                content='This cannot be undone!'
+                onCancel={() => this.setState({ editing: NONE, deleting: NONE, confirmOpen: false })}
+                onConfirm={() => this.deleteInstance(instance)}
+            />
+        );
+    }
+
+    renderLoading = () => <div />;
+
+    renderInstances(deleting, editing, instances) {
+        return instances.map((instance, index) => (editing === index || deleting === index ? this.renderEditOrDeleteInstance(instance, index) : this.renderInstance(instance, index)));
+    }
+
+    renderAddInstance = (instance) => {
         const { projectId } = this.props;
         return (
             <Card raised key={instance._id}>
                 <Card.Content extra>
-                    <Label attached='top right' as='a' icon='close' onClick={() => this.setState({ editing: NONE })} />
+                    <Label attached='top right' as='a' icon='close' onClick={() => this.setState({ editing: NONE, deleting: NONE })} />
                     <AutoForm
                         schema={InstancesCollection.simpleSchema()}
                         model={instance}
@@ -103,22 +137,16 @@ class Instances extends React.Component {
         );
     }
 
-    renderLoading = () => <div />;
-
-    renderInstances(editing, instances) {
-        return instances.map((instance, index) => (editing === index ? this.renderAddOrEditInstance(instance, index) : this.renderInstance(instance, index)));
-    }
-
-    renderReady = (editing, instances) => (
+    renderReady = (deleting, editing, instances) => (
         <Card.Group>
-            {this.renderInstances(editing, instances)}
-            {editing === NEW_INSTANCE && this.renderAddOrEditInstance({})}
+            {this.renderInstances(deleting, editing, instances)}
+            {editing === NEW_INSTANCE && this.renderAddInstance({})}
 
             {editing === NONE && (
                 <Card key='new-instance'>
                     <Card.Content />
                     <Card.Content extra>
-                        <Button fluid onClick={() => this.setState({ editing: NEW_INSTANCE })} primary disabled={editing !== NONE} icon='add' content='New instance' labelPosition='left' />
+                        <Button fluid onClick={() => this.setState({ editing: NEW_INSTANCE, deleting: NONE })} primary disabled={editing !== NONE} icon='add' content='New instance' labelPosition='left' />
                     </Card.Content>
                 </Card>
             )}
@@ -127,10 +155,10 @@ class Instances extends React.Component {
 
     render() {
         const { instances, ready } = this.props;
-        const { editing } = this.state;
+        const { editing, deleting } = this.state;
         return (
             <>
-                {ready && this.renderReady(editing, instances)}
+                {ready && this.renderReady(deleting, editing, instances)}
                 {!ready && this.renderLoading()}
             </>
         );
