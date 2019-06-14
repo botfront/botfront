c8df5309848e5884a3d6116019d8d633cd3feda6
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-06-14 14:52:26 -0400

    fix: wrong message in Project info

--- botfront/imports/ui/components/settings/ProjectInfo.jsx
+++ botfront/imports/ui/components/settings/ProjectInfo.jsx
@@ -1,11 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/label-has-for */
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
-import {
-    AutoForm, SubmitField, ErrorsField,
-} from 'uniforms-semantic';
+import { AutoForm, SubmitField, ErrorsField } from 'uniforms-semantic';
 import { Dropdown, Form, Message } from 'semantic-ui-react';
 import { ProjectsSchema as projectsSchemaDefault } from '../../../api/project/project.schema.default';
 import { Projects } from '../../../api/project/project.collection';
@@ -19,7 +19,7 @@ class ProjectInfo extends React.Component {
     constructor(props) {
         super(props);
         const { modelLanguages } = this.props;
-        this.state = { saving: false, value: modelLanguages.map(lang => (lang.value)) };
+        this.state = { saving: false, value: modelLanguages.map(lang => lang.value) };
     }
 
     getOptions = () => {
@@ -31,7 +31,7 @@ class ProjectInfo extends React.Component {
         return renderOptions;
     };
 
-    diffArray = (array1, array2) => (array1.filter(elementArray1 => (array2.indexOf(elementArray1)) < 0));
+    diffArray = (array1, array2) => array1.filter(elementArray1 => array2.indexOf(elementArray1) < 0);
 
     renderLabel = (language, languageCodes) => {
         const isModelExist = languageCodes.includes(language.value);
@@ -46,41 +46,40 @@ class ProjectInfo extends React.Component {
 
     onChange = (e, { value }) => {
         this.setState({ saving: false, value });
-    }
+    };
 
     createNLUModels = (languageArray, projectId) => {
-        const nluInsertArray = languageArray.map(language => (
-            Meteor.callWithPromise('nlu.insert', {
+        const nluInsertArray = languageArray.map(language => Meteor.callWithPromise(
+            'nlu.insert',
+            {
                 name: 'Default Model',
                 language,
                 description: 'Default description',
             },
-            projectId)
+            projectId,
         ));
         Promise.all(nluInsertArray).then(() => {
             this.setState({ saving: false });
         });
-    }
+    };
 
     onSave = (project, modelLanguages) => {
         const { value } = this.state;
         const { name, _id, defaultLanguage } = project;
-        const differenceArray = this.diffArray(value, modelLanguages.map(lang => (lang.value)));
+        const differenceArray = this.diffArray(value, modelLanguages.map(lang => lang.value));
         this.setState({ saving: true });
-        Meteor.call('project.update', { name, _id, defaultLanguage },
-            wrapMeteorCallback(() => this.createNLUModels(differenceArray, _id), 'Changes saved'));
+        Meteor.call('project.update', { name, _id, defaultLanguage }, wrapMeteorCallback(() => this.createNLUModels(differenceArray, _id), 'Changes saved'));
     };
 
-    renderDeleteModelMessage = () => (
+    renderDeleteModelLanguages = () => (
         <Message
+            size='tiny'
             info
-            icon='info'
-            header='Deleting a model'
-            content={(
-                <div>
-                    To remove a language from the project, go to <strong> NLU Model &gt;  Settings &gt;  Delete </strong>.
-                </div>
-            )}
+            content={
+                <>
+                    To remove a language from the project, go to <strong> NLU &gt; Settings &gt; Delete </strong>.
+                </>
+            }
         />
     );
 
@@ -90,18 +89,13 @@ class ProjectInfo extends React.Component {
         const projectsSchema = Projects.simpleSchema();
         return (
             <>
-                {!!modelLanguages.length && this.renderDeleteModelMessage() }
                 {ready && (
                     <AutoForm schema={projectsSchema || projectsSchemaDefault} model={project} onSubmit={updateProject => this.onSave(updateProject, modelLanguages)} disabled={saving}>
                         <InfoField name='name' label='Name' className='project-name' />
-                        {projectsSchema && projectsSchema.allowsKey('namespace') && (
-                            <InfoField name='namespace' label='Namespace' disabled />
-                        )}
-                        {projectsSchema && projectsSchema.allowsKey('apiKey') && (
-                            <InfoField name='apiKey' label='Botfront API key' disabled />
-                        )}
+                        {projectsSchema && projectsSchema.allowsKey('namespace') && <InfoField name='namespace' label='Namespace' disabled />}
+                        {projectsSchema && projectsSchema.allowsKey('apiKey') && <InfoField name='apiKey' label='Botfront API key' disabled />}
                         <Form.Field>
-                            <label> Select Languages </label>
+                            <label>Languages supported</label>
                             <Dropdown
                                 label='Select Languages'
                                 name='lang'
@@ -112,11 +106,12 @@ class ProjectInfo extends React.Component {
                                 selection
                                 onChange={this.onChange}
                                 options={this.getOptions()}
-                                renderLabel={language => this.renderLabel(language, modelLanguages.map(lang => (lang.value)))}
+                                renderLabel={language => this.renderLabel(language, modelLanguages.map(lang => lang.value))}
                                 data-cy='language-selector'
                             />
+                            {!!modelLanguages.length && this.renderDeleteModelLanguages()}
                         </Form.Field>
-                        { !!modelLanguages.length && <SelectField name='defaultLanguage' options={modelLanguages} className='project-default-language' data-cy='default-langauge-selection' />}
+                        {!!modelLanguages.length && <SelectField name='defaultLanguage' options={modelLanguages} className='project-default-language' data-cy='default-langauge-selection' />}
                         <br />
                         <ErrorsField />
                         <SubmitField className='primary save-project-info-button' value='Save Changes' data-cy='save-changes' />
@@ -135,11 +130,18 @@ ProjectInfo.propTypes = {
 
 const ProjectInfoContainer = withTracker(({ projectId }) => {
     const modelsHanlder = Meteor.subscribe('nlu_models.lite');
-    const project = Projects.findOne({ _id: projectId }, {
-        fields: {
-            name: 1, namespace: 1, apiKey: 1, nlu_models: 1, defaultLanguage: 1,
+    const project = Projects.findOne(
+        { _id: projectId },
+        {
+            fields: {
+                name: 1,
+                namespace: 1,
+                apiKey: 1,
+                nlu_models: 1,
+                defaultLanguage: 1,
+            },
         },
-    });
+    );
     if (!project) return browserHistory.replace({ pathname: '/404' });
     const projectsHandler = Meteor.subscribe('projects', projectId);
     const ready = modelsHanlder.ready() && projectsHandler.ready();
