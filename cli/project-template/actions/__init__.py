6012e5bb819ebca2239541ee6a03d065289712f3
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-06-27 09:33:31 -0400

    feat(cli): support for multiple actions files in scaffold project

---
+++ cli/project-template/actions/__init__.py
@@ -0,0 +1,13 @@
+###############################################################
+#  This file is necessary. Don't remove it and don't alter it #
+###############################################################
+
+from rasa_addons.custom_actions import ActionBotfrontMapping, ActionBotfrontMappingFollowUp
+
+###############################################################
+#  Add your actions in a separate file                        #
+###############################################################
+
+
+
+
