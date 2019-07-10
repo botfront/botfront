40a030ed32e3a6e429252a57aea973125a15cf18
Matthieu Joannon <mattjoannon@gmail.com>
2019-07-10 15:44:52 -0400

    chore: added a few storybooks

--- botfront/stories/ChangesSaved.stories.js
+++ botfront/stories/ChangesSaved.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
 import ChangesSaved from '../imports/ui/components/utils/ChangesSaved';
 
 storiesOf('ChangesSaved', module)
+    .addDecorator(withKnobs)
     .add('default', () => <ChangesSaved />)
-    .add('with title', () => <ChangesSaved title='Saved ✅' />);
+    .add('with variable title', () => <ChangesSaved title={text('Title', 'Saved')} />);
