2b3cd75fe90e335392bfaaf46f16fde486c8f625
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-07-15 14:50:36 -0400

    feat(AddStoryLine): first commit

---
+++ botfront/imports/ui/components/stories/common/AddStoryLine.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BotResponsePopupContent from './BotResponsePopupContent';
+import ActionPopupContent from './ActionPopupContent';
+import SlotPopupContent from './SlotPopupContent';
+import DashedButton from './DashedButton';
+import './style.less';
+
+const AddStoryLine = (props) => {
+    const {
+        onSelectResponse, onCreateResponse, onSelectAction, onSelectSlot,
+    } = props;
+    return (
+        <div>
+            <BotResponsePopupContent
+                onSelect={r => onSelectResponse(r)}
+                onCreate={r => onCreateResponse(r)}
+                trigger={<DashedButton color='green'>Bot Response</DashedButton>}
+            />
+            <ActionPopupContent
+                onSelect={action => onSelectAction(action)}
+                trigger={<DashedButton color='violet'>Action</DashedButton>}
+            />
+            <SlotPopupContent
+                onSelect={slot => onSelectSlot(slot)}
+                trigger={<DashedButton color='orange'>Slot</DashedButton>}
+            />
+        </div>
+    );
+};
+
+AddStoryLine.propTypes = {
+    onSelectResponse: PropTypes.func.isRequired,
+    onCreateResponse: PropTypes.func.isRequired,
+    onSelectAction: PropTypes.func.isRequired,
+    onSelectSlot: PropTypes.func.isRequired,
+};
+
+export default AddStoryLine;
---
+++ botfront/stories/AddStoryLine.stories.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, select } from '@storybook/addon-knobs';
+import AddStoryLine from '../imports/ui/components/stories/common/AddStoryLine';
+import Context from '../imports/ui/components/stories/common/Context';
+
+const noSlots = [];
+
+const selectionOne = [
+    { name: 'textSlot1', type: 'text' }, { name: 'textSlot2', type: 'text' }, { name: 'textSlot3', type: 'text' },
+    { name: 'floatSlot1', type: 'float' }, { name: 'floatSlot2', type: 'float' }, { name: 'floatSlot3', type: 'float' },
+];
+
+const selectionTwo = [
+    ...selectionOne,
+    { name: 'catSlot1', type: 'categorical' }, { name: 'catSlot2', type: 'categorical' }, { name: 'catSlot3', type: 'categorical' },
+    { name: 'listSlot1', type: 'list' }, { name: 'listSlot2', type: 'list' }, { name: 'listSlot3', type: 'list' },
+];
+
+const selectionThree = [
+    ...selectionTwo,
+    { name: 'boolSlot1', type: 'bool' }, { name: 'boolSlot2', type: 'bool' }, { name: 'boolSlot3', type: 'bool' },
+    { name: 'unfeatSlot1', type: 'unfeaturized' }, { name: 'unfeatSlot2', type: 'unfeaturized' }, { name: 'unfeatSlot3', type: 'unfeaturized' },
+];
+
+const selection = {
+    noSlots, selectionOne, selectionTwo, selectionThree,
+};
+
+const responses = [
+    { name: 'YO' },
+    { name: 'blah' },
+    { name: 'doodoo' },
+];
+
+storiesOf('AddStoryLine', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => (
+        <Context.Provider
+            value={{
+                slots: select('Available slots', selection, noSlots),
+                responses,
+            }}
+        >
+            {story()}
+        </Context.Provider>
+    ))
+    .add('default', () => (
+        <AddStoryLine
+            onSelectResponse={r => alert(`${r.name}!!`)}
+            onCreateResponse={r => alert(`${r}!!`)}
+            onSelectAction={action => alert(`${action}!!`)}
+            onSelectSlot={slot => alert(`${slot.name}!!`)}
+        />
+    ));
