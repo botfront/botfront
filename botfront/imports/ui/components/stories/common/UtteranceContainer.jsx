0eba46ca19c99ea08b229e60b7c743dda798864b
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-07-22 16:55:35 -0400

    chore(UtteranceContainer): remove edit mode

--- botfront/imports/ui/components/stories/common/UtteranceContainer.jsx
+++ botfront/imports/ui/components/stories/common/UtteranceContainer.jsx
@@ -7,24 +7,15 @@ import UserUtteranceViewer from '../../utils/UserUtteranceViewer';
 const UtteranceContainer = (props) => {
     const {
         agent, value, onInputUserUtterance, onInputBotResponse, onDelete, onAbort,
-        onChange, onSave,
+        onChange,
     } = props;
     const [mode, setMode] = useState(!value ? 'input' : 'view');
     const [input, setInput] = useState();
-    const container = useRef();
     useEffect(() => {
         setMode(!value ? 'input' : 'view');
     }, value);
 
-    const excludedTarget = e => (
-        // Check class of element that triggered blur against excluded classNames.
-        ['trash'].some(t => !!e.relatedTarget && e.relatedTarget.className.split(' ').includes(t))
-    );
-
     const render = () => {
-        if (mode === 'edit') {
-            container.current.focus();
-        }
         if (mode === 'input') {
             return (
                 <UtteranceInput
@@ -39,32 +30,22 @@ const UtteranceContainer = (props) => {
                 />
             );
         }
+        if (agent === 'user') {
+            return (
+                <UserUtteranceViewer
+                    value={value}
+                    allowEditing
+                    size='mini'
+                    onChange={v => onChange(v)}
+                />
+            );
+        }
         return (
-            <div
-                ref={container}
-                className='focus-box'
-                role='textbox'
-                tabIndex={0}
-                onMouseDown={(e) => { e.preventDefault(); }}
-                onBlur={(e) => { if (mode === 'edit' && !excludedTarget(e)) onSave(); }}
-            >
-                {agent === 'user'
-                    ? (
-                        <UserUtteranceViewer
-                            value={value}
-                            allowEditing={mode === 'edit'}
-                            size='mini'
-                            onChange={v => onChange(v)}
-                        />
-                    )
-                    : `Bot response ${mode}`
-                }
-            </div>
+            <div>{`Bot response ${mode}`}</div>
         );
     };
 
     return (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
         <div
             className='utterance-container'
             mode={mode}
@@ -74,9 +55,8 @@ const UtteranceContainer = (props) => {
                 {render()}
             </div>
             <FloatingIconButton
-                icon={mode === 'view' ? 'edit' : 'trash'}
+                icon='trash'
                 onClick={() => {
-                    if (mode === 'view') return setMode('edit');
                     if (mode === 'input') return onAbort();
                     return onDelete();
                 }}
@@ -91,7 +71,6 @@ UtteranceContainer.propTypes = {
     onInputUserUtterance: PropTypes.func.isRequired,
     onInputBotResponse: PropTypes.func.isRequired,
     onChange: PropTypes.func.isRequired,
-    onSave: PropTypes.func.isRequired,
     onDelete: PropTypes.func.isRequired,
     onAbort: PropTypes.func.isRequired,
 };
