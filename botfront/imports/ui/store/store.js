41d1f4ec6087030cd43cc43612a55a1911dcc3f5
Matthieu Joannon <mattjoannon@gmail.com>
2019-08-09 10:32:08 -0400

    feat: save current story group

--- botfront/imports/ui/store/store.js
+++ botfront/imports/ui/store/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import reducer from './reducers/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
