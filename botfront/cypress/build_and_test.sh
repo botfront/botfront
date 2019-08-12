e363f6af112fe9c36bb4621e53c970a76dab21c4
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-08-12 18:38:37 -0400

    test: fix ci for ee

--- botfront/cypress/build_and_test.sh
+++ botfront/cypress/build_and_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build -t botfront-local ../../.
-docker build -t gcr.io/botfront-project/cypress-runner:3.4.0 .
+docker build -t gcr.io/botfront-project/cypress-runner:latest .
 docker-compose up --abort-on-container-exit --exit-code-from e2e
