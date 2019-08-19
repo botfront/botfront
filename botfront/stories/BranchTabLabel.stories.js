6b36f0c6c1f75f028048ed5f0534cd867e798745
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-08-19 17:46:26 -0400

    refactor: remove repetative functions

--- botfront/stories/BranchTabLabel.stories.js
+++ botfront/stories/BranchTabLabel.stories.js
@@ -8,7 +8,7 @@ import BranchTabLabel from '../imports/ui/components/stories/BranchTabLabel.jsx'
 const MenuContentsWrapper = () => {
     // eslint-disable-next-line no-unused-vars
     const [activeItem, setActiveItem] = useState('hi');
-    const [tabValues, setTabValue] = useState(['hi', 'this is a longer variable name', 'test branch']);
+    const [tabValues, setTabValue] = useState(['hi', 'this is a longer variable name', 'test branch', 'new test', 'mybranch6', 'other branch', 'add more branch names to go further']);
     const onDelete = () => {
         // eslint-disable-next-line no-console
         console.log('deleted');
@@ -23,36 +23,28 @@ const MenuContentsWrapper = () => {
     const handleOnClick = (value) => {
         setActiveItem(value);
     };
-    return (
-        <>
-            <BranchTabLabel
-                active={activeItem}
-                onDelete={onDelete}
-                onChangeName={handleOnChange}
-                onSelect={setActiveItem}
-                value={tabValues[0]}
-                hasWarning={boolean('hasWarning', false)}
-                hasError={boolean('hasError', false)}
-            />
-            <BranchTabLabel
-                active={activeItem}
-                onDelete={onDelete}
-                onChangeName={handleOnChange}
-                onSelect={handleOnClick}
-                value={tabValues[1]}
-                hasWarning={boolean('hasWarning', false)}
-                hasError={boolean('hasError', false)}
-            />
-            <BranchTabLabel
-                active={activeItem}
-                onDelete={onDelete}
-                onChangeName={handleOnChange}
-                onSelect={handleOnClick}
-                value={tabValues[2]}
-                hasWarning={boolean('hasWarning', false)}
-                hasError={boolean('hasError', false)}
-            />
-        </>
+
+    const renderTabs = (nTabs) => {
+        // this will only add up to a max of seven branches add more branch names in const [tabValue, setTabValue]
+        const tabList = [];
+        for (let i = 0; i < nTabs; i += 1) {
+            const tab = (
+                <BranchTabLabel
+                    active={activeItem}
+                    onDelete={onDelete}
+                    onChangeName={handleOnChange}
+                    onSelect={handleOnClick}
+                    value={tabValues[i]}
+                    hasWarning={boolean('hasWarning', false)}
+                    hasError={boolean('hasError', false)}
+                />
+            );
+            tabList.push(tab);
+        }
+        return tabList;
+    };
+
+    return (renderTabs(7)
     );
 };
 
