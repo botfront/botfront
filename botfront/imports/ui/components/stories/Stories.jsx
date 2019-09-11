f6d810e720cad70e331cfc4c35d3c858edc6a406
Matthieu Joannon <mattjoannon@gmail.com>
2019-09-11 11:28:27 +0200

    fix: add empty branches to new stories

--- botfront/imports/ui/components/stories/Stories.jsx
+++ botfront/imports/ui/components/stories/Stories.jsx
@@ -103,6 +103,7 @@ class Stories extends React.Component {
                 storyGroupId: `${
                     !!introStoryGroup ? introStoryGroup._id : storyGroups[storyIndex]._id
                 }`,
+                branches: [],
             },
             wrapMeteorCallback(),
         );
