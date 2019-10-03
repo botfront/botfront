f13b1d264525da43f05fb77a91c19ad9a50e7064
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-10-03 09:51:08 -0400

    fix: code climate complexity in ImportProject

--- botfront/imports/ui/components/settings/ImportProject.jsx
+++ botfront/imports/ui/components/settings/ImportProject.jsx
@@ -38,7 +38,8 @@ const ImportProject = ({
     const [uploadedFiles, setUploadedFiles] = useState({});
 
     const validateImportType = () => {
-        if (!importTypeOptions.some(({ value }) => value === importType)) {
+        console.log(importType);
+        if (!importTypeOptions.some(({ value }) => value === importType.value)) {
             return false;
         }
         return true;
@@ -72,7 +73,7 @@ const ImportProject = ({
     };
 
     const importButtonText = () => {
-        if (importType === 'botfront') {
+        if (importType.value === 'botfront') {
             return 'Import Botfront project';
         }
         return 'Import project';
@@ -105,14 +106,43 @@ const ImportProject = ({
     //         </>
     //     )
     // );
+
+    const renderBackupMessage = () => {
+        if (backupDownloaded === true) {
+            return (
+                <Message
+                    positive
+                    icon='check circle'
+                    header='Backup successfully downloaded!'
+                    content='You may now import your Botfront project.'
+                />
+            );
+        }
+        return (
+            <>
+                <Message
+                    warning
+                    icon='exclamation circle'
+                    header='Your project will be overwritten.'
+                    content='Please use the button below to download a backup before proceeding.'
+                />
+                <Button onClick={backupProject} className='export-option' data-cy='backup-project-button'>
+                    <Icon name='download' />
+                    Backup current project
+                </Button>
+                <br />
+            </>
+        );
+    };
+
     if (importSuccessful) {
         return (
             <Message
                 positive
                 className='import-successful'
                 icon='check circle'
-                header={importTypeOptions.find(options => options.value === importType).successHeader}
-                content={importTypeOptions.find(options => options.value === importType).successText}
+                header={importType.successHeader}
+                content={importTypeOptions.successText}
                 data-cy='project-imported'
             />
         );
@@ -130,7 +160,7 @@ const ImportProject = ({
                     onChange={(x, { value }) => { setImportType(getImportType(value)); }}
                 />
                 <br />
-                {(importType === 'botfront' && validateImportType()) && (
+                {(importType.value === 'botfront' && validateImportType()) && (
                     <ImportDropField
                         onChange={fileAdded}
                         text='Drop your Botfront project in JSON format here. Data should not be larger than 30 Mb.'
@@ -141,34 +171,7 @@ const ImportProject = ({
                         successMessage='Your Botfront project file is ready.'
                     />
                 )}
-                {importType === 'botfront' && botfrontFileSuccess && (
-                    (backupDownloaded
-                        ? (
-                            <>
-                                <Message
-                                    positive
-                                    icon='check circle'
-                                    header='Backup successfully downloaded!'
-                                    content='You may now import your Botfront project.'
-                                />
-                            </>
-                        )
-                        : (
-                            <>
-                                <Message
-                                    warning
-                                    icon='exclamation circle'
-                                    header='Your project will be overwritten.'
-                                    content='Please use the button below to download a backup before proceeding.'
-                                />
-                                <Button onClick={backupProject} className='export-option' data-cy='backup-project-button'>
-                                    <Icon name='download' />
-                                    Backup current project
-                                </Button>
-                                <br />
-                            </>
-                        ))
-                )}
+                {renderBackupMessage()}
                 {validateImportType() && (
                     <Button
                         onClick={importProject}
