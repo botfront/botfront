e622a1c4fa77f5a4d2d1d703289fb9bd8193cc34
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-10-08 12:08:54 -0400

    fix: import of large nlu files simplify backend operations on incoming data

--- botfront/imports/ui/components/synonyms/GazetteConfig.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const modeDefault = "ratio";
-const minScoreDefault = 80;
-
-export function setGazetteDefaults(gazette) {
-    gazette.mode = gazette.mode || modeDefault;
-    gazette.min_score = gazette.min_score || minScoreDefault;
-}
