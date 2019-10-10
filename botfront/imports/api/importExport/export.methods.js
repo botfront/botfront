cb183ce69bac799176eb6ea1993688ef204d082b
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-10-10 15:40:52 -0400

    fix: remove timeout interval from export.methods

--- botfront/imports/api/importExport/export.methods.js
+++ botfront/imports/api/importExport/export.methods.js
@@ -14,40 +14,25 @@ if (Meteor.isServer) {
             check(projectId, String);
             checkIfCan('global-admin');
 
-            let timeOut = false;
-            let resolveByTimeOut;
-
             const options = getRequestOptions(apiHost, `/project/${projectId}/export`, 'GET');
 
             const exportRequest = new Promise((resolve) => {
                 const req = http.request(options, (res) => {
                     if (res.statusCode !== 200) {
-                        if (resolveByTimeOut !== undefined) clearInterval(resolveByTimeOut);
                         resolve({ success: false, errorText: `${res.statusCode} API was not found` });
                     }
                     let data = '';
                     res.on('data', (d) => {
                         data += d;
-                        timeOut = false;
-                        if (resolveByTimeOut !== undefined) clearInterval(resolveByTimeOut);
-                        resolveByTimeOut = setInterval(() => {
-                            if (timeOut === true) {
-                                if (resolveByTimeOut !== undefined) clearInterval(resolveByTimeOut);
-                                resolve({ data, success: true });
-                            }
-                            timeOut = true;
-                        }, 2000);
                         if (
                             res.statusCode === 200
                             && data.length === parseInt(res.headers['content-length'], 10)
                         ) {
-                            if (resolveByTimeOut !== undefined) clearInterval(resolveByTimeOut);
                             resolve({ data, success: true });
                         }
                     });
                 });
                 req.on('error', (error) => {
-                    if (resolveByTimeOut !== undefined) clearInterval(resolveByTimeOut);
                     resolve({ success: false, errorText: generateErrorText(error) });
                 });
                 req.end();
