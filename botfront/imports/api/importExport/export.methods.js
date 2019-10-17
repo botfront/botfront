f82ecee0efac04e1fe6ebb0f5a9809edd27c5b77
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-10-17 13:50:49 -0400

    feat: add option to export conversations

--- botfront/imports/api/importExport/export.methods.js
+++ botfront/imports/api/importExport/export.methods.js
@@ -9,11 +9,20 @@ import { generateErrorText } from './importExport.utils';
 
 if (Meteor.isServer) {
     Meteor.methods({
-        'exportProject'(apiHost, projectId) {
+        'exportProject'(apiHost, projectId, options) {
             check(apiHost, String);
             check(projectId, String);
+            check(options, Object);
 
-            const exportRequest = axios.get(`${apiHost}/project/${projectId}/export`)
+            const includeConversations = options.includeConversations !== undefined
+                ? options.includeConversations
+                : true;
+            check(includeConversations, Boolean);
+
+            const exportRequest = axios.get(
+                `${apiHost}/project/${projectId}/export`,
+                { params: { includeConversations: options.includeConversations } },
+            )
                 .then(res => ({ data: JSON.stringify(res.data) }))
                 .catch(err => (
                     { error: { header: 'Export Failed', text: generateErrorText(err) } }
