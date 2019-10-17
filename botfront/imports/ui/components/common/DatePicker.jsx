03f484da6ad18976f636b6b497137499417a9642
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-10-17 15:42:55 -0400

    feat: add a date picker component

---
+++ botfront/imports/ui/components/common/DatePicker.jsx
@@ -0,0 +1,150 @@
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
+import { DayPickerRangeController } from 'react-dates';
+import React, { useState } from 'react';
+import {
+    Popup, Button, Icon, Form, Menu,
+} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import momentPropTypes from 'react-moment-proptypes';
+
+function DatePicker({ startDate, endDate, onConfirm }) {
+    const [focusedInput, setFocusedInput] = useState('startDate');
+    const [newStartDate, setNewStartDate] = useState(startDate);
+    const [newEndDate, setNewEndDate] = useState(endDate);
+    const [popupOpen, setPopupOpen] = useState(false);
+    const [selectedRangeType, setSelectedRangeType] = useState(0);
+
+    function setNewDates(start, end) {
+        setNewStartDate(start);
+        setNewEndDate(end);
+    }
+
+    function getDateString(start, end) {
+        return `${start ? `${start.format('DD MMM YYYY')} - ` : ''} ${end ? end.format('DD MMM YYYY') : ''}`;
+    }
+
+    function sendNewDates() {
+        onConfirm(newStartDate, newEndDate);
+        setPopupOpen(!popupOpen);
+    }
+    /* the value field should is the array index of the object
+     it is used to get the data field after a selection in the dropdown
+     the dropdown does not support objects in its value so this a workaround */
+    const DateOptions = [
+        {
+            key: 'custom',
+            text: `Custom: ${getDateString(newStartDate, newEndDate)}`,
+            value: 0,
+            data: { startDate: null, endDate: null },
+        },
+        {
+            key: 'seven',
+            text: 'Last 7 days',
+            value: 1,
+            data: { startDate: moment().subtract(7, 'days'), endDate: moment() },
+        },
+        {
+            key: 'thirty',
+            text: 'Last 30 days',
+            value: 2,
+            data: { startDate: moment().subtract(30, 'days'), endDate: moment() },
+        },
+        {
+            key: 'ninety',
+            text: 'Last 90 days',
+            value: 3,
+            data: { startDate: moment().subtract(90, 'days'), endDate: moment() },
+        },
+    ];
+
+    function datesChange(newRange) {
+        setSelectedRangeType(0); // if there is date change, the range type is always custom (index 0)
+        setNewDates(newRange.startDate, newRange.endDate);
+    }
+
+    function rangeChange(data) {
+        setSelectedRangeType(data.value);
+        const range = DateOptions[data.value].data;
+        setNewDates(range.startDate, range.endDate);
+        setFocusedInput('startDate');
+    }
+
+    function handlePopupState() {
+        if (popupOpen) setNewDates(startDate, endDate); // removes dates changes before closing (in case of a cancel)
+        setPopupOpen(!popupOpen);
+    }
+
+    return (
+        <Popup
+            flowing
+            className='date-picker'
+            open={popupOpen}
+            trigger={(
+                <Button icon labelPosition='left' onClick={() => handlePopupState()}>
+                    {startDate ? getDateString(startDate, endDate) : 'Pick a range'}
+                    <Icon name='calendar alternate' />
+                </Button>
+            )}
+        >
+
+            <Form>
+                <Form.Dropdown
+                    label='Date range'
+                    value={selectedRangeType}
+                    fluid
+                    selection
+                    selectOnBlur={false}
+                    options={DateOptions}
+                    data-cy='date-range-selector'
+                    onChange={(_, data) => rangeChange(data)}
+                />
+            </Form>
+
+
+            <DayPickerRangeController
+                startDate={newStartDate}
+                endDate={newEndDate}
+                onDatesChange={dateRange => datesChange(dateRange)}
+                focusedInput={focusedInput}
+                onFocusChange={newFocusedInput => (
+                    newFocusedInput === null ? setFocusedInput('startDate') : setFocusedInput(newFocusedInput)
+                )}
+                initialVisibleMonth={() => moment().subtract(1, 'M')}
+                numberOfMonths={2}
+                noBorder
+                hideKeyboardShortcutsPanel
+            />
+
+            <Menu secondary>
+                <Menu.Item>
+                    <Button content='Cancel' onClick={() => handlePopupState()} />
+                </Menu.Item>
+
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                        <Button primary content='Confirm' onClick={() => sendNewDates()} />
+                    </Menu.Item>
+
+                </Menu.Menu>
+            </Menu>
+        </Popup>
+    );
+}
+
+
+DatePicker.propTypes = {
+    startDate: momentPropTypes.momentObj,
+    endDate: momentPropTypes.momentObj,
+    onConfirm: PropTypes.func.isRequired,
+};
+
+
+DatePicker.defaultProps = {
+    startDate: null,
+    endDate: null,
+};
+
+
+export default DatePicker;
