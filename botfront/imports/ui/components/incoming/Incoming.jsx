984a7640c5339b06b8d0d3a1c2af5eae6b0e5559
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-10-21 09:13:32 -0400

    feat: connect required data to incoming tab

--- botfront/imports/ui/components/incoming/Incoming.jsx
+++ botfront/imports/ui/components/incoming/Incoming.jsx
@@ -6,15 +6,19 @@ import PropTypes from 'prop-types';
 import { Menu, Container, Tab } from 'semantic-ui-react';
 
 import { Projects } from '../../../api/project/project.collection';
-import { getNluModelLanguages } from '../../../api/nlu_model/nlu_model.utils';
+import { NLUModels } from '../../../api/nlu_model/nlu_model.collection';
+import { getPublishedNluModelLanguages, getNluModelLanguages } from '../../../api/nlu_model/nlu_model.utils';
+import { Instances } from '../../../api/instances/instances.collection';
+
 import ConversationsBrowser from '../conversations/ConversationsBrowser.jsx';
+import Activity from '../nlu/activity/Activity';
 
 import LanguageDropdown from '../common/LanguageDropdown';
 
 class Incoming extends React.Component {
     constructor (props) {
         super(props);
-        this.state = { selectedLanguage: 'en', activeMenuItem: 'Incoming' };
+        this.state = { selectedLanguage: 'en' };
     }
 
     handleLanguageChange = (value) => {
@@ -26,7 +30,7 @@ class Incoming extends React.Component {
     renderPanes = () => {
         const panes = [
             { menuItem: 'Incoming', render: () => <Tab.Pane>Incoming data</Tab.Pane> },
-            { menuItem: 'Conversations', render: () => <Tab.Pane as={() => <ConversationsBrowser />} /> },
+            { menuItem: 'Conversations', render: () => <Tab.Pane>Conversations</Tab.Pane> },
             { menuItem: 'Out of Scope', render: () => <Tab.Pane>Out of Scope data</Tab.Pane> },
             { menuItem: 'Populate', render: () => <Tab.Pane>Populate data</Tab.Pane> },
         ];
@@ -34,8 +38,10 @@ class Incoming extends React.Component {
     }
 
     render () {
-        const { projectLanguages, ready } = this.props;
+        const { projectLanguages, ready, instances } = this.props;
         const { selectedLanguage } = this.state;
+        console.log(instances);
+
         if (!ready) {
             return <div>loading</div>;
         }
@@ -50,12 +56,14 @@ class Incoming extends React.Component {
                         />
                     </Menu.Item>
                 </Menu>
-                <Container>
-                    <Tab
-                        panes={this.renderPanes()}
-                        menu={{ secondary: true, pointing: true }}
-                    />
-                </Container>
+                <>
+                    <Container>
+                        <Tab
+                            panes={this.renderPanes()}
+                            menu={{ secondary: true, pointing: true }}
+                        />
+                    </Container>
+                </>
             </>
         );
     }
@@ -65,6 +73,13 @@ Incoming.propTypes = {
     projectLanguages: PropTypes.array,
     // projectId: PropTypes.string,
     ready: PropTypes.bool,
+    model: PropTypes.object.isRequired,
+    utterances: PropTypes.array.isRequired,
+    entities: PropTypes.array.isRequired,
+    intents: PropTypes.array.isRequired,
+    instances: PropTypes.object.isRequired,
+    linkRender: PropTypes.func.isRequired,
+    outDatedUtteranceIds: PropTypes.array.isRequired,
 };
 
 Incoming.defaultProps = {
@@ -76,9 +91,16 @@ Incoming.defaultProps = {
 const IncomingContainer = withTracker(({ projectId }) => {
     const project = Projects.findOne({ _id: projectId });
     const projectLanguages = getNluModelLanguages(project.nlu_models, true);
+    const instances = Instances.find({ projectId }).fetch();
+    const instance = instances ? instances.find(({ _id }) => _id === project.instance) : {};
+
+    console.log(instance);
     return {
         projectLanguages,
-        ready: !!projectLanguages,
+        ready: !!projectLanguages && !!instances,
+        project,
+        instances,
+        instance,
     };
 })(Incoming);
 
