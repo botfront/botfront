c6e95ef9a1b6b06eef0b7722e9224f695caed438
Nathan Zylbersztejn <nathan.zylbersztejn@gmail.com>
2019-10-25 18:23:01 +0000

    Merged in feat/namespace-format (pull request #65)
    feat: enforce namespaces starting with 'bf-'

--- botfront/imports/api/project/project.schema.gke.js
+++ botfront/imports/api/project/project.schema.gke.js
@@ -3,6 +3,17 @@ import { ProjectsSchema as BaseProjectsSchema } from './project.schema.default';
 export const ProjectsSchema = BaseProjectsSchema.extend({
     apiKey: { type: String, optional: true },
     namespace: {
-        type: String, regEx: /^[a-z0-9-_]+$/, unique: 1, sparse: 1,
+        type: String,
+        unique: 1,
+        sparse: 1,
+        custom() {
+            return !this.value.match(/^bf-[a-zA-Z0-9-]+$/) ? 'invalidNamespace' : null;
+        },
+    },
+});
+
+ProjectsSchema.messageBox.messages({
+    en: {
+        invalidNamespace: 'The namespace must starts with \'bf-\' and can only contain letters, numbers and dashes (\'-\')',
     },
 });
