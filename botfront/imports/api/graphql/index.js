df30d763f397d78c4b75b451531a40137260e07f
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-10-29 18:14:35 -0400

    fix(analytics): get rid of file-loader

--- botfront/imports/api/graphql/index.js
+++ botfront/imports/api/graphql/index.js
@@ -1,20 +1,15 @@
-import path from 'path';
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+import { mergeTypes } from 'merge-graphql-schemas';
 
 import conversationResolvers from './conversations/resolvers';
 import nluResolvers from './nlu/resolvers';
+import conversationTypes from './conversations/schemas';
+import nluTypes from './nlu/schemas';
 
-/*
-* fileLoader doesn't work with ES6 (https://github.com/Urigo/merge-graphql-schemas/issues?utf8=%E2%9C%93&q=unexpected+token) 
-* so we need to import resolvers one by one
-*/
 export const resolvers = [
     ...conversationResolvers,
     ...nluResolvers,
 ];
-const basePath = path.join(/(.+)\.meteor/.exec(process.cwd())[1], __dirname);
-const typesArray = fileLoader(path.join(basePath, '..', '**', '*.types.graphql'));
-
-// const resolversArray = fileLoader(path.join(basePath, '..', '**', '*Resolver.js'));
-
-export const typeDefs = mergeTypes(typesArray, { all: true });
+export const typeDefs = mergeTypes([
+    ...conversationTypes,
+    ...nluTypes,
+], { all: true });
---
+++ botfront/imports/api/graphql/nlu/schemas/index.js
@@ -0,0 +1,7 @@
+import entityDistributionTypes from './entityDistribution.types.graphql';
+import intentDistributionTypes from './intentDistribution.types.graphql';
+
+export default [
+    entityDistributionTypes,
+    intentDistributionTypes,
+];
