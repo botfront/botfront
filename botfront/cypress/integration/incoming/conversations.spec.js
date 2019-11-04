203e3426809e4610a5876f3e2bb5d648ef94f4ef
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-11-04 09:59:23 -0500

    fix: use withRouter for routing, func for get url

--- botfront/cypress/integration/incoming/conversations.spec.js
+++ botfront/cypress/integration/incoming/conversations.spec.js
@@ -193,3 +193,23 @@ describe('incoming page conversation tab pagination', function () {
         cy.dataCy('conversation-item').should('have.length', 5);
     });
 });
+
+describe('incoming page', function() {
+    beforeEach(function() {
+        cy.createProject('bf', 'My Project', 'en');
+        cy.login();
+    });
+
+    afterEach(function() {
+        cy.logout();
+        cy.deleteProject('bf');
+    });
+
+    it('should have be able to view different conversations', function() {
+        cy.visit('/project/bf/incoming');
+    });
+    it('should have be able to change pages', function() {
+    });
+    it('should go to the same page and conversation when refreshed', function() {
+    });
+});
