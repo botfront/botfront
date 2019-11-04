742e1335db52c69a2ac7fea51fa4f1a2cbbf0a3c
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-11-04 09:25:15 -0500

    test: fix before and after hooks

--- botfront/cypress/integration/stories/slots.spec.js
+++ botfront/cypress/integration/stories/slots.spec.js
@@ -6,15 +6,13 @@ describe('slots', function() {
     before(function() {
         cy.deleteProject('bf');
     });
-    beforeEach(function() {
-        cy.createProject('bf', 'My Project', 'fr');
-        cy.createUser('admin', 'admin@bf.com', 'project-admin', 'bf');
-        cy.loginTestUser('admin@bf.com');
-    });
 
     afterEach(function() {
-        cy.deleteUser('admin@bf.com');
         cy.deleteProject('bf');
+    });
+
+    beforeEach(function() {
+        cy.createProject('bf', 'My Project', 'fr').then(() => cy.login());
     });
 
     function createSlot() {
