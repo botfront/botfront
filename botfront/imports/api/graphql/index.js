c8da6e75cd3fba9e2fca1decb77bb34bd6c8ad68
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-11-05 15:37:31 -0500

    refactor: match with botfront and not meteor

--- botfront/imports/api/graphql/index.js
+++ botfront/imports/api/graphql/index.js
@@ -3,13 +3,13 @@ import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
 
 import conversationsResolver from './conversations/resolvers/conversationsResolver';
 /*
-* mergeResolvers doesnt work (https://github.com/Urigo/merge-graphql-schemas/issues?utf8=%E2%9C%93&q=unexpected+token) 
+* mergeResolvers doesnt work (https://github.com/Urigo/merge-graphql-schemas/issues?utf8=%E2%9C%93&q=unexpected+token)
 * so we need to import resolvers one by one
 */
 export const resolvers = [
     conversationsResolver,
 ];
-const basePath = path.join(/(.+)\.meteor/.exec(process.cwd())[1], __dirname);
+const basePath = path.join(/(.+botfront\/)/.exec(process.cwd())[1], __dirname);
 const typesArray = fileLoader(path.join(basePath, '..', '**', '*.graphql'));
 
 // const resolversArray = fileLoader(path.join(basePath, '..', '**', '*Resolver.js'));
