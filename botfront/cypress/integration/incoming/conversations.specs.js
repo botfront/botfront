e8dad49f9ef07947a453864491135264954cab65
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-11-06 17:24:55 -0500

    refactor: eslint fix and text fix

--- botfront/cypress/integration/incoming/conversations.specs.js
+++ botfront/cypress/integration/incoming/conversations.specs.js
@@ -12,115 +12,113 @@ describe('incoming page conversation tab', function () {
 
     afterEach(function () {
         cy.logout();
-
     });
 
     function addConversation(id) {
         let url = `http://localhost:8080/project/bf/conversations/${id}/insert?api-key=`;
-        if (Cypress.env('MODE') && Cypress.env('MODE') === 'CI_RUN') {
-            url = `http://botfront-api:8080/project/bf/conversations/${id}/insert?api-key=`;
+        if (Cypress.env('CYPRESS_API_URL')) {
+            url = `${CYPRESS_API_URL}/project/bf/conversations/${id}/insert?api-key=`;
         }
 
-        let body = {
-            "sender_id": "test",
-            "slots": {
-                "disambiguation_message": null
+        const body = {
+            sender_id: 'test',
+            slots: {
+                disambiguation_message: null,
             },
-            "latest_message": {
-                "text": "/get_started",
-                "intent": {
-                    "name": "get_started",
-                    "confidence": 1
+            latest_message: {
+                text: '/get_started',
+                intent: {
+                    name: 'get_started',
+                    confidence: 1,
                 },
-                "intent_ranking": [
+                intent_ranking: [
                     {
-                        "name": "get_started",
-                        "confidence": 1
-                    }
+                        name: 'get_started',
+                        confidence: 1,
+                    },
                 ],
-                "entities": [
+                entities: [
 
-                ]
+                ],
             },
-            "latest_event_time": 1572982150.8615842,
-            "followup_action": null,
-            "paused": false,
-            "events": [
+            latest_event_time: 1572982150.8615842,
+            followup_action: null,
+            paused: false,
+            events: [
                 {
-                    "event": "action",
-                    "timestamp": 1572982150.8264863,
-                    "name": "action_listen",
-                    "policy": null,
-                    "confidence": null
+                    event: 'action',
+                    timestamp: 1572982150.8264863,
+                    name: 'action_listen',
+                    policy: null,
+                    confidence: null,
                 },
                 {
-                    "event": "user",
-                    "timestamp": 1572982150.8352745,
-                    "text": `"/get_started_${id}"`,
-                    "parse_data": {
-                        "text": `"/get_started_${id}"`,
-                        "intent": {
-                            "name": `"get_started_${id}"`,
-                            "confidence": 1
+                    event: 'user',
+                    timestamp: 1572982150.8352745,
+                    text: `"/get_started_${id}"`,
+                    parse_data: {
+                        text: `"/get_started_${id}"`,
+                        intent: {
+                            name: `"get_started_${id}"`,
+                            confidence: 1,
                         },
-                        "intent_ranking": [
+                        intent_ranking: [
                             {
-                                "name": `"get_started_${id}"`,
-                                "confidence": 1
-                            }
+                                name: `"get_started_${id}"`,
+                                confidence: 1,
+                            },
                         ],
-                        "entities": [
+                        entities: [
 
-                        ]
+                        ],
                     },
-                    "input_channel": "webchat",
-                    "message_id": "7c17782e578348cda0f336333e4a4008",
-                    "metadata": null
+                    input_channel: 'webchat',
+                    message_id: '7c17782e578348cda0f336333e4a4008',
+                    metadata: null,
                 },
                 {
-                    "event": "action",
-                    "timestamp": 1572982150.8542428,
-                    "name": "utter_get_started",
-                    "policy": "policy_1_AugmentedMemoizationPolicy",
-                    "confidence": 1
+                    event: 'action',
+                    timestamp: 1572982150.8542428,
+                    name: 'utter_get_started',
+                    policy: 'policy_1_AugmentedMemoizationPolicy',
+                    confidence: 1,
                 },
                 {
-                    "event": "bot",
-                    "timestamp": 1572982150.854263,
-                    "text": "utter_get_started",
-                    "data": {
-                        "elements": null,
-                        "quick_replies": null,
-                        "buttons": null,
-                        "attachment": null,
-                        "image": null,
-                        "custom": null
+                    event: 'bot',
+                    timestamp: 1572982150.854263,
+                    text: 'utter_get_started',
+                    data: {
+                        elements: null,
+                        quick_replies: null,
+                        buttons: null,
+                        attachment: null,
+                        image: null,
+                        custom: null,
                     },
-                    "metadata": {
+                    metadata: {
 
-                    }
+                    },
                 },
                 {
-                    "event": "action",
-                    "timestamp": 1572982150.8615842,
-                    "name": "action_listen",
-                    "policy": "policy_1_AugmentedMemoizationPolicy",
-                    "confidence": 1
-                }
+                    event: 'action',
+                    timestamp: 1572982150.8615842,
+                    name: 'action_listen',
+                    policy: 'policy_1_AugmentedMemoizationPolicy',
+                    confidence: 1,
+                },
             ],
-            "latest_input_channel": "webchat",
-            "active_form": {
+            latest_input_channel: 'webchat',
+            active_form: {
 
             },
-            "latest_action_name": "action_listen"
-        }
+            latest_action_name: 'action_listen',
+        };
         cy.request({
             method: 'POST',
             url,
             headers: { 'Content-Type': 'application/json' },
             body,
         });
-
     }
     it('should show a message if no converastions', function () {
         cy.visit('/project/bf/incoming');
