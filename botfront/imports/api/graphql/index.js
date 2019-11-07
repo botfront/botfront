e30d3e2700c1464887b1a1bed1dca202e5b3d7b6
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-11-07 14:53:48 -0500

    feat: add resolver for activity

--- botfront/imports/api/graphql/index.js
+++ botfront/imports/api/graphql/index.js
@@ -1,12 +1,15 @@
 import { mergeTypes } from 'merge-graphql-schemas';
 import conversationsResolver from './conversations/resolvers/conversationsResolver';
 import conversationTypes from './conversations/schemas';
+import activityResolver from './activity/resolvers/activityResolver';
+import activityTypes from './activity/schemas';
 /*
 * mergeResolvers doesnt work (https://github.com/Urigo/merge-graphql-schemas/issues?utf8=%E2%9C%93&q=unexpected+token)
 * so we need to import resolvers one by one
 */
 export const resolvers = [
     conversationsResolver,
+    activityResolver,
 ];
 
-export const typeDefs = mergeTypes([...conversationTypes], { all: true });
+export const typeDefs = mergeTypes([...conversationTypes, ...activityTypes], { all: true });
