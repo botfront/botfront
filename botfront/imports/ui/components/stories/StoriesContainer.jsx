be97fe9464d7bde4f32374ef099a9de13e373c1a
Matthieu Joannon <mattjoannon@gmail.com>
2019-11-08 12:38:05 -0500

    fix: fixed rasa parsing in visual story editor (#314)

--- botfront/imports/ui/components/stories/StoriesContainer.jsx
+++ botfront/imports/ui/components/stories/StoriesContainer.jsx
@@ -1,13 +1,25 @@
+import { Loader } from 'semantic-ui-react';
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import StoriesPageMenu from './StoriesPageMenu';
 
 const Stories = React.lazy(() => import('./Stories'));
 
-const StoriesContainer = props => (
-    <>
-        <StoriesPageMenu projectId={props.params.project_id} />
-        <React.Suspense fallback={null}><Stories projectId={props.params.project_id} /></React.Suspense>
-    </>
-);
+const StoriesContainer = (props) => {
+    const { params } = props;
+    return (
+        <>
+            <StoriesPageMenu projectId={params.project_id} />
+            <React.Suspense fallback={<Loader />}>
+                <Stories projectId={params.project_id} />
+            </React.Suspense>
+        </>
+    );
+};
+
+StoriesContainer.propTypes = {
+    params: PropTypes.object.isRequired,
+};
 
 export default StoriesContainer;
