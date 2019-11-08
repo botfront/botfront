9541c36dc3b259ad327d51aecaf40ea2334f46f0
Kenton Rilea <kenton.rilea@mrbot.ai>
2019-11-08 17:28:52 -0500

    fix: persist state test clicks too fast

--- botfront/cypress/integration/stories/persist-state.spec.js
+++ botfront/cypress/integration/stories/persist-state.spec.js
@@ -23,13 +23,28 @@ describe('stories state persisting', function() {
         cy.dataCy('add-item-input')
             .find('input')
             .type(`${storyGroupOne}{enter}`);
+        cy.dataCy('browser-item')
+            .should('have.length', 3);
+        cy.dataCy('browser-item')
+            .last()
+            .should('have.class', 'active');
         cy.dataCy('add-item').click({ force: true });
         cy.dataCy('add-item-input')
             .find('input')
             .type(`${storyGroupTwo}{enter}`);
+        cy.dataCy('browser-item')
+            .should('have.length', 4);
+        cy.dataCy('browser-item')
+            .last()
+            .should('have.class', 'active');
         cy.wait(500);
         cy.contains('NLU').click({ force: true });
+        cy.dataCy('settings-in-model')
+            .should('exist');
         cy.contains('Stories').click({ force: true });
+        cy.wait(500);
+        cy.dataCy('browser-item')
+            .should('have.length', 4);
         cy.get('[data-cy=browser-item].active.item').contains(storyGroupTwo);
     });
 });
