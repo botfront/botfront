592813672337ff4805f92f653cae40941fe254cc
Matthieu Joannon <mattjoannon@gmail.com>
2019-11-08 17:57:53 -0500

    refactor: added trace to redux dev tools

--- botfront/imports/ui/store/store.js
+++ botfront/imports/ui/store/store.js
@@ -4,7 +4,7 @@ import reducer from './reducers/reducer';
 const store = createStore(
     reducer,
     // eslint-disable-next-line no-underscore-dangle
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true }),
 );
 
 export default store;
