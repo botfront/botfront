25f57bf6d7842ab6080224a242d17afcab1f9d59
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-11-27 13:42:04 -0500

    refactor: add create read and update mutations

--- botfront/imports/api/graphql/botResponses/mongo/botResponses.js
+++ botfront/imports/api/graphql/botResponses/mongo/botResponses.js
@@ -1,4 +1,34 @@
+import { dump as yamlDump, safeLoad as yamlLoad, safeDump } from 'js-yaml';
 import BotResponses from '../botResponses.model';
+
+const formatNewlines = (sequence) => {
+    const regexSpacedNewline = / {2}\n/g;
+    const regexNewline = /\n/g;
+    const updatedSequence = sequence.map(({ content: contentYaml }) => {
+        const content = yamlLoad(contentYaml);
+        if (content.text) {
+            content.text = content.text
+                .replace(regexSpacedNewline, '\n')
+                .replace(regexNewline, '  \n');
+        }
+        return { content: yamlDump({ ...content }) };
+    });
+    return updatedSequence;
+};
+
+const formatTextOnSave = values => values.map((item) => {
+    const updatedItem = item;
+    updatedItem.sequence = formatNewlines(item.sequence);
+    return updatedItem;
+});
+
+export const updateResponse = async (projectId, key, newResponse) => {
+    const formatedResponse = { ...newResponse, values: formatTextOnSave(newResponse.values) };
+    return BotResponses.findOneAndUpdate({ projectId, key }, formatedResponse).exec();
+};
+export const createResponse = async (projectId, newResponse) => BotResponses.create({ ...newResponse, projectId, _id: newResponse.key });
+export const deleteResponse = async (projectId, key) => BotResponses.deleteOne({ projectId, key });
+
 
 export const getBotResponses = async projectId => (BotResponses.find(
     {
@@ -7,8 +37,24 @@ export const getBotResponses = async projectId => (BotResponses.find(
 ).lean());
 
 
-export const getBotResponse = async key => (BotResponses.findOne(
-    {
-        key,
-    },
-).lean());
+export const getBotResponse = async (projectId, key, lang = 'en') => {
+    let botResponse = await BotResponses.findOne(
+        {
+            projectId, key,
+        },
+    ).lean();
+    const newSeq = {
+        sequence: [{ content: safeDump({ text: key }) }],
+        lang,
+    };
+    if (!botResponse) {
+        botResponse = { key, values: [newSeq] };
+        await createResponse(projectId, botResponse);
+        return botResponse;
+    }
+    if (!botResponse.values.some(v => v.lang === lang)) {
+        botResponse.values.push(newSeq);
+        await updateResponse(projectId, key, botResponse);
+    }
+    return botResponse;
+};
