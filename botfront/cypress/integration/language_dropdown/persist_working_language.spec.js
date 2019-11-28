58b0544d1739b0ce2f4bbbaa193886e9b50e7ee8
Kenton <kenton.rilea@mrbot.ai>
2019-11-28 15:51:00 -0500

    test: remove incoming from redux language tests

--- botfront/cypress/integration/language_dropdown/persist_working_language.spec.js
+++ botfront/cypress/integration/language_dropdown/persist_working_language.spec.js
@@ -31,19 +31,20 @@ describe('redux working language', function() {
         /* sidebar links are used over cy.vist as cy.visit clears the
            redux state causing the test to fail
         */
+        // add a language
         cy.visit('/project/bf/settings');
         cy.get('[data-cy=language-selector]').click();
         cy.get('[data-cy=language-selector] input').type('French{enter}');
         cy.get('.project-settings-menu-info').click();
         cy.get('[data-cy=save-changes]').click();
-
+        // add an English example
         cy.visit('/project/bf/nlu/models');
         cy.contains('Insert many').click();
         cy.get('.batch-insert-input').type('cya\nlater');
         cy.get('.purple > .ui').click();
         cy.get('.purple > .ui > .search').type('newintent{enter}');
         cy.get('[data-cy=save-button]').click();
-        // ////////////////////////////////////
+        // the active language in NLU should match the active language in stories
         cy.visit('/project/bf/stories');
         selectLanguage('French');
         checkLanguage('French');
@@ -55,27 +56,14 @@ describe('redux working language', function() {
         checkLanguage('French');
         cy.dataCy('intent-label')
             .should('not.exist');
-        // ///////////////////////
+        // switch the language back to english in nlu
         selectLanguage('English');
         checkLanguage('English');
         cy.dataCy('intent-label')
             .should('exist');
+        // the language in stories should be english
         cy.dataCy('stories-sidebar-link')
             .click({ force: true });
         checkLanguage('English');
-        cy.dataCy('incoming-sidebar-link')
-            .click({ force: true });
-        checkLanguage('English');
-        // //////////////////////
-        selectLanguage('French');
-        checkLanguage('French');
-        cy.dataCy('stories-sidebar-link')
-            .click({ force: true });
-        checkLanguage('French');
-        cy.dataCy('nlu-sidebar-link')
-            .click({ force: true });
-        checkLanguage('French');
-        cy.dataCy('intent-label')
-            .should('not.exist');
     });
 });
