9d79142c7475a3b948ff65b7613093a2b1eef75e
Guillaume TECHER <guillaumeval.techer@gmail.com>
2019-11-29 12:12:17 -0500

    refactor: removes unused code after migration from meteor to GraphQL

--- botfront/cypress/support/index.js
+++ botfront/cypress/support/index.js
@@ -27,30 +27,30 @@ Cypress.Screenshot.defaults({
 });
 
 switch (Cypress.env('abort_strategy')) {
-case 'run':
-    // eslint-disable-next-line no-undef
-    before(function onBeforeEach() {
-    // Skips any subsequent specs, if the run has been flagged as failed
-        cy.getCookie('has_failed_test').then((cookie) => {
-            if (cookie && typeof cookie === 'object' && cookie.value === 'true') {
+    case 'run':
+        // eslint-disable-next-line no-undef
+        before(function onBeforeEach() {
+            // Skips any subsequent specs, if the run has been flagged as failed
+            cy.getCookie('has_failed_test').then((cookie) => {
+                if (cookie && typeof cookie === 'object' && cookie.value === 'true') {
+                    Cypress.runner.stop();
+                }
+            });
+        });
+    /* fallthrough */
+    case 'spec':
+        afterEach(function onAfterEach() {
+            // Skips all subsequent tests in a spec, and flags the whole run as failed
+            if (this.currentTest.state === 'failed') {
+                cy.setCookie('has_failed_test', 'true');
                 Cypress.runner.stop();
             }
         });
-    });
-/* fallthrough */
-case 'spec':
-    afterEach(function onAfterEach() {
-    // Skips all subsequent tests in a spec, and flags the whole run as failed
-        if (this.currentTest.state === 'failed') {
-            cy.setCookie('has_failed_test', 'true');
-            Cypress.runner.stop();
-        }
-    });
-    Cypress.Cookies.defaults({
-        whitelist: 'has_failed_test',
-    });
-    break;
-default:
+        Cypress.Cookies.defaults({
+            whitelist: 'has_failed_test',
+        });
+        break;
+    default:
 }
 
 Cypress.Commands.add('login', (visit = true, email = 'test@test.com', password = 'Aaaaaaaa00') => {
@@ -134,7 +134,7 @@ Cypress.Commands.add('MeteorCall', (method, args) => {
 //             cy.fixture('nlu_import.json', 'utf8').then((content) => {
 //                 cy.get('.file-dropzone').upload(content, 'data.json');
 //             });
-        
+
 //             cy.contains('Import Training Data').click();
 //         });
 // });
@@ -178,16 +178,6 @@ Cypress.Commands.add('createResponse', (projectId, responseName) => {
     cy.get('.response-save-button').click();
 });
 
-Cypress.Commands.add('createResponseFast', (projectId, responseName) => {
-    cy.window().then(({ Meteor }) => Meteor.call(
-        'project.insertTemplate',
-        projectId,
-        {
-            key: responseName,
-            values: [{ sequence: [], lang: 'en' }, { sequence: [], lang: 'fr' }],
-        },
-    ));
-});
 
 Cypress.Commands.add('openResponse', (projectId, responseName) => {
     cy.visit(`/project/${projectId}/dialogue/templates`);
@@ -203,14 +193,6 @@ Cypress.Commands.add('deleteResponse', (projectId, responseName) => {
     cy.get('[style="flex: 200 0 auto; width: 200px; max-width: 200px;"] > input').clear();
     cy.get('[style="flex: 200 0 auto; width: 200px; max-width: 200px;"] > input').type(responseName);
     cy.get('[data-cy=remove-response-0]').click();
-});
-
-Cypress.Commands.add('deleteResponseFast', (projectId, key) => {
-    cy.window().then(({ Meteor }) => Meteor.call(
-        'project.deleteTemplate',
-        projectId,
-        key,
-    ));
 });
 
 Cypress.Commands.add('deleteProject', projectId => cy.visit('/')
@@ -255,7 +237,7 @@ Cypress.Commands.add(
     },
 );
 
-Cypress.Commands.add('waitForResolve', (url, maxTries = 1000) => new Cypress.Promise(async function(resolve, reject) {
+Cypress.Commands.add('waitForResolve', (url, maxTries = 1000) => new Cypress.Promise(async function (resolve, reject) {
     for (let i = 1; i < Number.MAX_VALUE; i += 1) {
         try {
             await axios(url);
--- botfront/imports/api/project/response.methods.js
+++ botfront/imports/api/project/response.methods.js
@@ -13,111 +13,9 @@ export const getTemplateLanguages = (templates) => {
     return sortBy(langs);
 };
 
-Meteor.methods({
-    'project.updateTemplate'(projectId, key, item) {
-        check(projectId, String);
-        check(key, String);
-        check(item, Object);
-
-        const formattedItem = item;
-        formattedItem.values = formatTextOnSave(formattedItem.values);
-
-        try {
-            return Projects.update(
-                { _id: projectId, 'templates.key': key },
-                { $set: { 'templates.$': formattedItem, responsesUpdatedAt: Date.now() } },
-            );
-        } catch (e) {
-            throw new Meteor.Error(e);
-        }
-    },
-
-    'project.deleteTemplate'(projectId, key, lang = 'en') {
-        check(projectId, String);
-        check(lang, String);
-        check(key, String);
-        
-        try {
-            return Projects.update(
-                { _id: projectId, 'templates.key': key },
-                { $pull: { templates: { key } }, $set: { responsesUpdatedAt: Date.now() } },
-            );
-        } catch (e) {
-            throw new Meteor.Error(e);
-        }
-    },
-});
 
 if (Meteor.isServer) {
     Meteor.methods({
-        'project.findTemplate'(projectId, key, lang = 'en') {
-            check(projectId, String);
-            check(key, String);
-            check(lang, String);
-
-            try {
-                let template = Projects.findOne(
-                    {
-                        _id: projectId,
-                        templates: { $elemMatch: { key } },
-                    },
-                    {
-                        fields: {
-                            templates: { $elemMatch: { key } },
-                        },
-                    },
-                );
-                const newSeq = {
-                    sequence: [{ content: safeDump({ text: key }) }],
-                    lang,
-                };
-                if (!template) {
-                    template = { key, values: [newSeq] };
-                    Projects.update(
-                        { _id: projectId },
-                        {
-                            $push: { templates: template },
-                            $set: { responsesUpdatedAt: Date.now() },
-                        },
-                    );
-                    return template;
-                }
-                if (!template.templates[0].values.some(v => v.lang === lang)) {
-                    template.templates[0].values.push(newSeq);
-                    Projects.update(
-                        { _id: projectId, 'templates.key': key },
-                        {
-                            $push: { 'templates.$.values': newSeq },
-                            $set: { responsesUpdatedAt: Date.now() },
-                        },
-                    );
-                }
-                return template.templates[0];
-            } catch (e) {
-                throw new Meteor.Error(e);
-            }
-        },
-
-        'project.insertTemplate'(projectId, item) {
-            check(projectId, String);
-            check(item, Object);
-
-            const { key } = item;
-            const { templates } = Projects.findOne({ _id: projectId }, { fields: { templates: 1 } });
-
-            if (find(templates, { key })) {
-                throw new Meteor.Error('template-collision', `Can not add template because one already exists with the key ${key}`);
-            }
-
-            try {
-                Projects.update(
-                    { _id: projectId },
-                    { $push: { templates: item }, $set: { responsesUpdatedAt: Date.now() } },
-                );
-            } catch (e) {
-                throw new Meteor.Error(e);
-            }
-        },
 
         'templates.download'(projectId) {
             check(projectId, String);
@@ -146,26 +44,10 @@ if (Meteor.isServer) {
             }
         },
 
-        'templates.removeByKey'(projectId, arg) {
-            check(projectId, String);
-            check(arg, Match.OneOf(String, [String]));
-
-            const templateKeys = (typeof arg === 'string') ? [arg] : arg;
-
-            try {
-                Projects.update(
-                    { _id: projectId },
-                    { $pull: { templates: { key: { $in: templateKeys } } }, $set: { responsesUpdatedAt: Date.now() } },
-                );
-            } catch (e) {
-                throw formatError(e);
-            }
-        },
-
         'templates.countWithIntent'(projectId, intent) {
             check(projectId, String);
             check(intent, String);
-            
+
             const project = Projects.find(
                 { _id: projectId },
                 {
