a106ae9b6d4d54040460a866b7b4511a642dd352
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-12-01 14:31:26 -0500

    fix: import project error message not displayed

--- botfront/imports/api/importExport/import.methods.js
+++ botfront/imports/api/importExport/import.methods.js
@@ -9,21 +9,17 @@ import { generateErrorText, generateImportResponse } from './importExport.utils'
 
 if (Meteor.isServer) {
     Meteor.methods({
-        'importProject'(projectFile, apiHost, projectId) {
+        async 'importProject'(projectFile, apiHost, projectId) {
             check(projectFile, Object);
             check(apiHost, String);
             check(projectId, String);
 
-            const importRequest = axios.put(
-                `${apiHost}/project/${projectId}/import`,
-                projectFile,
-            )
-                .then(res => (generateImportResponse(res.status)))
-                .catch(err => (
-                    { error: { header: 'Import Failed', text: generateErrorText(err) } }
-                ));
-
-            return importRequest;
+            try {
+                const res = await axios.put(`${apiHost}/project/${projectId}/import`, projectFile);
+                return generateImportResponse(res.status);
+            } catch (err) {
+                throw new Meteor.Error(generateErrorText(err));
+            }
         },
     });
 }
--- botfront/imports/ui/components/settings/ImportProject.jsx
+++ botfront/imports/ui/components/settings/ImportProject.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { withTracker } from 'meteor/react-meteor-data';
 import { saveAs } from 'file-saver';
 
-
 import {
     Dropdown, Button, Message, Icon, Confirm,
 } from 'semantic-ui-react';
@@ -55,12 +54,12 @@ const ImportProject = ({
 
     const importProject = () => {
         setLoading(true);
-        Meteor.call('importProject', uploadedFiles.botfront, apiHost, projectId, (error, { success, errorMessage }) => {
-            if (success === true) {
+        Meteor.call('importProject', uploadedFiles.botfront, apiHost, projectId, (err) => {
+            if (!err) {
                 setImportSuccessful(true);
             } else {
                 setImportSuccessful(false);
-                setImportErrorMessage(!!errorMessage ? errorMessage : importErrorMessage);
+                setImportErrorMessage({ header: 'Import failed', text: err.error });
             }
             setLoading(false);
         });
@@ -136,109 +135,109 @@ const ImportProject = ({
     }
 
     return (
-            <>
-                <Dropdown
-                    data-cy='import-type-dropdown'
-                    key='format'
-                    className='export-option'
-                    options={importTypeOptions.map(({ value, key, text }) => ({ value, key, text }))}
-                    placeholder='Select a format'
-                    selection
-                    onChange={(x, { value }) => {
-                        setImportType(importTypeOptions.find(options => options.value === value));
-                    }}
+        <>
+            <Dropdown
+                data-cy='import-type-dropdown'
+                key='format'
+                className='export-option'
+                options={importTypeOptions.map(({ value, key, text }) => ({ value, key, text }))}
+                placeholder='Select a format'
+                selection
+                onChange={(x, { value }) => {
+                    setImportType(importTypeOptions.find(options => options.value === value));
+                }}
+            />
+            <br />
+            {importType.value === 'botfront' && !botfrontFileSuccess && (
+                <ImportDropField
+                    onChange={fileAdded}
+                    text='Drop your Botfront project in JSON format here.'
+                    manipulateData={JSON.parse}
+                    verifyData={verifyBotfrontFile}
+                    success={botfrontFileSuccess}
+                    fileTag='botfront'
+                    successMessage='Your Botfront project file is ready.'
                 />
-                <br />
-                {importType.value === 'botfront' && !botfrontFileSuccess && (
-                    <ImportDropField
-                        onChange={fileAdded}
-                        text='Drop your Botfront project in JSON format here.'
-                        manipulateData={JSON.parse}
-                        verifyData={verifyBotfrontFile}
-                        success={botfrontFileSuccess}
-                        fileTag='botfront'
-                        successMessage='Your Botfront project file is ready.'
-                    />
-                )}
-                {backupSuccess === true && (
-                    <p className='plain-text-message'>
-                        If the backup download did not automatically start after 10 seconds, click
-                        <a
-                            href={`${apiHost}/project/${projectId}/export?output=json&conversations=${includeConvos}`}
-                            data-cy='backup-link'
-                        > here
-                        </a> to retry.
-                        <br />Please verify that the backup has downloaded before continuing.
-                    </p>
-                )}
-                {(backupSuccess === false && (
-                    <Message
-                        error
-                        icon='times circle'
-                        header='Backup Failed'
-                        content={backupErrorMessage.text}
-                    />
-                ))}
-                {backupSuccess === 'skipped' && (
-                    <Message
-                        warning
-                        icon='exclamation circle'
-                        header='Warning!'
-                        data-cy='skiped-backup-warning'
-                        content={(
-                            <>
-                                All your current project data will be permanently overwritten and erased when you click
-                                <b> Import Botfront Project. </b>
-                            </>
-                        )}
-                    />
-                )}
-                {backupSuccess === undefined && botfrontFileSuccess && (
-                    <Message
-                        warning
-                        icon='exclamation circle'
-                        header='Your project will be overwritten.'
-                        content='It is highly advised to download a backup of your current project before importing a new one.'
-                    />
-                )}
-                { backupSuccess === undefined && botfrontFileSuccess && (
-                    <>
-                        <Button.Group>
-                            <Button onClick={() => { backupProject(true); setIncludeConvos(true); }} className='export-option' data-cy='export-with-conversations'>
-                                Download backup with conversations
-                            </Button>
-                            <Button.Or />
-                            <Button onClick={() => { backupProject(false); setIncludeConvos(false); }} className='export-option' data-cy='export-without-conversations'>
-                                Download backup without conversations
-                            </Button>
-                            <Button.Or />
-                            <Button onClick={() => setConfirmSkipOpen(true)} className='export-option' data-cy='skip' negative>Skip</Button>
-                            <Confirm
-                                header='Are you sure you want to skip backing up your project?'
-                                content='All your current project data will be permanently overwritten and erased.'
-                                open={confirmSkipOpen}
-                                onCancel={() => setConfirmSkipOpen(false)}
-                                onConfirm={() => {
-                                    setbackupSuccess('skipped');
-                                    setConfirmSkipOpen(false);
-                                }}
-                            />
-                        </Button.Group>
-                        <br />
-                    </>
-                )}
-                {validateImportType() && (
-                    <Button
-                        onClick={importProject}
-                        disabled={!backupSuccess}
-                        className='export-option'
-                        data-cy='import-button'
-                    >
-                        <Icon name='upload' />
-                        {importButtonText()}
-                    </Button>
-                )}
-            </>
+            )}
+            {backupSuccess === true && (
+                <p className='plain-text-message'>
+                    If the backup download did not automatically start after 10 seconds, click
+                    <a
+                        href={`${apiHost}/project/${projectId}/export?output=json&conversations=${includeConvos}`}
+                        data-cy='backup-link'
+                    > here
+                    </a> to retry.
+                    <br />Please verify that the backup has downloaded before continuing.
+                </p>
+            )}
+            {(backupSuccess === false && (
+                <Message
+                    error
+                    icon='times circle'
+                    header='Backup Failed'
+                    content={backupErrorMessage.text}
+                />
+            ))}
+            {backupSuccess === 'skipped' && (
+                <Message
+                    warning
+                    icon='exclamation circle'
+                    header='Warning!'
+                    data-cy='skiped-backup-warning'
+                    content={(
+                        <>
+                            All your current project data will be permanently overwritten and erased when you click
+                            <b> Import Botfront Project. </b>
+                        </>
+                    )}
+                />
+            )}
+            {backupSuccess === undefined && botfrontFileSuccess && (
+                <Message
+                    warning
+                    icon='exclamation circle'
+                    header='Your project will be overwritten.'
+                    content='It is highly advised to download a backup of your current project before importing a new one.'
+                />
+            )}
+            { backupSuccess === undefined && botfrontFileSuccess && (
+                <>
+                    <Button.Group>
+                        <Button onClick={() => { backupProject(true); setIncludeConvos(true); }} className='export-option' data-cy='export-with-conversations'>
+                            Download backup with conversations
+                        </Button>
+                        <Button.Or />
+                        <Button onClick={() => { backupProject(false); setIncludeConvos(false); }} className='export-option' data-cy='export-without-conversations'>
+                            Download backup without conversations
+                        </Button>
+                        <Button.Or />
+                        <Button onClick={() => setConfirmSkipOpen(true)} className='export-option' data-cy='skip' negative>Skip</Button>
+                        <Confirm
+                            header='Are you sure you want to skip backing up your project?'
+                            content='All your current project data will be permanently overwritten and erased.'
+                            open={confirmSkipOpen}
+                            onCancel={() => setConfirmSkipOpen(false)}
+                            onConfirm={() => {
+                                setbackupSuccess('skipped');
+                                setConfirmSkipOpen(false);
+                            }}
+                        />
+                    </Button.Group>
+                    <br />
+                </>
+            )}
+            {validateImportType() && (
+                <Button
+                    onClick={importProject}
+                    disabled={!backupSuccess}
+                    className='export-option'
+                    data-cy='import-button'
+                >
+                    <Icon name='upload' />
+                    {importButtonText()}
+                </Button>
+            )}
+        </>
     );
 };
 
