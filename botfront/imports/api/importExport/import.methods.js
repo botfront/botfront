d38782ccb5d72cc4ae1a980849f532bf78bded2c
Matthieu Joannon <mattjoannon@gmail.com>
2019-12-06 16:22:05 -0500

    fix: import project error message not displayed

--- botfront/imports/api/importExport/import.methods.js
+++ botfront/imports/api/importExport/import.methods.js
@@ -1,4 +1,3 @@
-
 import { Meteor } from 'meteor/meteor';
 import axios from 'axios';
 
@@ -6,25 +5,23 @@ import { check } from 'meteor/check';
 
 import { generateErrorText, generateImportResponse } from './importExport.utils';
 
-
 if (Meteor.isServer) {
     Meteor.methods({
-        'importProject'(projectFile, apiHost, projectId) {
+        async importProject(projectFile, apiHost, projectId) {
             check(projectFile, Object);
             check(apiHost, String);
             check(projectId, String);
 
-            const importRequest = axios.put(
-                `${apiHost}/project/${projectId}/import`,
-                projectFile,
-                { maxContentLength: 100000000 },
-            )
-                .then(res => (generateImportResponse(res.status)))
-                .catch(err => (
-                    { error: { header: 'Import Failed', text: generateErrorText(err) } }
-                ));
-
-            return importRequest;
+            try {
+                const res = axios.put(
+                    `${apiHost}/project/${projectId}/import`,
+                    projectFile,
+                    { maxContentLength: 100000000 },
+                );
+                return generateImportResponse(res.status);
+            } catch (err) {
+                throw new Meteor.Error(generateErrorText(err));
+            }
         },
     });
 }
