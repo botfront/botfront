412a26b176333dbadf63b51fa2117769bbc76482
Matthieu Joannon <mattjoannon@gmail.com>
2019-12-11 11:37:16 -0500

    refactor: fix test and build

--- botfront/cypress/build_and_test.sh
+++ botfront/cypress/build_and_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker build -t botfront-local ../../.
-docker build -t botfront-api-local ../../.
+docker build -t botfront-api-local ../../api/.
 docker-compose up --abort-on-container-exit --exit-code-from e2e
