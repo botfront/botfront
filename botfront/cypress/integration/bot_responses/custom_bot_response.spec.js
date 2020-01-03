15c834a9b96266409289974d0605d414db2b72ff
kenrilea <kenton.rilea@mrbot.ai>
2020-01-03 10:46:36 -0500

    test: custom bot responses

---
+++ botfront/cypress/integration/bot_responses/custom_bot_response.spec.js
@@ -0,0 +1,40 @@
+/* global cy:true */
+
+describe('Bot responses', function() {
+    beforeEach(function() {
+        cy.deleteProject('bf')
+        cy.createProject('bf', 'My Project', 'en').then(
+            () => cy.createNLUModelProgramatically('bf', '', 'de')
+        )
+        cy.login();
+    });
+
+    afterEach(function() {
+    });
+    it('should create a custom response using the response editor', function() {
+        cy.visit('/project/bf/dialogue/templates')
+        cy.dataCy('create-response').click();
+        cy.dataCy('add-custom-response').click();
+        cy.dataCy('response-name-input').click().find('input').type('test_A');
+        cy.dataCy('custom-response-editor').click().find('textarea').type('test: success');
+        cy.get('.dimmer').click({ position: 'topLeft'}) // close the response editor
+        cy.dataCy('template-intent').contains('utter_test_A').should("exist");
+        cy.dataCy('edit-response-0').click();
+        cy.dataCy('custom-response-editor').contains('test: success').should('exist');
+    });
+    it('should edit a custom response using the response editor', function() {
+        cy.visit('/project/bf/dialogue/templates')
+        cy.dataCy('create-response').click();
+        cy.dataCy('add-custom-response').click();
+        cy.dataCy('response-name-input').click().find('input').type('test_A');
+        cy.dataCy('custom-response-editor').click().find('textarea').type('test: success');
+        cy.get('.dimmer').click({ position: 'topLeft'}) // close the response editor
+        cy.dataCy('template-intent').contains('utter_test_A').should("exist");
+
+        cy.dataCy('edit-response-0').click();
+        cy.dataCy('custom-response-editor').click().find('textarea').clear().type('success: true');
+        cy.get('.dimmer').click({ position: 'topLeft'}) // close the response editor
+        cy.dataCy('edit-response-0').click();
+        cy.dataCy('custom-response-editor').contains('success: true').should('exist');
+    });
+});
