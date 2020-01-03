34d1fefcd20217c70392a80c367d8ad31fa10f7f
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-01-03 14:27:35 -0500

    style: white glowbox everywhere

--- botfront/imports/ui/components/nlu/models/NLUModel.jsx
+++ botfront/imports/ui/components/nlu/models/NLUModel.jsx
@@ -334,7 +334,6 @@ class NLUModel extends React.Component {
                                     />
                                 </div>
                             )}
-                            <br />
                         </>
                     )}
                     <br />
