133dd0d3b6e5677f9ab8c22f706dee9a7f6ea82c
kenrilea <kenton.rilea@mrbot.ai>
2020-01-09 14:03:02 -0500

    test: variations

--- botfront/imports/ui/components/templates/templates-list/BotResponseEditor.jsx
+++ botfront/imports/ui/components/templates/templates-list/BotResponseEditor.jsx
@@ -239,7 +239,7 @@ const BotResponseEditor = (props) => {
                         </div>
                         <div className='response-editor-topbar-section'>
                             <Menu pointing secondary activeIndex={activeTab}>
-                                <MenuItem onClick={() => { setActiveTab(0); }} active={activeTab === 0} className='response-variations' data-cy='variations-tab'>Response</MenuItem>
+                                <MenuItem onClick={() => { setActiveTab(0); }} active={activeTab === 0} className='response-variations' data-cy='variations-tab'>Variations</MenuItem>
                                 <MenuItem onClick={() => { setActiveTab(1); }} active={activeTab === 1} className='metadata' data-cy='metadata-tab'>Metadata</MenuItem>
                             </Menu>
                         </div>
