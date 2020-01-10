715fa357108e01042f03ca06aac9d42d846d3151
kenrilea <kenton.rilea@mrbot.ai>
2020-01-10 13:01:35 -0500

    test: fix bot_response_consistency tests

--- botfront/cypress/integration/bot_responses/bot_response_consistency.spec.js
+++ botfront/cypress/integration/bot_responses/bot_response_consistency.spec.js
@@ -16,6 +16,21 @@ describe('Bot responses', function() {
         cy.deleteProject('bf');
     });
 
+    const createPersistedResponse = () => {
+        cy.dataCy('add-story').click();
+        cy.dataCy('from-text-template').last().click({ force: true }).parents('[data-cy=single-story-editor]')
+            .find('[data-cy=bot-response-input]')
+            .should('exist');
+
+        cy.dataCy('bot-response-input').last().click()
+            .find('textarea')
+            .clear()
+            .type('persistent response');
+
+        cy.dataCy('delete-story').first().click();
+        cy.dataCy('confirm-yes').click({ force: true });
+    };
+
 
     it('Should delete an existing response from the project when it is deleted in a story', function() {
         cy.visit('/project/bf/stories');
@@ -31,11 +46,12 @@ describe('Bot responses', function() {
         cy.dataCy('bot-response-input')
             .find('textarea')
             .clear()
-            .type('test delete response default');
-        
+            .type('test delete response default')
+            .blur();
         cy.dataCy('icon-trash')
             .click({ force: true });
         cy.dataCy('bot-response-input').should('not.exist');
+        createPersistedResponse();
         cy.visit('/project/bf/dialogue/templates');
         cy.dataCy('response-text').contains('test delete response default').should('not.exist');
     });
@@ -59,6 +75,13 @@ describe('Bot responses', function() {
         // add a story so that the story group is not deleted
         // ensuring that this test only tests deleting responses when a story is deleted
         cy.dataCy('add-story').click();
+        cy.dataCy('from-text-template').last().click({ force: true });
+        cy.dataCy('bot-response-input').should('have.length', 2);
+        cy.dataCy('bot-response-input').last().click()
+            .find('textarea')
+            .clear()
+            .type('persistent response');
+
         cy.dataCy('delete-story').first().click();
         cy.dataCy('confirm-yes').click({ force: true });
         
@@ -91,6 +114,8 @@ describe('Bot responses', function() {
             .findCy('delete-menu')
             .click({ force: true });
         cy.get('.ui.primary.button').contains('Delete').click();
+
+        createPersistedResponse();
      
         cy.visit('/project/bf/dialogue/templates');
         cy.dataCy('response-text').contains('delete storyGroup test response').should('not.exist');
