e317597f1a2f97cd57922c160471c1d0592ed2e4
Kenton <kenton.rilea@mrbot.ai>
2020-01-13 14:15:25 -0500

    refactor: widgetRules schema

--- botfront/imports/ui/components/widget_rules/WidgetRulesForm.jsx
+++ botfront/imports/ui/components/widget_rules/WidgetRulesForm.jsx
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 import { GraphQLBridge } from 'uniforms-bridge-graphql';
 import { buildASTSchema, parse } from 'graphql';
 import {
-    AutoForm, AutoField, ErrorsField,
+    AutoForm, AutoField, ErrorsField, SubmitField,
 } from 'uniforms-semantic';
 import { Segment } from 'semantic-ui-react';
 import ToggleField from '../common/ToggleField';
-
 
 // force open affect force close and vice versa
 class AutoFormWidgetRules extends AutoForm {
@@ -31,10 +30,12 @@ function WidgetRulesForm({
 }) {
     const schema = buildASTSchema(parse(`
         type WidgetRules {
-            test: Boolean!
             payload: String!
-            text: String!
-            trigger: [String]
+            text: String
+            trigger: Trigger!
+        }
+
+        type Trigger {
             timeOnPage: Float
             numberOfVists: Int
             numberOfPageVisits: Int
@@ -72,16 +73,13 @@ function WidgetRulesForm({
     };
 
     const schemaData = {
-        test: {
-            label: 'this is a test',
-            defaultValue: false,
-        },
         payload: {
-            label: 'Payload',
+            label: 'The name of the trigger payload, used to trigger related stories',
             defaultValue: '',
+            placeholder: 'payload name',
         },
         text: {
-            label: 'text',
+            label: 'content of the trigger',
             defaultValue: '',
         },
         trigger: {
@@ -110,28 +108,22 @@ function WidgetRulesForm({
 
     const displayModel = WidgetRules || defaultModel;
     return (
-        <Segment>
-            <div className='response-metadata-form'>
-                {/* We don't need validation as fields are optionals and there is no user input */}
-                <AutoFormWidgetRules autosave model={displayModel} schema={new GraphQLBridge(schema, () => { }, schemaData)} onSubmit={model => onChange(model)}>
-                    <ToggleField
-                        name='test'
-                        className='toggle'
-                    />
-                    <AutoField name='payload' />
-                    <AutoField name='text' />
-                    <AutoField name='trigger' />
-                    <AutoField name='timeOnPage' />
-                    <AutoField name='numberOfPageVisits' />
-                    <AutoField name='device' />
-                    <AutoField name='queryString' />
-                    <AutoField name='eventListeners' />
-                    <br />
-                    <ErrorsField />
-                    <br />
-                </AutoFormWidgetRules>
-            </div>
-        </Segment>
+        <div style={{ position: 'absolute', margin: '0px', top: '50px' }}>
+            <Segment>
+                <div className='response-metadata-form'>
+                    {/* We don't need validation as fields are optionals and there is no user input */}
+                    <AutoFormWidgetRules autosave model={displayModel} schema={new GraphQLBridge(schema, () => { }, schemaData)} onSubmit={model => onChange(model)}>
+                        <AutoField name='payload' />
+                        <AutoField name='text' />
+                        <AutoField name='trigger' />
+                        <br />
+                        <ErrorsField />
+                        <br />
+                        <SubmitField value='Save' />
+                    </AutoFormWidgetRules>
+                </div>
+            </Segment>
+        </div>
     );
 }
 
