db314b9a042826710d0f5b690425e359f772e706
kenrilea <kenton.rilea@mrbot.ai>
2020-01-14 16:09:52 -0500

    refactor: fix image test

--- botfront/cypress/integration/bot_responses/image_bot_response.spec.js
+++ botfront/cypress/integration/bot_responses/image_bot_response.spec.js
@@ -20,6 +20,7 @@ describe('Bot responses', function() {
         cy.dataCy('add-image-response').click();
         cy.dataCy('response-name-input').click().find('input').type('test_A');
         cy.dataCy('image-url-input').find('input').type(imageUrlA);
+        cy.wait(500);
         cy.get('.dimmer').click({ position: 'topLeft' }); // close the response editor
         cy.dataCy('template-intent').contains('utter_test_A').should('exist');
         cy.dataCy('response-text').find('img').should('have.attr', 'src').and('equal', imageUrlA);
@@ -32,6 +33,7 @@ describe('Bot responses', function() {
         cy.dataCy('image-url-input').find('input').type(imageUrlA).blur();
         cy.dataCy('add-variation').click();
         cy.dataCy('image-url-input').last().find('input').type(imageUrlB);
+        cy.wait(500);
         cy.get('.dimmer').click({ position: 'topLeft' }); // close the response editor
         cy.dataCy('template-intent').contains('utter_test_A').should('exist');
         cy.dataCy('response-text').find('img').first().should('have.attr', 'src')
@@ -46,16 +48,21 @@ describe('Bot responses', function() {
         cy.dataCy('response-name-input').click().find('input').type('test_A');
         cy.dataCy('image-url-input').find('input').type(imageUrlA).blur();
         cy.dataCy('add-variation').click();
-        cy.dataCy('image-url-input').last().find('input').type(imageUrlB);
+        cy.dataCy('image-url-input').last().find('input').type(imageUrlB)
+            .blur();
+        cy.wait(500);
         cy.get('.dimmer').click({ position: 'topLeft' }); // close the response editor
         cy.get('.dimmer').should('not.exist');
+        cy.wait(250);
 
         cy.dataCy('edit-response-0').click();
         cy.dataCy('icon-trash').first().click();
         cy.dataCy('icon-trash').first().click();
         cy.dataCy('image-url-input').find('input').type(imageUrlB).blur();
+        cy.wait(500);
         cy.get('.dimmer').click({ position: 'topLeft' }); // close the response editor
         cy.get('.dimmer').should('not.exist');
+        cy.wait(250);
 
         cy.dataCy('response-text').find('img').first().should('have.attr', 'src')
             .and('equal', imageUrlB);
