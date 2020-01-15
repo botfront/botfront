747aca27c258326c4de367a3702a2c1c8d3fa244
Kenton <kenton.rilea@mrbot.ai>
2020-01-15 11:51:32 -0500

    refactor: multiple triggers per story

--- botfront/imports/ui/components/DisplayIf.jsx
+++ botfront/imports/ui/components/DisplayIf.jsx
@@ -1,11 +1,11 @@
+import { Children } from 'react';
 import BaseField from 'uniforms/BaseField';
 import nothing from 'uniforms/nothing';
-import { Children } from 'react';
 
 // We have to ensure that there's only one child, because returning an array
 // from a component is prohibited.
-const DisplayIf = ({ children, condition }, { uniforms }) => (condition(uniforms) ? Children.only(children) : nothing);
-DisplayIf.contextTypes = BaseField.contextTypes;
 
+const DisplayIf = ({ children, condition, name }, { uniforms }) => (condition(uniforms, name) ? Children.only(children[0]) : nothing);
+DisplayIf.contextTypes = BaseField.contextTypes;
 
 export default DisplayIf;
