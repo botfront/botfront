e50b308f0ea71b2f86d1ffbc5a52faa884c51f36
Kenton <kenton.rilea@mrbot.ai>
2020-01-16 10:48:15 -0500

    feat: stories: create and edit trigger conditions

--- botfront/imports/ui/components/stories/StoryTriggerEditor.jsx
+++ botfront/imports/ui/components/stories/StoryTriggerEditor.jsx
@@ -7,27 +7,49 @@ import { Modal, Segment } from 'semantic-ui-react';
 import StoryTriggersForm from '../chat_rules/StoryTriggersForm';
 
 const StoryTriggerEditor = (props) => {
+    const [localOpen, setLocalOpen] = useState(false);
+
     const {
         trigger,
         storyId,
         projectId,
         triggers: incommingTriggers,
+        open = localOpen,
+        setOpen = setLocalOpen,
     } = props;
 
     const [triggers, setTriggers] = useState({ triggers: incommingTriggers });
 
+    const modalTrigger = { // customize onClick and className of the trigger element
+        ...trigger,
+        props: {
+            ...trigger.props,
+            className: `${trigger.props.className || ''} trigger-editor-modal-trigger`,
+            onClick: (...args) => {
+                setOpen(true);
+                if (!trigger.props.onClick) return undefined;
+                return trigger.props.onClick(...args);
+            },
+        },
+    };
+    
     const handleChangeTriggers = (model) => {
         setTriggers(model);
     };
 
     const handleModalClose = () => {
-        Meteor.call('stories.updateTriggers', projectId, storyId, triggers);
+        Meteor.call('stories.updateTriggers', projectId, storyId, triggers, (err) => {
+            if (err) return;
+            setOpen(false);
+        });
     };
+
 
     return (
         <Modal
-            trigger={trigger}
+            trigger={modalTrigger}
             onClose={handleModalClose}
+            open={open}
         >
             <Segment.Group>
                 <Segment>
@@ -39,14 +61,18 @@ const StoryTriggerEditor = (props) => {
 };
 
 StoryTriggerEditor.propTypes = {
-    trigger: PropTypes.element.isRequired,
+    trigger: PropTypes.element.isRequired, // the trigger element will have it's onClick and className props modified
     storyId: PropTypes.string.isRequired,
     projectId: PropTypes.string.isRequired,
     triggers: PropTypes.array,
+    open: PropTypes.bool,
+    setOpen: PropTypes.func,
 };
 
 StoryTriggerEditor.defaultProps = {
     triggers: [],
+    open: null,
+    setOpen: null,
 };
 
 const mapStateToProps = state => ({
