a0c2adeede3e128a3b7441e3f15b412a728cacda
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-02-13 15:23:32 -0500

    refactor: add error logger in api change configuration to properly store httpRequest in stackdriver

--- api/app.js
+++ api/app.js
@@ -30,13 +30,55 @@ if (process.env.CORS === '*') {
 config().then(async config => {
 
     const routes = require('./routes/index.js');
+  
+    app.use(expressWinston.logger({
+        transports: logsTransport,
+        statusLevels: true,
+        metaField: null, //this causes the metadata to be stored at the root of the log entry
+        responseField: null, // this prevents the response from being included in the metadata (including body and status code)
+        requestWhitelist: ['headers', 'query'],  //these are not included in the standard StackDriver httpRequest
+        responseWhitelist: ['body'], // this populates the `res.body` so we can get the response size (not required)
+        dynamicMeta:  (req, res) => {
+            const httpRequest = {}
+            const meta = {}
+            if (req) {
+                meta.httpRequest = httpRequest
+                httpRequest.requestMethod = req.method
+                httpRequest.requestUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+                httpRequest.protocol = `HTTP/${req.httpVersion}`
+                // httpRequest.remoteIp = req.ip // this includes both ipv6 and ipv4 addresses separated by ':'
+                httpRequest.remoteIp = req.ip.indexOf(':') >= 0 ? req.ip.substring(req.ip.lastIndexOf(':') + 1) : req.ip   // just ipv4
+                httpRequest.requestSize = req.socket.bytesRead
+                httpRequest.userAgent = req.get('User-Agent')
+                httpRequest.referrer = req.get('Referrer')
+            }
+    
+            if (res) {
+                meta.httpRequest = httpRequest
+                httpRequest.status = res.statusCode
+                httpRequest.latency = {
+                    seconds: Math.floor(res.responseTime / 1000),
+                    nanos: ( res.responseTime % 1000 ) * 1000000,
+                }
+                if (res.body) {
+                    if (typeof res.body === 'object') {
+                        httpRequest.responseSize = JSON.stringify(res.body).length
+                    } else if (typeof res.body === 'string') {
+                        httpRequest.responseSize = res.body.length
+                    }
+                }
+            }
+            return meta
+        },
+    }));
+
+    app.use('/', routes);
+
     app.use(
-        expressWinston.logger({
+        expressWinston.errorLogger({
             transports: logsTransport,
-            statusLevels: true,
         }),
     );
-    app.use('/', routes);
  
     // Setup swagger UI
     require('./config/swagger')(app);
