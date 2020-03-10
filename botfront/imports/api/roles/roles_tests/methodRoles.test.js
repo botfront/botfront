1181846649bb097359f18a977e0dd7592f44918e
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-03-10 15:03:10 -0400

    refactor: removes old methods test

--- botfront/imports/api/roles/roles_tests/methodRoles.test.js
+++ botfront/imports/api/roles/roles_tests/methodRoles.test.js
@@ -427,16 +427,6 @@ if (Meteor.isServer) {
             roles: writers.responses,
             args: [projectId],
         },
-        {
-            name: 'rasa.restart',
-            roles: writers.projects,
-            args: [projectId],
-        },
-        {
-            name: 'get.rasaRestart.webhooks',
-            roles: writers.projects,
-            args: [projectId],
-        },
     ];
 
 
