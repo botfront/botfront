c53ee879a726dd7b6cc4c4a032419e2f03908b88
kenrilea <kenton.rilea@mrbot.ai>
2020-03-12 10:50:20 -0400

    chore: add stories SearchBar component

---
+++ botfront/imports/ui/components/stories/search/queries.js
@@ -0,0 +1,6 @@
+import gql from 'graphql-tag';
+
+export const SEARCH_STORIES = gql`
+query stories($projectId: String! $language: String! $queryString: String!) {
+    stories(projectId: $projectId language: $language queryString: $queryString)
+}`;
