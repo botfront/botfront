2800dec20c267326a5a09a132d08804cf1d2c96b
Matthieu Joannon <mattjoannon@gmail.com>
2020-03-13 17:50:40 -0400

    fix: not global admins cant add global admins

--- botfront/imports/api/user/user.schema.js
+++ botfront/imports/api/user/user.schema.js
@@ -1,43 +1,58 @@
 import SimpleSchema from 'simpl-schema';
+
 import { Projects } from '../project/project.collection';
+import { can } from '../../lib/scopes';
 
-export const UserRoleSchema = new SimpleSchema({
-    roles: Array,
-    'roles.$': {
-        type: String,
-        allowedValues: () => Meteor.roles.find({}, { fields: { _id: 1 } })
-            .fetch()
-            .map(r => r._id)
-            .filter(name => !name.match(/\w+:\w+/)),
+export const UserRoleSchema = new SimpleSchema(
+    {
+        roles: Array,
+        'roles.$': {
+            type: String,
+            allowedValues: () => Meteor.roles
+                .find({}, { fields: { _id: 1 } })
+                .fetch()
+                .map(r => r._id)
+                .filter((name) => {
+                    if (name === 'global-admin' && !can('global-admin')) return false;
+                    return !name.match(/\w+:\w+/);
+                }),
+        },
+        project: {
+            type: String,
+            allowedValues: () => Projects.find({}, { fields: { _id: 1, name: 1 } })
+                .fetch()
+                .map(p => p._id)
+                .concat(['GLOBAL']),
+        },
     },
-    project: {
-        type: String,
-        allowedValues: () => Projects.find({}, { fields: { _id: 1, name: 1 } })
-            .fetch()
-            .map(p => p._id)
-            .concat(['GLOBAL']),
+    { tracker: Tracker },
+);
+
+export const UserEditSchema = new SimpleSchema(
+    {
+        _id: { type: String },
+        emails: Array,
+        'emails.$': Object,
+        'emails.$.address': { type: String, regEx: SimpleSchema.RegEx.Email },
+        'emails.$.verified': { type: Boolean, autoValue: true },
+        profile: Object,
+        'profile.firstName': { type: String },
+        'profile.lastName': { type: String },
+        roles: { type: Array, minCount: 1 },
+        'roles.$': UserRoleSchema,
     },
-}, { tracker: Tracker });
+    { tracker: Tracker },
+);
 
-export const UserEditSchema = new SimpleSchema({
-    _id: { type: String },
-    emails: Array,
-    'emails.$': Object,
-    'emails.$.address': { type: String, regEx: SimpleSchema.RegEx.Email },
-    'emails.$.verified': { type: Boolean, autoValue: true },
-    profile: Object,
-    'profile.firstName': { type: String },
-    'profile.lastName': { type: String },
-    roles: { type: Array, minCount: 1 },
-    'roles.$': UserRoleSchema,
-}, { tracker: Tracker });
-
-export const UserCreateSchema = new SimpleSchema({
-    profile: Object,
-    'profile.firstName': { type: String },
-    'profile.lastName': { type: String },
-    email: { type: String, regEx: SimpleSchema.RegEx.Email },
-    sendEmail: { type: Boolean, defaultValue: true },
-    roles: { type: Array, minCount: 1 },
-    'roles.$': UserRoleSchema,
-}, { tracker: Tracker });
+export const UserCreateSchema = new SimpleSchema(
+    {
+        profile: Object,
+        'profile.firstName': { type: String },
+        'profile.lastName': { type: String },
+        email: { type: String, regEx: SimpleSchema.RegEx.Email },
+        sendEmail: { type: Boolean, defaultValue: true },
+        roles: { type: Array, minCount: 1 },
+        'roles.$': UserRoleSchema,
+    },
+    { tracker: Tracker },
+);
