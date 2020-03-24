8547fa45e072d93cc2df554de58986ef13b6d7d2
kenrilea <kenton.rilea@mrbot.ai>
2020-03-24 15:44:47 -0400

    refactor: fix bot response tests

--- botfront/cypress/integration/bot_responses/bot_response_consistency.spec.js
+++ botfront/cypress/integration/bot_responses/bot_response_consistency.spec.js
@@ -13,25 +13,43 @@ const createResponse = () => {
         .clear()
         .type(RESPONSE_TEXT)
         .blur();
+    cy.dataCy('bot-response-input').should('have.text', RESPONSE_TEXT);
 };
 
 describe('Bot responses', function() {
     beforeEach(function() {
         cy.createProject('bf').then(() => cy.login());
+        cy.visit('/project/bf/stories');
+        cy.browseToStory('Greetings', 'Default stories');
+        cy.dataCy('story-title').should('have.value', 'Greetings');
+        cy.dataCy('bot-response-input')
+            .find('textarea')
+            .clear()
+            .type('permanent response')
+            .blur();
+
         cy.createStoryGroup({ groupName: GROUP_NAME });
         cy.createStoryInGroup({ groupName: GROUP_NAME, storyName: STORY_NAME });
     });
 
     afterEach(function() {
-        cy.deleteProject('bf');
+        // cy.deleteProject('bf');
     });
 
-    const retryResponsePageCheck = (assert) => {
+    const checkResponsesDeleted = () => {
+        /*
+            we check for the permanent response first because it make checking for other
+            responses existence more reliable. other checks pass when the shouldn't:
+                1. Checking for the no responses message passes because the message is
+                always briefly rendered
+                2. Checking for dataCy('intent-label').should('not.exist') or similar
+                passes while the bot responses page is loading
+            checking for the permanent response to exist causes the test to wait for the page
+            to load before checking if the response from the test was correctly deleted
+        */
         cy.visit('/project/bf/dialogue/templates');
-        cy.dataCy('responses-screen').should(() => {
-            cy.visit('/project/bf/dialogue/templates');
-            assert();
-        });
+        cy.dataCy('template-intent').contains('utter_hi').should('exist');
+        cy.dataCy('template-intent').should('have.length', 1);
     };
 
     it('Should delete an existing response from the project when it is deleted in a story', function() {
@@ -41,42 +59,30 @@ describe('Bot responses', function() {
             .click({ force: true });
         cy.wait(500);
         cy.dataCy('bot-response-input').should('not.exist');
-        retryResponsePageCheck(() => {
-            cy.dataCy('no-responses').should('exist');
-        });
+        checkResponsesDeleted();
     });
 
     it('Should delete a response in a story from the project when the story is deleted', function() {
         createResponse();
-        retryResponsePageCheck(() => {
-            cy.dataCy('response-text').should('contain.text', RESPONSE_TEXT);
-        });
+        cy.visit('/project/bf/dialogue/templates');
+        cy.dataCy('response-text').should('contain.text', RESPONSE_TEXT);
 
         cy.visit('/project/bf/stories');
         cy.deleteStoryOrGroup(STORY_NAME, 'story');
         cy.wait(500);
-        
-        retryResponsePageCheck(() => {
-            cy.dataCy('no-responses').should('exist');
-        });
+        checkResponsesDeleted();
     });
 
     it('Should delete an existing response from the project when the story group is deleted', function() {
         createResponse();
         cy.visit('/project/bf/dialogue/templates');
-        retryResponsePageCheck(() => {
-            cy.dataCy('response-text').should('contain.text', RESPONSE_TEXT);
-        });
-        
-        
+        cy.dataCy('response-text').should('contain.text', RESPONSE_TEXT);
         cy.visit('/project/bf/stories');
         cy.deleteStoryOrGroup(STORY_NAME, 'story');
         cy.wait(500);
         cy.visit('/project/bf/dialogue/templates');
         // to properly retry the visit must be done again
-        retryResponsePageCheck(() => {
-            cy.dataCy('no-responses').should('exist');
-        });
+        checkResponsesDeleted();
     });
     
     it('Should delete a response in a branch when the branch is deleted', function() {
@@ -140,10 +146,11 @@ describe('Bot responses', function() {
             .click({ force: true });
         cy.wait(500);
         // check the correct bot responses were deleted from the project
-        retryResponsePageCheck(() => {
-            cy.dataCy('response-text').contains('first response should not exist').should('not.exist');
-            cy.dataCy('response-text').contains('second response should not exist').should('not.exist');
-            cy.dataCy('response-text').contains('third response should exist').should('exist');
-        });
+        cy.visit('/project/bf/dialogue/templates');
+        cy.dataCy('template-intent').contains('utter_hi').should('exist');
+        cy.dataCy('response-text').contains('first response should not exist').should('not.exist');
+        cy.dataCy('response-text').contains('second response should not exist').should('not.exist');
+        cy.dataCy('response-text').contains('third response should exist').should('exist');
+        cy.dataCy('template-intent').should('have.length', 2);
     });
 });
