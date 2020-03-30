137ec232ca4c8bc519c788f48f9851f0b6ef499e
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-03-30 13:07:24 -0400

    chore: change string to number in test

--- botfront/cypress/integration/stories/visual_editor/visual_editor.spec.js
+++ botfront/cypress/integration/stories/visual_editor/visual_editor.spec.js
@@ -104,7 +104,7 @@ describe('story visual editor', function () {
             .as('response');
 
         cy.visit('/project/bf/dialogue/templates/');
-        cy.wait('500');
+        cy.wait(500);
         cy.get('@response').then((response) => {
             cy.log(response);
             cy.get('[role=row]')
