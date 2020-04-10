e351ed99a2eb7c8764beb100773c124d940f081d
kenrilea <kenton.rilea@mrbot.ai>
2020-04-10 17:21:34 -0400

    refactor: create hoverable popup component

--- botfront/imports/ui/components/common/HoverablePopup.jsx
+++ botfront/imports/ui/components/common/HoverablePopup.jsx
@@ -1,21 +1,19 @@
 import {
-    Grid, Button, Popup, Header, Icon,
+    Popup,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import EntityDropdown from '../nlu/common/EntityDropdown';
-import { entityPropType } from '../utils/EntityUtils';
 
-class EntityPopup extends React.Component {
+class HoverablePopup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showDeleteConfirmation: false,
             open: false,
         };
         this.entityNode = React.createRef();
         this.popupNode = React.createRef();
+        this.trigger = React.createRef();
     }
 
     componentDidMount() {
@@ -29,154 +27,86 @@ class EntityPopup extends React.Component {
         clearTimeout(this.mouseLeaveTimer);
     }
 
-    handleDeleteClick = () => {
-        this.setState({ showDeleteConfirmation: true });
-    };
 
     handleScroll = (event) => {
-        const { onSelectionReset } = this.props;
         if (this.popupNode.current && !this.popupNode.current.contains(event.target)) {
-            onSelectionReset();
             this.setState({ open: false });
         }
     };
 
     handleClickOutside = (event) => {
-        const { onSelectionReset } = this.props;
         if (
             this.entityNode.current
             && !this.entityNode.current.contains(event.target)
             && (!this.popupNode.current || !this.popupNode.current.contains(event.target))
         ) {
-            this.setState({ open: false, showDeleteConfirmation: false });
-            onSelectionReset();
+            this.setState({ open: false });
         }
     };
 
-    handleMouseEnter = () => {
+    handleMouseEnter = (delayOpen = 0) => {
         clearTimeout(this.mouseLeaveTimer);
-        this.setState({ open: true });
+        this.mouseEnterTimer = setTimeout(() => {
+            this.setState({ open: true });
+        }, delayOpen);
     };
 
-    handleMouseLeave = () => {
+    handleMouseLeave = (overideLeaveTime) => {
         // The time in ms before the component actually close the popup
         // this is useful for allowing the user to put his mouse on the popup before it closes.
         // reduce this value to improve responsiveness, but this will reduce the time the user
         // has to move his mouse to the popup.
-        const leaveTime = 35;
-
+        clearTimeout(this.mouseEnterTimer);
+        const leaveTime = overideLeaveTime || 100;
         this.mouseLeaveTimer = setTimeout(() => {
             // this lets the user time to put his mouse on the popup after leaving the label
             this.setState({ open: false });
         }, leaveTime);
     };
 
-    getContent = () => {
-        const {
-            deletable, onDelete, entity, options, onAddOrChange,
-        } = this.props;
-        const { showDeleteConfirmation } = this.state;
-
-        return (
-            <Grid columns='1'>
-                <Grid.Row centered style={{ padding: '0.9em 0 0.4em 0' }}>
-                    <Header as='h4' style={{ fontWeight: '300' }}>
-                        {deletable ? 'Change' : 'Add'} entity
-                    </Header>
-                </Grid.Row>
-                <Grid.Row style={{ padding: '0 0.7em' }}>
-                    {(deletable && !showDeleteConfirmation) ? (
-                        <>
-                            <Grid.Column width='14' style={{ padding: '0' }}>
-                                <EntityDropdown
-                                    entity={entity}
-                                    options={[
-                                        ...options,
-                                        { text: entity.entity, value: entity.entity },
-                                    ]}
-                                    onAddItem={onAddOrChange}
-                                    onChange={onAddOrChange}
-                                />
-                            </Grid.Column>
-                            <Grid.Column width='2' style={{ padding: '0' }}>
-                                <Icon
-                                    link
-                                    name='trash'
-                                    color='grey'
-                                    onClick={this.handleDeleteClick}
-                                    style={{ padding: '15px 0 0 8px' }}
-                                />
-                            </Grid.Column>
-                        </>
-                    ) : (
-                        <EntityDropdown
-                            entity={entity}
-                            options={[...options, { text: entity.entity, value: entity.entity }]}
-                            onAddItem={onAddOrChange}
-                            onChange={onAddOrChange}
-                        />
-                    )}
-                </Grid.Row>
-                {showDeleteConfirmation ? (
-                    <Grid.Row centered>
-                        <Button negative size='mini' onClick={onDelete}>
-                            Confirm deletion
-                        </Button>
-                    </Grid.Row>
-                ) : (
-                    // Here for styling purposes
-                    <div style={{ height: '10px' }} />
-                )}
-            </Grid>
-        );
-    };
-
     render() {
         const {
             trigger,
-            selection,
-            length,
             disabled,
+            content,
         } = this.props;
         const { open } = this.state;
         return (
-            <span
-                onMouseEnter={this.handleMouseEnter}
-                onMouseLeave={this.handleMouseLeave}
-                length={length}
-                ref={this.entityNode}
-            >
-                <Popup
-                    trigger={trigger}
-                    content={(
-                        <div
-                            onMouseEnter={this.handleMouseEnter}
-                            onMouseLeave={this.handleMouseLeave}
-                            ref={this.popupNode}
-                        >
-                            {this.getContent()}
-                        </div>
-                    )}
-                    position='top center'
-                    open={selection || open}
-                    disabled={disabled}
-                    className='entity-popup'
-                />
-            </span>
+            <Popup
+                trigger={(
+                    <div
+                        onMouseEnter={() => this.handleMouseEnter(50)}
+                        onMouseLeave={() => this.handleMouseLeave(200)}
+                    >
+                        {trigger}
+                    </div>
+                )}
+                content={(
+                    <div
+                        onMouseEnter={this.handleMouseEnter}
+                        onMouseLeave={this.handleMouseLeave}
+                        ref={this.popupNode}
+                    >
+                        {content}
+                    </div>
+                )}
+                position='top center'
+                open={open}
+                disabled={disabled}
+                className='entity-popup'
+            />
         );
     }
 }
 
-EntityPopup.propTypes = {
-    content: PropTypes.string.isRequired,
+HoverablePopup.propTypes = {
+    content: PropTypes.element.isRequired,
     trigger: PropTypes.element.isRequired,
     disabled: PropTypes.bool,
-    // open: PropTypes.bool,
 };
 
-EntityPopup.defaultProps = {
+HoverablePopup.defaultProps = {
     disabled: false,
-    open: false,
 };
 
-export default EntityPopup;
+export default HoverablePopup;
