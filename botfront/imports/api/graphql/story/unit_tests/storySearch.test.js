b1a0c3ff948beee42fb1737170aa9293e35976e5
kenrilea <kenton.rilea@mrbot.ai>
2020-04-14 11:55:50 -0400

    chore: re-enable full test suite

--- botfront/imports/api/graphql/story/unit_tests/storySearch.test.js
+++ botfront/imports/api/graphql/story/unit_tests/storySearch.test.js
@@ -69,7 +69,7 @@ if (Meteor.isServer) {
         }
     };
     // ------ test suite -------
-    describe.only('test searching stories by their index', () => {
+    describe('test searching stories by their index', () => {
         before((done) => {
             insertDataAndIndex(done);
         });
