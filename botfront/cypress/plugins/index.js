01c7f4c797f40abc0c9bd77805f28ccb4a3ff997
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-04-24 13:04:15 -0400

    refactor: use meteor authtoken to execute graphql query from cypress this backports changes on ee

--- botfront/cypress/plugins/index.js
+++ botfront/cypress/plugins/index.js
@@ -12,6 +12,8 @@
 // the project's config changing)
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+};
+
+module.exports = (on) => { on('task', { log (message) { console.log(message); return null; } }); };
