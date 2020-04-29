557126cc538d3990d39bd0ebf861fbae795cefa7
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-04-29 10:44:09 -0400

    test: shuffle things around in roles-page.spec.js

--- botfront/cypress/integration/admin_dashboard/roles-page.spec.js
+++ botfront/cypress/integration/admin_dashboard/roles-page.spec.js
@@ -1,43 +1,56 @@
-/* eslint-disable no-undef */
+/* global cy */
 
 const roleName = 'TestRole';
+const roleDesc = 'description of test role';
+const permission = 'nlu-data:w';
 const editedRoleName = 'NewRole';
+
+const createRole = () => {
+    cy.visit('/admin/roles');
+    cy.dataCy('create-role').click();
+    cy.dataCy('role-name-input').click().type(roleName);
+    cy.dataCy('role-description-input').click().type(roleDesc);
+    cy.dataCy('role-children-dropdown').click();
+    cy.dataCy('role-children-dropdown').find('div').contains(permission).click();
+    cy.dataCy('role-children-dropdown').find('input').type('{esc}');
+    cy.dataCy('save-button').click();
+    cy.dataCy('save-button').should('have.text', 'Saved');
+};
 
 describe('can create, edit, and delete a role', () => {
     beforeEach(() => {
         cy.createProject('bf', 'My Project', 'en').then(() => {
             cy.login();
+            createRole();
+            cy.createUser('roleTestUser', 'roleTestUser@test.test', [roleName], 'bf');
         });
     });
     afterEach(function() {
         cy.logout();
         cy.deleteUser('roleTestUser@test.test');
-        cy.deleteRole('NewRole', 'global-admin');
-        cy.deleteRole('TestRole', 'global-admin');
+        cy.deleteRole(editedRoleName, 'global-admin');
         cy.deleteProject('bf');
     });
-    const createRole = () => {
+    it('should create, edit and delete a role', () => {
         cy.visit('/admin/roles');
-        cy.dataCy('create-role').click();
-        cy.dataCy('role-name-input').click().type(roleName);
-        cy.dataCy('role-description-input').click().type('description of test role');
-        cy.dataCy('role-children-dropdown').click();
-        cy.dataCy('role-children-dropdown').find('div').contains('nlu-data:w').click();
-        cy.dataCy('role-children-dropdown').find('input').type('{esc}');
-        cy.dataCy('save-button').click();
-        cy.dataCy('save-button').should('have.text', 'Saved');
-    };
-    it('should create and delete a role', () => {
-        createRole();
-        cy.createUser('roleTestUser', 'roleTestUser@test.test', [roleName], 'bf');
 
         // verify role data was saved
-        cy.visit('/admin/roles');
         cy.get('.-btn').contains('Next').click();
         cy.dataCy('role-link').contains(roleName).click();
         cy.dataCy('role-name-input').find('input').should('have.value', roleName);
-        cy.dataCy('role-description-input').find('input').should('have.value', 'description of test role');
-        cy.dataCy('role-children-dropdown').find('.ui.label').contains('nlu-data:w').should('exist');
+        cy.dataCy('role-description-input').find('input').should('have.value', roleDesc);
+        cy.dataCy('role-children-dropdown').find('.ui.label').contains(permission).should('exist');
+
+        // edit role
+        cy.dataCy('role-name-input').find('input').clear().type(editedRoleName);
+        cy.dataCy('save-button').click();
+        cy.dataCy('save-button').should('have.text', 'Saved');
+
+        // verfiy that the role was not duplicated
+        cy.visit('/admin/roles');
+        cy.get('.-btn').contains('Next').click();
+        cy.dataCy('role-link').contains(roleName).should('not.exist');
+        cy.dataCy('role-link').contains(editedRoleName).should('exist').click();
 
         // delete role
         cy.dataCy('delete-role').click();
@@ -53,22 +66,5 @@ describe('can create, edit, and delete a role', () => {
         cy.visit('/admin/users');
         cy.dataCy('edit-user').last().click();
         cy.dataCy('user-roles-field').find('.label').contains('global-admin').should('exist');
-    });
-    it('should edit a role', () => {
-        createRole();
-        cy.createUser('roleTestUser', 'roleTestUser@test.test', [roleName], 'bf');
-
-        cy.visit('/admin/roles');
-        cy.get('.-btn').contains('Next').click();
-        cy.dataCy('role-link').contains(roleName).click();
-        cy.dataCy('role-name-input').find('input').clear().type(editedRoleName);
-        cy.dataCy('save-button').click();
-        cy.dataCy('save-button').should('have.text', 'Saved');
-
-        // verfiy that the role was not duplicated
-        cy.visit('/admin/roles');
-        cy.get('.-btn').contains('Next').click();
-        cy.dataCy('role-link').contains(roleName).should('not.exist');
-        cy.dataCy('role-link').contains(editedRoleName).should('exist');
     });
 });
