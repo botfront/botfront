3c0aca62781c2fce8035364ae0c482094146594d
kenrilea <kenton.rilea@mrbot.ai>
2020-05-21 14:10:55 -0400

    refactor: setQuickReply cypress command

--- botfront/cypress/support/response.commands.js
+++ botfront/cypress/support/response.commands.js
@@ -76,17 +76,14 @@ Cypress.Commands.add('addButtonOrSetPayload', (title, payload, oldTitleOrPositio
 
 Cypress.Commands.add('setQuickReplyContent', (text, title, intent, index = 0) => {
     cy.dataCy('response-editor').find('[data-cy=bot-response-input]').eq(index).click();
-    cy.dataCy('response-editor').find('[data-cy=bot-response-input]')
-        .find('textarea')
-        .eq(index)
-        .type(text)
-        .blur();
-    cy.dataCy('response-editor').find('[data-cy=button_title]').eq(index).click({ force: true });
-    cy.dataCy('response-editor').find('[data-cy=button_title]').eq(index).click({ force: true });
-    cy.dataCy('response-editor').find('[data-cy=button_title]').eq(index).click({ force: true });
     cy.dataCy('response-editor').find('[data-cy=button_title]').eq(index).click({ force: true });
     cy.dataCy('enter-button-title').find('input').type(title);
     cy.dataCy('intent-label').should('exist').click();
     cy.dataCy('intent-dropdown').find('input').type(`${intent}{enter}`);
     cy.dataCy('save-button').click();
+    cy.dataCy('response-editor').find('[data-cy=bot-response-input]')
+        .find('textarea')
+        .eq(index)
+        .type(text)
+        .blur();
 });
