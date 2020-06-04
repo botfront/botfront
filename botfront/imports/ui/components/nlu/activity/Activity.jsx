cb3b404e81fdea80b81d9a7a0aa0d53503d29dff
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-06-04 09:31:34 -0400

    fix: entity tagging in activity screen

--- botfront/imports/ui/components/nlu/activity/Activity.jsx
+++ botfront/imports/ui/components/nlu/activity/Activity.jsx
@@ -200,7 +200,7 @@ function Activity(props) {
         return (
             <UserUtteranceViewer
                 value={datum}
-                onChange={handleUpdate}
+                onChange={({ _id, entities }) => handleUpdate([{ _id, entities }])}
                 projectId={projectId}
                 disabled={isUtteranceOutdated(datum)}
                 disableEditing={isUtteranceOutdated(datum)}
