9b5e67fe7f0e45a1bdd1cdb5e2895fc07efd899c
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-06-10 17:53:58 -0400

    chore: trash api routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 
-const { importConversationalData, importConversationalDataValidator, latestImport, latestImportValidator } = require('../server/imports/imports.controller');
 const {
     exportProject,
     exportProjectValidator,
@@ -15,9 +14,5 @@ router.get('/project/:project_id/export', exportProjectValidator, exportProject)
 router.put('/project/:project_id/import', importProject);
 
 router.get('/health-check', (req, res) => res.status(200).json({ version, healthy: true }));
-
-router.post('/conversations/:project_id/environment/:env', importConversationalDataValidator, importConversationalData);
-router.get('/conversations/:project_id/environment/:env/latest-imported-event', latestImportValidator, latestImport);
-
 
 module.exports = router;
