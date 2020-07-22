a76522b026b50d43d4f48b4215511bea8af60f4a
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-07-22 16:34:43 -0400

    test: prober script compatibility

--- botfront/cypress/prober.sh
+++ botfront/cypress/prober.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-mkdir cypress/output
+mkdir -p cypress/output
 npx cypress run --spec "cypress/integration/01_initial_setup_dont_change_name/initial_setup.spec.js"
-for i in {1..5}
+
+for i in 1 2 3 4 5
 do
-    npx cypress run --record --spec "cypress/integration/$SPEC"
-    mv cypress/screenshots cypress/ouput/screenshots_$i
-    mv cypress/videos cypress/ouput/videos_$i
+    mkdir -p cypress/screenshots
+    mkdir -p cypress/videos
+    npx cypress run --spec "cypress/integration/$SPEC"
+    mv cypress/screenshots cypress/output/screenshots_$i
+    mv cypress/videos cypress/output/videos_$i
 done
