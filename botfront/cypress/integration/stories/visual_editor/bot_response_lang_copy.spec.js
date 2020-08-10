84d25e19ff6954df341b5c988d8e62b670a1c234
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-08-10 17:21:17 -0400

    refactor: test copy from language

--- botfront/cypress/integration/stories/visual_editor/bot_response_lang_copy.spec.js
+++ botfront/cypress/integration/stories/visual_editor/bot_response_lang_copy.spec.js
@@ -17,40 +17,42 @@ describe('Bot responses copy between languages', function() {
     });
 
     
-    // it('should allow copy from another language with a text response', function() {
-    //     cy.visit('/project/bf/stories');
-    //     cy.browseToStory('Groupo (1)');
-    //     cy.dataCy('add-bot-line').click({ force: true });
-    //     cy.dataCy('from-text-template').click({ force: true });
-    //     cy.dataCy('bot-response-input')
-    //         .find('textarea')
-    //         .clear()
-    //         .type('test response');
+    it('should allow copy from another language with a text response', function() {
+        cy.visit('/project/bf/stories');
+        cy.browseToStory('Groupo (1)');
+        cy.dataCy('add-bot-line').click({ force: true });
+        cy.dataCy('from-text-template').click({ force: true });
+        cy.dataCy('bot-response-input')
+            .find('textarea')
+            .clear()
+            .type('test response');
+    
+        // copy in a second language
+        cy.dataCy('language-selector').click();
+        cy.contains('German').click();
+        cy.dataCy('bot-response-input').should('not.have.text', 'test response');
+        cy.dataCy('import-from-lang').click({ force: true });
+        cy.dataCy('import-from-lang').contains('English').click();
+        cy.dataCy('bot-response-input').should('have.text', 'test response');
+    });
+    it('should allow copy from another language with a image response', function() {
+        cy.visit('/project/bf/stories');
+        cy.browseToStory('Groupo (1)');
+        cy.dataCy('add-bot-line').click({ force: true });
+        cy.dataCy('from-image-template').click({ force: true });
+        cy.setImage(imageUrlA);
         
 
-    //     // copy in a second language
-    //     cy.dataCy('language-selector').click();
-    //     cy.contains('German').click();
-    //     cy.dataCy('import-from-lang').click({ force: true });
-    //     cy.dataCy('import-from-lang').contains('English').click();
-    //     cy.dataCy('bot-response-input').should('have.text', 'test response');
-    // });
-    // it('should allow copy from another language with a image response', function() {
-    //     cy.visit('/project/bf/stories');
-    //     cy.browseToStory('Groupo (1)');
-    //     cy.dataCy('add-bot-line').click({ force: true });
-    //     cy.dataCy('from-image-template').click({ force: true });
-    //     cy.setImage(imageUrlA);
-        
+        // copy in a second language
+        cy.dataCy('language-selector').click();
+        cy.contains('German').click();
+        cy.dataCy('bot-response-input').find('img').should('have.attr', 'src')
+            .and('equal', '');
+        cy.dataCy('import-from-lang').click({ force: true });
+        cy.dataCy('import-from-lang').contains('English').click();
 
-    //     // copy in a second language
-    //     cy.dataCy('language-selector').click();
-    //     cy.contains('German').click();
-    //     cy.dataCy('import-from-lang').click({ force: true });
-    //     cy.dataCy('import-from-lang').contains('English').click();
-
-    //     cy.get('[data-cy=image-container] img').should('have.attr', 'src', imageUrlA);
-    // });
+        cy.get('[data-cy=image-container] img').should('have.attr', 'src', imageUrlA);
+    });
 
     it('should allow copy from another language with a button response', function() {
         cy.visit('/project/bf/stories');
@@ -60,34 +62,50 @@ describe('Bot responses copy between languages', function() {
         cy.dataCy('bot-response-input')
             .find('textarea')
             .type('buttons test');
+        cy.dataCy('button_title').click({ force: true });
+        cy.dataCy('button_title').click({ force: true });
+
+        cy.dataCy('enter-button-title').find('input').type('button A');
+        cy.dataCy('intent-label').should('exist').click();
+        cy.dataCy('intent-dropdown').find('input').type('option_A{enter}');
+        cy.dataCy('save-button').click();
         
 
         // copy in a second language
         cy.dataCy('language-selector').click();
         cy.contains('German').click();
-        cy.wait(100000);
+        cy.dataCy('bot-response-input').should('not.contain.text', 'buttons testbutton A');
         cy.dataCy('import-from-lang').click({ force: true });
         cy.dataCy('import-from-lang').contains('English').click();
-        cy.dataCy('bot-response-input').should('have.text', 'buttons test');
+        cy.dataCy('bot-response-input').should('contain.text', 'buttons testbutton A');
     });
 
-    // it('should allow copy from another language with a carousel response', function() {
-    //     addTextResponse('test_A', 'response content');
-    //     cy.dataCy('template-intent').contains('utter_test_A').should('exist');
-    //     // edit in a second language
-    //     cy.get('.item').contains('German').click();
-    //     cy.dataCy('edit-response-0').click();
-    //     cy.dataCy('bot-response-input').click().find('textarea').clear()
-    //         .type('new response')
-    //         .blur();
-    //     cy.wait(100);
-    //     cy.escapeModal();
-    //     cy.wait(1000);
-    //     cy.dataCy('template-intent').contains('utter_test_A').should('exist');
-    //     cy.dataCy('response-text').contains('new response').should('exist');
-    //     // verify original language has not changed
-    //     cy.get('.item').contains('English').click();
-    //     cy.dataCy('template-intent').contains('utter_test_A').should('exist');
-    //     cy.dataCy('response-text').contains('response content').should('exist');
-    // });
+    it('should allow copy from another language with a carousel response', function() {
+        cy.visit('/project/bf/stories');
+        cy.dataCy('language-selector').click();
+        cy.dataCy('add-bot-line').click({ force: true });
+        cy.dataCy('from-carousel-template').click({ force: true });
+        cy.setImage(imageUrlA);
+        cy.setTitleAndSubtitle('fancy title', 'fancy subtitle');
+        cy.addButtonOrSetPayload('postback option', { payload: { intent: 'get_started' } }, 0);
+        cy.addButtonOrSetPayload('web_url option', { url: 'http://yahoo.com/' }, 0);
+        
+
+        // copy in a second language
+        cy.dataCy('language-selector').click();
+        cy.contains('German').click();
+        cy.get('.carousel-slide img').should('not.exist');
+        cy.get('.carousel-slide').should('not.contain.text', 'fancy title');
+        cy.get('.carousel-slide').should('not.contain.text', 'fancy subtitle');
+        cy.get('.carousel-slide').should('not.contain.text', 'postback option');
+        cy.get('.carousel-slide').should('not.contain.text', 'web_url option');
+        cy.dataCy('import-from-lang').click({ force: true });
+        cy.dataCy('import-from-lang').contains('English').click();
+        cy.get('.carousel-slide img').should('have.attr', 'src')
+            .and('equal', imageUrlA);
+        cy.get('.carousel-slide').should('contain.text', 'fancy title');
+        cy.get('.carousel-slide').should('contain.text', 'fancy subtitle');
+        cy.get('.carousel-slide').should('contain.text', 'postback option');
+        cy.get('.carousel-slide').should('contain.text', 'web_url option');
+    });
 });
