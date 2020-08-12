87f5882e0bf5ca5721ee8d27dfa1846d48756138
GuillaumeTech <guillaumeval.techer@gmail.com>
2020-08-12 14:26:38 -0400

    refactor: backport fix for the test getTrainingDataInRasaFormat

--- botfront/imports/api/instances/instances.methods.test.js
+++ botfront/imports/api/instances/instances.methods.test.js
@@ -3,10 +3,35 @@ import { expect } from 'chai';
 import { getTrainingDataInRasaFormat } from './instances.methods';
 import nluModel from './testData/nlu_model_chitchat.data';
 import { allExamples, selectedExamples, selectedExampleAndDummy } from './testData/results.data';
+import { Projects } from '../project/project.collection';
+
+
+const testProject = {
+    _id: 'test',
+    namespace: 'bf-test',
+    name: 'My Project',
+    defaultLanguage: 'en',
+    defaultDomain: { content: 'slots:\n  disambiguation_message:\n    type: unfeaturized\nactions:\n  - action_botfront_disambiguation\n  - action_botfront_disambiguation_followup\n  - action_botfront_fallback\n  - action_botfront_mapping' },
+    enableSharing: false,
+    disabled: false,
+    nlu_models: ['test_model'],
+    updatedAt: { $date: '2020-07-24T13:51:26.698Z' },
+    storyGroups: ['r4xYPj8w6MgkwjQTm'],
+    training: { instanceStatus: 'notTraining' },
+};
 
 if (Meteor.isTest) {
     describe('getTrainingDataInRasaFormat', function () {
         if (Meteor.isServer) {
+            beforeEach(async (done) => {
+                await Projects.insert(testProject);
+                done();
+            });
+            afterEach(async (done) => {
+                await Projects.remove({ _id: 'test' });
+                done();
+            });
+
             it('should generate a payload with all the nlu when there are no selected intents', function () {
                 const data = getTrainingDataInRasaFormat(nluModel);
                 expect(data).to.deep.equal(allExamples);
