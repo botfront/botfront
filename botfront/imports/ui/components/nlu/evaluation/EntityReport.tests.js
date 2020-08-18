86f6e1b2873cbb05d6e541bc9763d6fb0f997cf2
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-08-18 10:54:10 -0400

    test: fix EntityReport tests

--- botfront/imports/ui/components/nlu/evaluation/EntityReport.tests.js
+++ botfront/imports/ui/components/nlu/evaluation/EntityReport.tests.js
@@ -1,6 +1,6 @@
 import Meteor from 'meteor/meteor';
 import chai, { expect } from 'chai';
-import EntityReport from './EntityReport'
+import EntityReport from './EntityReport';
 
 const example_perfect = {
     text: 'this is an example',
@@ -9,15 +9,15 @@ const example_perfect = {
         start: 0,
         end: 4,
         value: 'test',
-        entity: 'this'
+        entity: 'this',
     }],
     predicted: 'test',
     predicted_entities: [{
         start: 0,
         end: 4,
         value: 'test',
-        entity: 'this'
-    }]
+        entity: 'this',
+    }],
 };
 
 const example_overlap = {
@@ -27,15 +27,15 @@ const example_overlap = {
         start: 0,
         end: 4,
         value: 'this',
-        entity: 'test'
+        entity: 'test',
     }],
     predicted: 'test',
     predicted_entities: [{
         start: 0,
         end: 7,
         value: 'this is',
-        entity: 'test'
-    }]
+        entity: 'test',
+    }],
 };
 
 const example_mismatch = {
@@ -45,15 +45,15 @@ const example_mismatch = {
         start: 0,
         end: 4,
         value: 'this',
-        entity: 'test'
+        entity: 'test',
     }],
     predicted: 'test',
     predicted_entities: [{
         start: 0,
         end: 7,
         value: 'this is',
-        entity: 'another'
-    }]
+        entity: 'another',
+    }],
 };
 
 const example_not_found = {
@@ -63,60 +63,75 @@ const example_not_found = {
         start: 0,
         end: 4,
         value: 'this',
-        entity: 'test'
+        entity: 'test',
     }],
     predicted: 'test',
     predicted_entities: [{
         start: 11,
         end: 18,
         value: 'example',
-        entity: 'another'
-    }]
+        entity: 'another',
+    }],
+};
+
+const example_surprise = {
+    text: 'this is an example',
+    intent: 'test',
+    entities: [],
+    predicted: 'test',
+    predicted_entities: [{
+        start: 0,
+        end: 4,
+        value: 'this',
+        entity: 'test',
+    }],
 };
 
 if (!Meteor.isServer) {
-    describe("error matching for entity and prediction", function () {
-        const entity1 = {start: 1, end: 5};
-        const entity2 = {start: 0, end: 3};
+    describe('error matching for entity and prediction', function () {
+        const entity1 = { start: 1, end: 5 };
+        const entity2 = { start: 0, end: 3 };
 
-        it("overlap returns number of characters overlapping", function () {
+        it('overlap returns number of characters overlapping', function () {
             const overlap = EntityReport.getOverlap(entity1, entity2);
             expect(overlap).to.be.equal(2);
         });
 
-        it("overlap is symmetric", function () {
+        it('overlap is symmetric', function () {
             const overlap1 = EntityReport.getOverlap(entity1, entity2);
             const overlap2 = EntityReport.getOverlap(entity2, entity1);
-            expect(overlap1).to.be.equal(overlap2)
+            expect(overlap1).to.be.equal(overlap2);
         });
 
-        it("error code matches the correct type of error", function () {
+        it('error code matches the correct type of error', function () {
             const perfect_code = EntityReport.getErrorCode(example_perfect.entities[0], example_perfect.predicted_entities[0]);
             const overlap_code = EntityReport.getErrorCode(example_overlap.entities[0], example_overlap.predicted_entities[0]);
             const mismatch_code = EntityReport.getErrorCode(example_mismatch.entities[0], example_mismatch.predicted_entities[0]);
             const not_found_code = EntityReport.getErrorCode(example_not_found.entities[0], example_not_found.predicted_entities[0]);
-            expect(perfect_code).to.be.equal(0);
-            expect(overlap_code).to.be.equal(1);
-            expect(mismatch_code).to.be.equal(2);
-            expect(not_found_code).to.be.equal(3);
+            const surprise_code = EntityReport.getErrorCode(example_surprise.entities[0], example_surprise.predicted_entities[0]);
+            expect(perfect_code).to.be.equal(null);
+            expect(overlap_code).to.be.equal(0);
+            expect(mismatch_code).to.be.equal(1);
+            expect(not_found_code).to.be.equal(2);
+            expect(surprise_code).to.be.equal(3);
         });
 
-        it("not_found_error_code for null result", function () {
+        it('not_found_error_code for null result', function () {
             const null_code = EntityReport.getErrorCode(example_perfect.entities[0], null);
             const not_found_code = EntityReport.getErrorCode(example_not_found.entities[0], example_not_found.predicted_entities[0]);
             expect(not_found_code).to.be.equal(null_code);
-        })
+        });
     });
 
-    describe("finding closest entities", function() {
-        it("should select closest from list of possibilities", function () {
+    describe('finding closest entities', function() {
+        it('should select closest from list of possibilities', function () {
             const closest = EntityReport.findClosestEntity(example_perfect.entities[0], example_mismatch.predicted_entities.concat(example_not_found.predicted_entities));
             expect(closest).to.be.equal(example_mismatch.predicted_entities[0]);
         });
 
-        it("should return null if no overlap", function () {
+        it('should return null if no overlap', function () {
             const closest = EntityReport.findClosestEntity(example_perfect.entities[0], example_not_found.predicted_entities);
             expect(closest).to.be.equal(null);
         });
-    })
+    });
 }
