4de12cb13f3809c020c26693e0e188df774fdbbb
pheel <philippe@mrbot.ai>
2020-08-19 10:07:26 -0400

    fix: display entity evaluation

--- botfront/imports/ui/components/nlu/evaluation/EntityReport.jsx
+++ botfront/imports/ui/components/nlu/evaluation/EntityReport.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
 import ReactTable from 'react-table-v6';
 import _ from 'lodash';
-import { Message } from 'semantic-ui-react';
-
+import KeyMetrics from './KeyMetrics';
+import ReportTable from './ReportTable';
 import UserUtteranceViewer from '../common/UserUtteranceViewer';
 import EntityUtils from '../../utils/EntityUtils';
+import { Info } from '../../common/Info';
 import { examplePropType } from '../../utils/ExampleUtils';
-import { Info } from '../../utils/Utils';
-import { Metrics } from './KeyMetrics';
 
 function ExampleTextComparison({ example, prediction }) {
     return (
-        <div className='entity_evaluation'>
-            <UserUtteranceViewer value={example} color='green' disableEditing />
-            <UserUtteranceViewer value={prediction} color='red' disableEditing />
+        <div>
+            <p><div><UserUtteranceViewer value={example} color='green' disableEditing showIntent={false} /></div></p>
+            <p><div><UserUtteranceViewer value={prediction} color='red' disableEditing showIntent={false} /></div></p>
         </div>
     );
 }
@@ -22,27 +22,28 @@ function ExampleTextComparison({ example, prediction }) {
 ExampleTextComparison.propTypes = {
     example: PropTypes.shape(examplePropType).isRequired,
     prediction: PropTypes.shape(examplePropType).isRequired,
-    entities: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default class EntityReport extends React.Component {
     static getErrorCode(entity, prediction) {
+        if (!entity) return 3;
+
         if (_.isNull(prediction)) {
-            return 3;
+            return 2;
         }
 
         const overlap = EntityReport.getOverlap(entity, prediction);
         if (overlap === 0) {
-            return 3;
-        }
-        if (entity.entity !== prediction.entity) {
             return 2;
         }
-        if (entity.start !== prediction.start || entity.end !== prediction.end) {
+        if (entity.entity !== prediction.entity) {
             return 1;
         }
+        if (entity.start !== prediction.start || entity.end !== prediction.end) {
+            return 0;
+        }
 
-        return 0;
+        return null;
     }
 
     static getOverlap(entity1, entity2) {
@@ -63,6 +64,7 @@ export default class EntityReport extends React.Component {
     }
 
     static findClosestEntity(entity, collection) {
+        if (!entity) return null;
         let closest = null;
         let overlap = 0;
 
@@ -87,76 +89,104 @@ export default class EntityReport extends React.Component {
             expanded: {},
         };
 
-        this.errorTypes = ['Correct', 'Overlap', 'Mismatch', 'Not Found'];
+        this.errorTypes = ['Overlap', 'Mismatch', 'Not Found', 'Surprise'];
         this.errorInfo = [
-            'This field measures the portion of entities that were matched correctly, in both position and classification',
-            'These errors correspond to a correct classification but slightly incorrect token boundary',
-            'These indicate an overlapping token boundary but conflicting classification',
-            'Not found indicates either that the entity was missed entirely or appeared in a non-overlapping position in the text',
+            'Correct classification but slightly incorrect token boundary',
+            'Overlapping token boundary but conflicting classification',
+            'Either the entity was missed entirely or it appeared in a non-overlapping position in the text',
+            'An entity was predicted where there was supposed to be none',
         ];
-        this.errorMessages = ['', 'Incorrect token boundary for this entity', 'Incorrect classification for this entity', 'No corresponding entity in prediction'];
+        this.errorMessages = [
+            'Incorrect token boundary for entity',
+            'Incorrect classification for entity',
+            'No corresponding entity in prediction',
+            'Predicted entity not present in test utterance',
+        ];
     }
 
-    getEntityData() {
+    getPanes = () => {
+        const { report, predictions } = this.props;
+        const tabs = [
+            {
+                menuItem: 'Detailed Report',
+                render: () => <ReportTable report={report} labelType='entity' />,
+            },
+        ];
+        if (predictions && predictions.length) {
+            tabs.push({
+                menuItem: 'Misclassifications',
+                render: this.renderPredictionsTable,
+            });
+        }
+        return tabs;
+    }
+
+    getEntityData = () => {
+        // Load entities in state for colour matching
+        const { entities } = this.state;
         const statEntities = [];
         const { predictions } = this.props;
 
         predictions.forEach((p) => {
-            if (p.entities) {
-                p.entities.forEach((e) => {
-                    // Load entities in state for colour matching
-                    const { entities } = this.state;
-                    const { entity } = e;
+            const predictedEntities = (p.predicted_entities || []).filter(EntityUtils.filterDuckling);
+            const entitiesGold = p.entities || [];
+            const join = entitiesGold.reduce((acc, curr) => {
+                if (acc.some(a => a.entity === curr.entity)) return acc;
+                return [...acc, curr];
+            }, predictedEntities);
+            join.forEach((e) => {
+                const { entity } = e;
 
-                    if (_.indexOf(entities, entity) < 0) {
-                        entities.push(entity);
-                        this.setState({ entities });
-                    }
+                if (_.indexOf(entities, entity) < 0) {
+                    entities.push(entity);
+                    this.setState({ entities });
+                }
 
-                    // Load entity stat entry for display
-                    if (_.find(statEntities, { entity }) === undefined) {
-                        statEntities.push({
-                            entity,
-                            failed_examples: [],
-                            errorCount: [0, 0, 0, 0], // One for each error code (see getErrorCode)
-                        });
-                    }
+                // Load entity stat entry for display
+                if (_.find(statEntities, { entity }) === undefined) {
+                    statEntities.push({
+                        entity,
+                        failed_examples: [],
+                        errorCount: [0, 0, 0, 0], // One for each error code (see getErrorCode)
+                    });
+                }
 
-                    // The row to modify
-                    const statEntity = _.find(statEntities, { entity });
+                // The row to modify
+                const statEntity = _.find(statEntities, { entity });
 
-                    const prediction = EntityReport.findClosestEntity(e, p.predicted_entities.filter(EntityUtils.filterDuckling));
-                    const errorCode = EntityReport.getErrorCode(e, prediction);
+                const entityGold = entitiesGold.find(eg => eg.entity === entity);
 
-                    statEntity.errorCount[errorCode] += 1;
-                    if (errorCode > 0) {
-                        // Some error has occurred
-                        const intent = p.intent || 'none';
-                        const predicted = p.predicted || 'none';
-                        const { text } = p;
+                const prediction = EntityReport.findClosestEntity(
+                    entityGold, predictedEntities,
+                );
+                const errorCode = EntityReport.getErrorCode(entityGold, prediction);
 
-                        statEntity.failed_examples.push({
-                            intent,
-                            example: {
-                                text,
-                                intent,
-                                entities: [e],
-                            },
-                            prediction: {
-                                text,
-                                intent: predicted,
-                                entities: prediction ? [prediction] : [],
-                            },
-                            errorCode,
-                        });
-                    }
+                statEntity.errorCount[errorCode] += 1;
+
+                const intent = p.intent || 'none';
+                const predicted = p.predicted || 'none';
+                const { text } = p;
+
+                statEntity.failed_examples.push({
+                    intent,
+                    example: {
+                        text,
+                        intent,
+                        entities: entitiesGold,
+                    },
+                    prediction: {
+                        text,
+                        intent: predicted,
+                        entities: predictedEntities || [],
+                    },
+                    errorCode,
                 });
-            }
+            });
         });
         return statEntities;
     }
 
-    getFailedExamplesColumns() {
+    getFailedExamplesColumns = () => {
         const { entities } = this.state;
         return [
             {
@@ -165,44 +195,53 @@ export default class EntityReport extends React.Component {
                 Header: '',
                 Cell: () => (
                     <div>
-                        <p className='overflow with_margin'>Expected: </p>
-                        <p className='overflow'>Predicted: </p>
+                        <p>Expected: </p>
+                        <p>Predicted: </p>
                     </div>
                 ),
                 width: 80,
+                style: { paddingTop: '20px' },
             },
             {
                 id: 'example',
                 accessor: e => e,
                 Header: 'Example',
-                Cell: e => <ExampleTextComparison example={e.value.example} prediction={e.value.prediction} entities={entities} />,
-                className: 'left',
+                Cell: e => (
+                    <ExampleTextComparison
+                        example={e.value.example}
+                        prediction={e.value.prediction}
+                        entities={entities}
+                    />
+                ),
+                width: '100%',
+                style: { paddingTop: '20px' },
             },
             {
                 id: 'error',
                 accessor: 'errorCode',
                 Header: 'Error Type',
                 Cell: errorCode => (
-                    <div className='error_text'>
-                        <p style={{ display: 'inline', color: 'red' }}>{this.errorTypes[errorCode.value]}</p>
+                    <div>
+                        <p style={{ display: 'inline', color: 'red' }}>
+                            {this.errorTypes[errorCode.value]}
+                        </p>
                         {': '}
                         {this.errorMessages[errorCode.value]}
                     </div>
                 ),
                 width: 200,
-                className: 'left',
+                style: { paddingTop: '20px' },
             },
         ];
     }
 
-    getEntitiesColumns() {
+    getEntitiesColumns = () => {
         const columns = [
             {
                 id: 'entity',
                 accessor: 'entity',
                 Header: 'Entity',
                 Cell: e => <p>{e.value}</p>,
-                className: 'left',
             },
         ];
 
@@ -216,8 +255,13 @@ export default class EntityReport extends React.Component {
                         <Info info={this.errorInfo[i]} />
                     </div>
                 ),
-                Cell: e => <p>{`${((e.value.errorCount[i] / _.sum(e.value.errorCount)) * 100).toFixed(2)}%`}</p>,
-                className: 'right',
+                Cell: e => (
+                    <p>{`${(
+                        (e.value.errorCount[i] / _.sum(e.value.errorCount))
+                        * 100
+                    ).toFixed(2)}%`}
+                    </p>
+                ),
                 sortMethod: (a, b) => {
                     const aValue = a.errorCount[i] / _.sum(a.errorCount);
                     const bValue = b.errorCount[i] / _.sum(b.errorCount);
@@ -240,7 +284,6 @@ export default class EntityReport extends React.Component {
             accessor: e => e,
             Header: 'Total',
             Cell: e => <p>{_.sum(e.value.errorCount)}</p>,
-            className: 'right',
             sortMethod: (a, b /* , desc */) => {
                 const aValue = _.sum(a.errorCount);
                 const bValue = _.sum(b.errorCount);
@@ -260,65 +303,64 @@ export default class EntityReport extends React.Component {
         return columns;
     }
 
-    render() {
+    renderPredictionsTable = () => {
         const { expanded } = this.state;
         const data = this.getEntityData();
         const totalErrors = [0, 1, 2, 3].map(i => _.sum(data.map(datum => datum.errorCount[i])));
 
-        if (_.sum(totalErrors) > 0) {
-            return (
-                <div>
-                    <br />
-                    <Metrics
-                        data={_.zip(this.errorTypes, this.errorInfo, totalErrors).map(([name, info, count]) => ({
-                            label: name,
-                            value: count / _.sum(totalErrors),
-                            help: info,
-                        }))}
-                    />
-                    <br />
-                    <br />
-                    <ReactTable
-                        data={data}
-                        columns={this.getEntitiesColumns()}
-                        expanded={expanded}
-                        onPageChange={(page) => {
-                            this.setState({ expanded: {} });
-                        }}
-                        onExpandedChange={(newExpanded, index) => {
-                            if (newExpanded[index[0]] === false) {
-                                // eslint-disable-next-line no-param-reassign
-                                newExpanded = {};
-                            } else {
-                                Object.keys(newExpanded).forEach((k) => {
-                                    // eslint-disable-next-line no-param-reassign
-                                    newExpanded[k] = parseInt(k, 10) === index[0] ? {} : false;
-                                });
-                            }
-                            this.setState({ expanded: newExpanded });
-                        }}
-                        SubComponent={(row) => {
-                            const failureData = row.original.failed_examples;
-                            const length = failureData && failureData.length;
-                            const pageSize = 5;
-                            return length ? (
-                                <ReactTable data={failureData} columns={this.getFailedExamplesColumns()} showPagination={length > pageSize} defaultPageSize={length > pageSize ? pageSize : length} />
-                            ) : null;
-                        }}
-                    />
-                </div>
-            );
-        }
         return (
-            <Message negative>
-                <Message.Header>Nothing to show</Message.Header>
-                <p>There are no entities in the evaluation data set that can be shown.</p>
-            </Message>
+            <ReactTable
+                data={data}
+                columns={this.getEntitiesColumns()}
+                expanded={expanded}
+                onPageChange={() => this.setState({ expanded: {} })}
+                onExpandedChange={(newExpanded, index) => {
+                    if (newExpanded[index[0]] === false) {
+                        // eslint-disable-next-line no-param-reassign
+                        newExpanded = {};
+                    } else {
+                        Object.keys(newExpanded).forEach((k) => {
+                            // eslint-disable-next-line no-param-reassign
+                            newExpanded[k] = parseInt(k, 10) === index[0] ? {} : false;
+                        });
+                    }
+                    this.setState({ expanded: newExpanded });
+                }}
+                SubComponent={(row) => {
+                    const failureData = row.original.failed_examples;
+                    const length = failureData && failureData.length;
+                    const pageSize = 5;
+                    return length ? (
+                        <ReactTable
+                            data={failureData}
+                            columns={this.getFailedExamplesColumns()}
+                            showPagination={length > pageSize}
+                            defaultPageSize={length > pageSize ? pageSize : length}
+                        />
+                    ) : null;
+                }}
+            />
+        );
+    }
+
+    render = () => {
+        const { accuracy, precision, f1_score: f1 } = this.props;
+        return (
+            <div>
+                <br />
+                <KeyMetrics accuracy={accuracy} precision={precision} f1={f1} />
+                <br />
+                <br />
+                <Tab menu={{ pointing: true, secondary: true }} panes={this.getPanes()} />
+            </div>
         );
     }
 }
 
-EntityReport.propType = {
+EntityReport.propTypes = {
+    report: PropTypes.string.isRequired,
+    accuracy: PropTypes.number.isRequired,
+    precision: PropTypes.number.isRequired,
+    f1_score: PropTypes.number.isRequired,
     predictions: PropTypes.array.isRequired,
-    hideEntities: PropTypes.func.isRequired,
 };
--- botfront/imports/ui/components/nlu/evaluation/IntentReport.jsx
+++ botfront/imports/ui/components/nlu/evaluation/IntentReport.jsx
@@ -1,192 +1,46 @@
-import ReactTable from "react-table-v6";
-import PropTypes from "prop-types";
-import React from "react";
-import matchSorter from "match-sorter";
-import {Icon, Label, Popup, Tab} from "semantic-ui-react";
-import IntentMetrics from "./KeyMetrics";
-
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Tab } from 'semantic-ui-react';
+import KeyMetrics from './KeyMetrics';
+import ReportTable from './ReportTable';
+import PredictionTable from './PredictionTable';
 
 export default class IntentReport extends React.Component {
-    getReportData() {
-        // // fix first line header by adding the missing "intent" header
-        const report = this.props.report;
-        let tokens = report.replace("avg / total", "avg/total");
-        let reportRows = tokens.split(/\r\n|\r|\n/);
-        reportRows = reportRows.slice(1, tokens.length - 1);
-        const tableRows = [];
-        reportRows.forEach(r => {
-            const tokens = r.trim().split(/\s+/g);
-            if (tokens.length === 5) { // the "None" intent row of the report splits in 4 tokens
-                tableRows.push({
-                    intent: tokens[0],
-                    f1: tokens[3],
-                    precision: tokens[1],
-                    recall: tokens[2],
-                    support: tokens[4],
-                });
-            }
-        });
-
-        return tableRows;
-    }
-
-    getPredictionsData() {
-        return this.props.predictions.filter(
-            (pred) => {
-                return pred.predicted !== pred.intent;
-            }
-        );
+    getPanes() {
+        const { report, predictions } = this.props;
+        const tabs = [{
+            menuItem: 'Detailed Report',
+            render: () => <ReportTable report={report} labelType='intent' />,
+        }];
+        if (predictions && predictions.length) {
+            tabs.push(
+                {
+                    menuItem: 'Misclassifications',
+                    render: () => <PredictionTable predictions={predictions} labelType='intent' />,
+                },
+            );
+        }
+        return tabs;
     }
 
     render() {
-        return <div>
-            <br/>
-            <IntentMetrics
-                accuracy={this.props.accuracy}
-                precision={this.props.precision}
-                f1={this.props.f1_score}
-            />
-            <br/>
-            <br/>
-            <Tab menu={{pointing: true, secondary: true}} panes={this.getIntentPanes()}/>
-        </div>
-    }
-
-    getIntentPanes() {
-        return [
-            {
-                menuItem: 'Intent Misclassification',
-                render: () => <div>
-                    {this.getPredictionsData(true).length > 0 &&
-                    <ReactTable
-                        data={this.getPredictionsData(true)}
-                        columns={this.getPredictionsColumns(true)}/>
-                    }
-                </div>
-            },
-            {
-                menuItem:'Detailed Report',
-                render: () => <ReactTable
-                    data={this.getReportData()}
-                    filterable
-                    columns={this.getReportColums()}
-                    minRows={1}
-                    SubComponent={null}
+        const { accuracy, precision, f1_score: f1 } = this.props;
+        return (
+            <div>
+                <br />
+                <KeyMetrics
+                    accuracy={accuracy}
+                    precision={precision}
+                    f1={f1}
                 />
-            }
-        ]
-    }
-
-
-    getReportColums() {
-        return [
-            {
-                accessor: 'intent',
-                Header: "Intent",
-                filterMethod: (filter, rows) =>
-                    matchSorter(rows, filter.value, {keys: ["intent"]}),
-                className: "left",
-                filterAll: true
-            },
-            {
-                accessor: 'f1',
-                Header: () => (
-                    <div>F1-Score <Popup trigger={<Icon name='question circle' color="grey"/>}
-                                         content="A more general measure of the quality of your model based on precision and accuracy"/>
-                    </div>
-                ),
-                className: "right",
-                filterable: false,
-                width: 100
-            },
-            {
-                accessor: 'precision',
-                Header: () => (
-                    <div>Precision <Popup trigger={<Icon name='question circle' color="grey"/>}
-                                          content="On 100 examples predicted 'greet', how many were actually labeled 'greet'" />
-                    </div>
-                ),
-                className: "right",
-                filterable: false,
-                width: 100
-            },
-            {
-                accessor: 'recall',
-                Header: () => (
-                    <div>Recall <Popup
-                        trigger={<Icon name='question circle' color='grey' />}
-                        content="On 100 examples labeled 'greet', how many were actually predicted 'greet'"
-                    />
-                    </div>
-                ),
-                className: "right",
-                filterable: false,
-                width: 100
-            },
-            {
-                accessor: 'support',
-                Header: () => (
-                    <div>Support <Popup trigger={<Icon name='question circle' color="grey"/>}
-                                        content='The number of examples for that intent'/></div>
-                ),
-                className: "right",
-                filterable: false,
-                width: 100
-            },
-        ];
-    }
-
-    getPredictionsColumns() {
-        return [
-            {
-                accessor: 'text',
-                Header: "Text",
-                Cell: props => <div><Icon name="quote left" size="small"/>{props.value}</div>,
-                className:'left',
-            },
-            {
-                id: "intent",
-                accessor: r => r.intent,
-                Header: "Correct intent",
-                filterMethod: (filter, rows) => {
-                    return matchSorter(rows, filter.value, {keys: ["intent"]})
-                },
-                Cell: props => <div><Label basic>{props.value}</Label></div>,
-                filterAll: true,
-                width: 200
-            },
-            {
-                id: 'predicted',
-                accessor: r => r.predicted,
-                Header: "Predicted intent",
-                filterMethod: (filter, rows) => {
-                    return matchSorter(rows, filter.value, {keys: ["predicted"]})
-                },
-                Cell: props => {
-                    return <div>
-                        {props.original.predicted === props.original.intent &&
-                        <div><Label basic>{props.value}</Label></div>}
-                        {props.original.predicted !== props.original.intent &&
-                        <div><Label basic color='red'>{props.value ? props.value : 'None'}</Label></div>}
-                    </div>
-                },
-                filterAll: true,
-                width: 200
-            },
-            {
-                id: 'equal',
-                accessor: e => e.intent === e.predicted ? 1 : 0,
-                show: false
-            },
-            {
-                id: "confidence",
-                accessor: p => Math.round(p.confidence * 100) / 100,
-                Header: "Conf.",
-                resizable: false,
-                className: "right",
-                width: 60
-            },
-        ];
+                <br />
+                <br />
+                <Tab
+                    menu={{ pointing: true, secondary: true }}
+                    panes={this.getPanes()}
+                />
+            </div>
+        );
     }
 }
 
