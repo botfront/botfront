e711f57d469beb7a5412e1d2fd0c1db3bc216ce8
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-09-22 10:27:47 -0400

    refactor: add test for draft status

--- botfront/cypress/integration/nlu_tagging/nlu_tagging.spec.js
+++ botfront/cypress/integration/nlu_tagging/nlu_tagging.spec.js
@@ -14,7 +14,7 @@ describe('nlu tagging in training data', function() {
         cy.logout();
         cy.deleteProject('bf');
     });
-
+    
     it('should be able to change the intent with a popup', function() {
         cy.visit('/project/bf/nlu/models');
         cy.get('.row:contains(chitchat.presentation)')
@@ -45,5 +45,21 @@ describe('nlu tagging in training data', function() {
             .find('input')
             .type('person{enter}');
         cy.get('.row:contains(person)');
+    });
+
+    it('should remove the draft status on the example', function() {
+        cy.visit('/project/bf/nlu/models');
+        cy.addExamples(['testa', 'testb']);
+        cy.get('.row').eq(0).click().should('have.class', 'selected');
+        cy.get('body').type('{shift}', { release: false });
+        cy.get('.row').eq(1).click();
+        cy.get('.row.selected').should('have.length', 2);
+        cy.get('body').type('{shift}');
+        cy.changeIntentOfSelectedUtterances('test_intent');
+        cy.get('.virtual-table').focus();
+        cy.dataCy('draft-button').should('have.length', 2);
+        cy.get('body').type('s');
+        cy.get('@texts').then((texts) => { if (texts.length > 1) cy.yesToConfirmation(); });
+        cy.dataCy('draft-button').should('not.exist');
     });
 });
