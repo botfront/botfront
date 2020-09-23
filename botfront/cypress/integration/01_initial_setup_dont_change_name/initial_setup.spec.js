6eecec686009e8cc0345a2c92f87055f101b4311
kenrilea <kenton.rilea@mrbot.ai>
2020-09-23 14:32:50 -0400

    refactor: fix initial test

--- botfront/cypress/integration/01_initial_setup_dont_change_name/initial_setup.spec.js
+++ botfront/cypress/integration/01_initial_setup_dont_change_name/initial_setup.spec.js
@@ -18,7 +18,7 @@ describe('intial setup', function () {
 
     it('Should create projects when completing the initial setup', () => {
         cy.visit('/');
-        cy.url().should('equal', 'http://localhost:3000/setup/welcome');
+        cy.url().should('include', '/setup/welcome');
         cy.get('[data-cy=start-setup]').click();
 
         cy.get('#uniforms-0001-0001').type('Testing');
