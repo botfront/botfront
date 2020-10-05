6afa766c9ec8d7f701e05f98efd21b11f669a2b5
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-10-05 09:14:02 -0400

    fix: endpoints saving when the document does not exist

--- botfront/imports/api/endpoints/endpoints.methods.js
+++ botfront/imports/api/endpoints/endpoints.methods.js
@@ -18,7 +18,7 @@ export const saveEndpoints = async (endpoints) => {
         if (!Meteor.isServer) throw Meteor.Error(400, 'Not Authorized');
         return Endpoints.upsert(
             { projectId: endpoints.projectId },
-            { $set: { endpoints: endpoints.endpoints } },
+            { $set: { endpoints: endpoints.endpoints, projectId: endpoints.projectId, environment: endpoints.environment || 'development' } },
         );
     } catch (e) {
         throw formatError(e);
