49dde2aeec2ee1d1085b46c361669ab34827e170
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-10-07 11:47:35 -0400

    refactor: remove differentiaion betweem rasa and botfront export

--- botfront/imports/ui/components/settings/ExportProject.jsx
+++ botfront/imports/ui/components/settings/ExportProject.jsx
@@ -3,92 +3,23 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { saveAs } from 'file-saver';
 import {
-    Dropdown, Button, Message, Icon, Checkbox,
+    Dropdown, Button, Message, Icon,
 } from 'semantic-ui-react';
 import JSZIP from 'jszip';
 import { ProjectContext } from '../../layouts/context';
 
 const ExportProject = ({
-    setLoading, apiHost,
+    setLoading,
 }) => {
     const { projectLanguages, language, project: { _id: projectId } } = useContext(ProjectContext);
 
-    const getExportTypeOptions = () => [
-        {
-            key: 'botfront',
-            text: 'Export for Botfront',
-            value: 'botfront',
-            successText: 'Your project has been successfully exported for Botfront!',
-        },
-        {
-            key: 'rasa',
-            text: 'Export for Rasa/Rasa X',
-            value: 'rasa',
-            successText: 'Your project has been successfully exported for Rasa/Rasa X!',
-        },
-    ];
-    const [exportType, setExportType] = useState(getExportTypeOptions()[1]);
     const [exportLanguage, setExportLanguage] = useState(projectLanguages.length > 1 ? 'all' : language);
     const [ExportSuccessful, setExportSuccessful] = useState(undefined);
     const [errorMessage, setErrorMessage] = useState({
         header: 'Export Failed',
         text: 'There was an unexpected error in the api request.',
     });
-    const toggles = {
-        projectData: [...useState(true), 'Export project data (NLU, stories, responses)'],
-        conversations: [...useState(false), 'Export conversations'],
-        credentials: [...useState(false), 'Export credentials'],
-        endpoints: [...useState(false), 'Export endpoints'],
-        instances: [...useState(false), 'Export instances'],
-    };
 
-    const projectDataCols = [
-        'models',
-        'activity',
-        'evaluations',
-        'storyGroups',
-        'stories',
-        'slots',
-        'corePolicies',
-        'botResponses',
-    ];
-
-    const generateParamString = () => {
-        let params = '';
-        Object.keys(toggles).forEach((t) => {
-            if (t === 'projectData' && !toggles.projectData[0]) {
-                params += projectDataCols.map(c => `&${c}=0`).join('');
-                params += '&thinProject=1';
-            } else if (!toggles[t][0]) params += `&${t}=0`;
-        });
-        return params;
-    };
-
-    const paramsFromString = paramString => paramString.split('&').reduce((acc, curr) => {
-        const [param, value] = curr.split('=');
-        return {
-            ...acc,
-            ...(param && value ? { [param]: value } : {}),
-        };
-    }, {});
-
-    const getSuccessMessageContent = () => {
-        if (exportType.value === 'botfront') {
-            return (
-                <p>
-                    If your download does not start within 5 seconds click{' '}
-                    <a
-                        href={`${apiHost}/project/${projectId}/export?output=json${generateParamString()}`}
-                        data-cy='export-link'
-                    >
-                        here{' '}
-                    </a>
-                    to retry.
-                </p>
-            );
-        }
-        return <></>;
-    };
 
     const getLanguageOptions = () => [
         ...(projectLanguages.length > 1
@@ -101,39 +32,6 @@ const ExportProject = ({
         value,
     }));
 
-    const exportForBotfront = () => {
-        setLoading(true);
-        const exportOptions = paramsFromString(generateParamString());
-        Meteor.call(
-            'exportProject',
-            apiHost,
-            projectId,
-            exportOptions,
-            (err, { data, error }) => {
-                if (data) {
-                    const blob = new Blob([data], { type: 'text/plain;charset=utf-8' });
-                    const filename = `BotfrontProject_${projectId}.json`;
-                    if (window.Cypress) {
-                        setExportSuccessful(true);
-                        setLoading(false);
-                        return;
-                    }
-                    saveAs(blob, filename);
-                    setExportSuccessful(true);
-                    setLoading(false);
-                    return;
-                }
-                if (error) {
-                    setErrorMessage(error);
-                    setExportSuccessful(false);
-                    setLoading(false);
-                    return;
-                }
-                setLoading(false);
-                setExportSuccessful(false);
-            },
-        );
-    };
 
     const exportForRasa = () => {
         setLoading(true);
@@ -156,28 +54,7 @@ const ExportProject = ({
             }
         });
     };
-
-    const exportProject = () => {
-        if (exportType.value === 'botfront') exportForBotfront();
-        if (exportType.value === 'rasa') exportForRasa();
-    };
-
-    const handleDropdownOnChange = (x, { value }) => {
-        setExportType(
-            getExportTypeOptions().find(option => option.value === value) || {},
-        );
-    };
-
-    const renderToggle = t => (
-        <Checkbox
-            toggle
-            checked={toggles[t][0]}
-            onChange={() => toggles[t][1](!toggles[t][0])}
-            label={toggles[t][2]}
-            className='export-option'
-            key={t}
-        />
-    );
+    
 
     if (ExportSuccessful === true) {
         return (
@@ -185,8 +62,7 @@ const ExportProject = ({
                 data-cy='export-success-message'
                 positive
                 icon='check circle'
-                header={exportType.successText}
-                content={getSuccessMessageContent()}
+                header='Your project has been successfully exported'
             />
         );
     }
@@ -203,100 +79,69 @@ const ExportProject = ({
     }
     return (
         <>
-            <Dropdown
-                data-cy='export-type-dropdown'
-                key='format'
-                className='export-option'
-                options={getExportTypeOptions().map(({ value, text, key }) => ({
-                    value,
-                    text,
-                    key,
-                }))}
-                placeholder='Select a format'
-                selection
-                value={exportType.value}
-                onChange={handleDropdownOnChange}
-            />
-            <br />
-            {exportType.value === 'botfront'
-                && Object.keys(toggles).map(t => (
-                    <>
-                        {renderToggle(t)}
-                        <br />
-                    </>
-                ))}
-            {exportType.value === 'rasa' && (
-                <>
-                    <Message
-                        info
-                        header='A few things to keep in mind when exporting for Rasa'
-                        content={(
-                            <>
-                                <h5>NLU pipeline</h5>
-                                <p>
+            
+            
+            <>
+                <Message
+                    info
+                    header='A few things to keep in mind when exporting for Rasa'
+                    content={(
+                        <>
+                            <h5>NLU pipeline</h5>
+                            <p>
                                     Consider removing Botfront specific NLU components,
                                     such as{' '}
-                                    <b className='monospace'>
+                                <b className='monospace'>
                                         rasa_addons.nlu.components.gazette.Gazette
-                                    </b>
+                                </b>
                                     .
-                                </p>
-                                <h5>Credentials and endpoints</h5>
-                                <p>
+                            </p>
+                            <h5>Credentials and endpoints</h5>
+                            <p>
                                     In most cases, you do not need to change credentials
                                     or endpoints. If you need to keep credentials from
                                     Botfront, be sure to keep the{' '}
-                                    <b className='monospace'>rasa</b> and{' '}
-                                    <b className='monospace'>rest</b> fields from the{' '}
-                                    <b className='monospace'>credentials.yml</b> provided
+                                <b className='monospace'>rasa</b> and{' '}
+                                <b className='monospace'>rest</b> fields from the{' '}
+                                <b className='monospace'>credentials.yml</b> provided
                                     by Rasa X.
-                                </p>
-                            </>
-                        )}
-                    />
-                    <Dropdown
-                        data-cy='export-language-dropdown'
-                        key='language'
-                        className='export-option'
-                        options={getLanguageOptions()}
-                        placeholder='Select a language'
-                        selection
-                        value={exportLanguage}
-                        onChange={(x, { value }) => {
-                            setExportLanguage(value);
-                        }}
-                    />
-                    <br />
-                </>
-            )}
+                            </p>
+                        </>
+                    )}
+                />
+                <Dropdown
+                    data-cy='export-language-dropdown'
+                    key='language'
+                    className='export-option'
+                    options={getLanguageOptions()}
+                    placeholder='Select a language'
+                    selection
+                    value={exportLanguage}
+                    onChange={(x, { value }) => {
+                        setExportLanguage(value);
+                    }}
+                />
+                <br />
+            </>
+            
             <br />
-            {exportType.value === 'rasa' && (
-                <Button
-                    onClick={exportProject}
-                    className='export-option'
-                    data-cy='export-button'
-                >
-                    <Icon name='download' />
+             
+            <Button
+                onClick={exportForRasa}
+                className='export-option'
+                data-cy='export-button'
+            >
+                <Icon name='download' />
                     Export project for Rasa/Rasa X
-                </Button>
-            )}
-            {exportType.value === 'botfront' && (
-                <Button
-                    onClick={exportProject}
-                    className='export-option'
-                    data-cy='export-button'
-                >
-                    <Icon name='download' />
-                    Export project for Botfront
-                </Button>
-            )}
+            </Button>
+            
+           
         </>
     );
 };
 
 ExportProject.propTypes = {
     setLoading: PropTypes.func.isRequired,
-    apiHost: PropTypes.string.isRequired,
 };
 
 export default ExportProject;
