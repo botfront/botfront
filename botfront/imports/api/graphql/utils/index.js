ebe7bb101f5a5f4fbf882c8ac3421f3f9dd1f0a5
Kenton Rilea <49006070+kenrilea@users.noreply.github.com>
2020-10-09 15:56:28 -0400

    refactor: fix long date range intervals

--- botfront/imports/api/graphql/utils/index.js
+++ botfront/imports/api/graphql/utils/index.js
@@ -1,37 +1,33 @@
+export const generateWeekCutoffs = (from, to, nBuckets) => {
+    const duration = to - from;
+    const days = Math.ceil(duration / (3600 * 24));
+    const groupDuration = Math.floor(days / (nBuckets)) * 24 * 3600;
+    const groups = Array.from(Array(nBuckets)).map((e, i) => [
+        from + i * groupDuration,
+        from + (i + 1) * groupDuration - 1,
+    ]);
+    return [...groups, [from + nBuckets * groupDuration, to]];
+};
 
 export const generateCuttoffs = (from, to, nBuckets) => {
     const delta = Math.abs(from - to);
     const bucketSize = (delta / nBuckets).toFixed(7);
+    if (bucketSize > 24 * 3600) return generateWeekCutoffs(from, to, nBuckets);
     return Array.from(Array(nBuckets)).map((_e, i) => [
         from + i * bucketSize, i === nBuckets - 1 ? to : from + (i + 1) * bucketSize,
     ]);
 };
 
-export const generateBuckets = (from, to, pathToTimestamp, nBuckets, conversion = v => v) => (
-    generateCuttoffs(from, to, nBuckets)
-        .map((bounds, i, emptyBuckets) => {
-            if (i === emptyBuckets.length - 1) {
-                return {
-                    case: {
-                        $and: [
-                            { $gte: [pathToTimestamp, conversion(bounds[0])] },
-                            { $lt: [pathToTimestamp, conversion(bounds[1])] },
-                        ],
-                    },
-                    then: bounds[1].toFixed(0).toString(),
-                };
-            }
-            return {
-                case: {
-                    $and: [
-                        { $gte: [pathToTimestamp, conversion(bounds[0])] },
-                        { $lt: [pathToTimestamp, conversion(bounds[1])] },
-                    ],
-                },
-                then: bounds[1].toFixed(0).toString(),
-            };
-        })
-);
+export const generateBuckets = (from, to, pathToTimestamp, nBuckets, conversion = v => v) => generateCuttoffs(from, to, nBuckets)
+    .map(bounds => ({
+        case: {
+            $and: [
+                { $gte: [pathToTimestamp, conversion(bounds[0])] },
+                { $lt: [pathToTimestamp, conversion(bounds[1])] },
+            ],
+        },
+        then: bounds[1].toFixed(0).toString(),
+    }));
 
 export const fillInEmptyBuckets = async (collection, from, to, nBuckets) => {
     // This function inserts buckets with no data in them, so that the full graph can be seen
