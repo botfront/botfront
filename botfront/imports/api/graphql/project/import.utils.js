373089244c3d6e21c76f61fe77a9284954b9277d
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-10-27 17:16:02 -0400

    refactor: change the order of validation so project data is available for response validation ( default domain is needed )

--- botfront/imports/api/graphql/project/import.utils.js
+++ botfront/imports/api/graphql/project/import.utils.js
@@ -3,7 +3,11 @@ import { determineDataType } from '../../../lib/importers/common';
 import {
     loadBotfrontConfig, loadRasaConfig, loadConversations, loadIncoming, loadEndpoints, loadCredentials, doValidation,
 } from '../../../lib/importers/loadMisc.js';
+import {
+    loadDomain,
+} from '../../../lib/importers/loadDomain.js';
 import { handleImportAll } from './fileImporters';
+import Project from './project.model';
 
 function streamToString (stream) {
     const chunks = [];
@@ -22,76 +26,71 @@ export function generateSummary(files) {
     return null;
 }
 // dispatch file to correct loader and check for errors
-export function loadFile(file, rawText, params) {
-    const dataType = determineDataType(file, rawText);
-    // if (dataType === 'domain') {
-    //     return update(params.setFileList, file, loadDomain({ rawText, ...params }));
-    // }
+export function loadFile(file, params) {
+    const { dataType } = file;
+    if (dataType === 'domain') {
+        return loadDomain({ file, params });
+    }
     // if (dataType === 'stories') {
-    //     return addStoryFile({ file, rawText, ...params });
+    //     return addStoryFile({ file, ...params });
     // }
     // if (dataType === 'nlu') {
-    //     return addNluFile({ file, rawText, ...params });
+    //     return addNluFile({ file, ...params });
     // }
     if (dataType === 'conversations') {
-        return loadConversations({ file, rawText, params });
+        return loadConversations({ file, params });
     }
     if (dataType === 'incoming') {
-        return loadIncoming({ file, rawText, params });
+        return loadIncoming({ file, params });
     }
     if (dataType === 'bfconfig') {
-        return loadBotfrontConfig({ file, rawText, params });
+        return loadBotfrontConfig({ file, params });
     }
     if (dataType === 'rasaconfig') {
-        return loadRasaConfig({ file, rawText, params });
+        return loadRasaConfig({ file, params });
     }
     if (dataType === 'endpoints') {
-        return loadEndpoints({ file, rawText, params });
+        return loadEndpoints({ file, params });
     }
     if (dataType === 'credentials') {
-        return loadCredentials({ file, rawText, params });
+        return loadCredentials({ file, params });
     }
     return { file, errors: ['unknown file format'] };
 }
 
-
-export async function readAndValidate(files, params) {
-    // conflictsTracker, keep track of the file that should be unique, or in limited number
-    // keyed on the dataType that correspond to an array of filename,
-    // e.g {'credentials': [credentials.yaml, credentials.production.yaml]}
-    const conflictsTracker = {
-        credentials: [], endpoints: [], rasaconfig: [], botfrontconfig: [],
-    };
-    // the dataTypes that are sensible to conflicts
-    const conflictsSensiblesType = ['credentials', 'endpoints', 'rasaconfig', 'botfrontconfig'];
-    const fileWithMessages = await Promise.all(files.map(async (file) => {
+// extract the raw text from the files and infer types
+// if there is a bfconfig file it process it, because we need the data from that file for the validation later
+// (eg: the default domain, the project languages)
+export async function getRawTextAndType(files, params) {
+    const projectConfig = null;
+    const filesDataAndTypes = await Promise.all(files.map(async (file) => {
         const { filename } = file;
         if (file.filename.match(/\.(yml|json|md)/)) {
             const rawText = await streamToString(file.createReadStream());
             if (/\ufffd/.test(rawText)) { // out of range char test
                 return {
                     file,
+                    filename,
                     errors: ['file is not parseable text'],
                 };
             }
-            const newFileData = {
-                filename,
-                ...loadFile(file, rawText, params),
-            };
-            const { dataType } = newFileData;
-            if (doValidation(params) && conflictsSensiblesType.includes(dataType)) {
-                if (conflictsTracker[dataType] && conflictsTracker[dataType].length === 1) {
-                    if (conflictsTracker[dataType] === 'rasaconfig') {
-                        newFileData.warnings = [...(newFileData.warnings || []), `Policies from this file conflicts with policies from ${conflictsTracker[dataType][0]}, and thus they won't be used in the import`];
-                    } else {
-                        newFileData.warnings = [...(newFileData.warnings || []), `Conflicts with ${conflictsTracker[dataType][0]}, and thus won't be used in the import`];
-                        newFileData.conflicts = true;
-                    }
-                } else {
-                    conflictsTracker[dataType].push(filename);
-                }
+            
+            const dataType = determineDataType(file, rawText);
+            if (dataType === 'bfconfig' && projectConfig === null) { // that way we take the first file if there are multiple project files
+                const bfconfig = loadBotfrontConfig({ file: { file, rawText }, params });
+                return {
+                    filename,
+                    rawText,
+                    dataType,
+                    ...bfconfig,
+                };
             }
-            return newFileData;
+            return {
+                file,
+                filename,
+                rawText,
+                dataType,
+            };
         }
         return {
             filename,
@@ -99,6 +98,61 @@ export async function readAndValidate(files, params) {
             errors: ['file is neither .json or.yaml or .md'],
         };
     }));
+
+    return { filesDataAndTypes, projectConfig };
+}
+
+// validateFil
+function validateFiles(files, params) {
+    // conflictsTracker, keep track of the file that should be unique, or in limited number
+    // keyed on the dataType that correspond to an array of filename,
+    // e.g {'credentials': [credentials.yaml, credentials.production.yaml]}
+    const conflictsTracker = {
+        credentials: [], endpoints: [], rasaconfig: [], botfrontconfig: [], domain: [],
+    };
+    // the dataTypes that are sensible to conflicts
+
+    const conflictsSensiblesType = ['credentials', 'endpoints', 'rasaconfig', 'botfrontconfig', 'domain'];
+
+    const fileWithMessages = files.map((file) => {
+        const newFileData = loadFile(file, params);
+        const { dataType } = newFileData;
+        if (doValidation(params) && conflictsSensiblesType.includes(dataType)) {
+            if (conflictsTracker[dataType] && conflictsTracker[dataType].length === 1) {
+                if (conflictsTracker[dataType] === 'rasaconfig') {
+                    newFileData.warnings = [...(newFileData.warnings || []), `Policies from this file conflicts with policies from ${conflictsTracker[dataType][0]}, and thus they won't be used in the import`];
+                } else if (conflictsTracker[dataType] === 'domain') {
+                    newFileData.warnings = [...(newFileData.warnings || []), 'You have multiple domain files if some data conflicts, the one from the first file with that data will be used'];
+                } else {
+                    newFileData.warnings = [...(newFileData.warnings || []), `Conflicts with ${conflictsTracker[dataType][0]}, and thus won't be used in the import`];
+                    newFileData.conflicts = true;
+                }
+            } else {
+                conflictsTracker[dataType].push(file.filename);
+            }
+        }
+        return newFileData;
+    });
+    return fileWithMessages;
+}
+
+
+export async function readAndValidate(files, params) {
+    let projectLanguages = null;
+    let defaultDomain = null;
+
+    // get raw text and type from every file, also check if there is project data in the import
+    const { filesDataAndTypes, projectConfig } = await getRawTextAndType(files, params);
+    if (projectConfig === null) {
+        const project = await Project.findOne({ _id: params.projectId }).lean();
+        projectLanguages = project.languages;
+        ({ defaultDomain } = project);
+    } else {
+        projectLanguages = projectConfig.languages;
+        ({ defaultDomain } = projectConfig);
+    }
+    const fileWithMessages = validateFiles(filesDataAndTypes, { ...params, defaultDomain, projectLanguages });
+  
     let summary = {};
     if (!params.noValidate) {
         // generateSummary generate the summary of what will be imported
--- botfront/imports/lib/importers/loadMisc.js
+++ botfront/imports/lib/importers/loadMisc.js
@@ -5,75 +5,76 @@ export const doValidation = params => !params.noValidate;
 
 
 export const loadIncoming = ({
-    file, rawText, params,
+    file, params,
 }) => {
-    const incoming = JSON.parse(rawText);
+    const incoming = JSON.parse(file.rawText);
     if (doValidation(params) && (!Array.isArray(incoming) || incoming.length < 1)) {
-        return { file, dataType: 'incoming', warnings: [...(file?.warnings || []), 'There are no incoming in this file'] };
+        return { file, warnings: [...(file?.warnings || []), 'There are no incoming in this file'] };
     }
     return {
-        file, dataType: 'incoming', rawText, incoming,
+        ...file, incoming,
     };
 };
 
 
 export const loadConversations = ({
-    file, rawText, params,
+    file, params,
 }) => {
-    const conversations = JSON.parse(rawText);
+    const conversations = JSON.parse(file.rawText);
     if (doValidation(params) && (!Array.isArray(conversations) || conversations.length < 1)) {
-        return { file, dataType: 'conversations', warnings: [...(file?.warnings || []), 'There are no conversations in this file'] };
+        return { ...file, warnings: [...(file?.warnings || []), 'There are no conversations in this file'] };
     }
     return {
-        file, dataType: 'conversations', rawText, conversations,
+        ...file, conversations,
     };
 };
 
 
 export const loadEndpoints = ({
-    file, rawText,
+    file,
 }) => {
     let endpoints;
     try {
-        endpoints = yaml.safeLoad(rawText);
+        endpoints = yaml.safeLoad(file.rawText);
     } catch (e) {
         return {
-            file, dataType: 'endpoints', rawText, errors: [...(file?.errors || []), 'Not valid yaml'],
+            ...file, errors: [...(file?.errors || []), 'Not valid yaml'],
         };
     }
     return {
-        file, dataType: 'endpoints', rawText, endpoints,
+        ...file, endpoints,
     };
 };
 
 
 export const loadCredentials = ({
-    file, rawText,
+    file,
 }) => {
     let credentials;
     try {
-        credentials = yaml.safeLoad(rawText);
+        credentials = yaml.safeLoad(file.rawText);
     } catch (e) {
         return {
-            file, dataType: 'credentials', rawText, errors: [...(file?.errors || []), 'Not valid yaml'],
+            ...file, errors: [...(file?.errors || []), 'Not valid yaml'],
         };
     }
     return {
-        file, dataType: 'credentials', rawText, credentials,
+        ...file, credentials,
     };
 };
 
 
 export const loadRasaConfig = ({
-    file, rawText, params,
+    file, params,
 }) => {
     const errors = [];
+    const { rawText } = file;
     let rasaConfig;
     try {
         rasaConfig = yaml.safeLoad(rawText);
     } catch (e) {
         return {
-            file, dataType: 'rasaconfig', rawText, errors: [...(file?.errors || []), 'Not valid yaml'],
+            ...file, errors: [...(file?.errors || []), 'Not valid yaml'],
         };
     }
     if (doValidation(params)) {
@@ -89,26 +90,28 @@ export const loadRasaConfig = ({
         }
         if (errors.length > 0) {
             return {
-                file, dataType: 'rasaconfig', rawText, errors: [...(file?.errors || []), ...errors],
+                ...file, errors: [...(file?.errors || []), ...errors],
             };
         }
     }
     return {
-        file, dataType: 'rasaconfig', rawText, ...rasaConfig,
+        ...file, ...rasaConfig,
     };
 };
 
 
 export const loadBotfrontConfig = ({
-    file, rawText, params,
+    file, params,
 }) => {
     const errors = [];
+    const { rawText } = file;
+
     let bfConfig;
     try {
         bfConfig = yaml.safeLoad(rawText);
     } catch (e) {
         return {
-            file, rawText, dataType: 'bfconfig', errors: [...(file?.errors || []), ['Not valid yaml']],
+            ...file, errors: [...(file?.errors || []), ['Not valid yaml']],
         };
     } if (doValidation(params)) {
         const configsKeys = Object.keys(bfConfig);
@@ -123,11 +126,11 @@ export const loadBotfrontConfig = ({
         }
         if (errors.length > 0) {
             return {
-                file, rawText, dataType: 'bfconfig', errors: [...(file?.errors || []), ...errors],
+                ...file, errors: [...(file?.errors || []), ...errors],
             };
         }
     }
     return {
-        file, rawText, dataType: 'bfconfig', ...bfConfig,
+        ...file, ...bfConfig,
     };
 };
