31c07a1cd86abd3460ef824ba54218e81d7fa05a
Matthieu Joannon <mattjoannon@gmail.com>
2020-10-28 18:54:28 -0400

    feat: popup for augmentation factor

--- botfront/imports/ui/components/settings/CorePolicy.jsx
+++ botfront/imports/ui/components/settings/CorePolicy.jsx
@@ -1,6 +1,7 @@
 import { NumField, AutoForm, ErrorsField } from 'uniforms-semantic';
 import { withTracker } from 'meteor/react-meteor-data';
 import 'react-s-alert/dist/s-alert-default.css';
+import { Popup } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { cloneDeep } from 'lodash';
 import PropTypes from 'prop-types';
@@ -60,7 +61,30 @@ class CorePolicy extends React.Component {
                     return newModel;
                 }}
             >
-                <NumField data-cy='augmentation-factor' name='augmentationFactor' label='Augmentation factor' />
+                <Popup
+                    trigger={(
+                        <NumField
+                            data-cy='augmentation-factor'
+                            name='augmentationFactor'
+                            label='Augmentation factor'
+                            disabled={!can('stories:w', projectId)}
+                            defaultF
+                        />
+                    )}
+                    hoverable
+                    content={(
+                        <>
+                            <a
+                                href='https://rasa.com/docs/rasa/policies/#data-augmentation'
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                Click here
+                            </a>
+                            <span> for more information on this setting (if left empty, this will default to 20)</span>
+                        </>
+                    )}
+                />
                 <AceField data-cy='core-policies-yaml' name='policies' mode='yaml' label={null} readOnly={!can('stories:w', projectId)} />
                 <ErrorsField />
                 {showConfirmation && (
