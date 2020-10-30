392df858accbf49f230d08e21bdecfb46abc952e
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-10-30 13:55:33 -0400

    chore: move stuff around

--- botfront/cypress/integration/stories/branches.spec.js
+++ botfront/cypress/integration/stories/branches.spec.js
@@ -1,5 +1,37 @@
 /* global cy:true */
 
+const getBranchContainer = (depth) => {
+    let branch = cy.dataCy('single-story-editor').first();
+    for (let i = 0; i < depth; i += 1) {
+        branch = branch.find('[data-cy=single-story-editor]').first();
+    }
+    return branch;
+};
+const getBranchEditor = depth => getBranchContainer(depth).find('.story-visual-editor').first();
+const addBlock = (depth) => {
+    getBranchEditor(depth).then((editor) => {
+        cy.wrap(editor)
+            .findCy('add-user-line')
+            .click({ force: true });
+        cy.wrap(editor)
+            .findCy('user-line-from-input')
+            .last()
+            .click({ force: true });
+        cy.wrap(editor)
+            .findCy('utterance-input')
+            .find('input')
+            .type('I love typing into boxes.{enter}');
+        cy.dataCy('intent-label').should('have.length', depth + 1);
+        cy.wrap(editor)
+            .findCy('intent-label')
+            .click({ force: true })
+            .type('myTestIntent{enter}');
+    });
+    cy.dataCy('save-new-user-input')
+        .click({ force: true });
+    cy.dataCy('save-new-user-input').should('not.exist');
+};
+
 describe('branches', function() {
     beforeEach(function() {
         cy.createProject('bf', 'My Project', 'fr').then(() => cy.login());
@@ -148,7 +180,7 @@ describe('branches', function() {
         cy.dataCy('single-story-editor').last().contains('- intent: bbb');
     });
 
-    it('should save branch title on blur and Enter, discard on esc', function() {
+    it('should save branch title on blur and Enter', function() {
         cy.dataCy('create-branch').click({ force: true });
 
         // test Enter
@@ -173,5 +205,46 @@ describe('branches', function() {
             .blur();
         cy.dataCy('branch-title-input', null, `[value="${newBranchNameTwo}"]`)
             .should('exist');
+    });
+
+    it('should append the contents of the last branch when the second last branch is deleted', function () {
+        cy.wrap([]).as('intent-labels'); // initial intent-labels
+        // create 2 levels of branches
+        cy.dataCy('create-branch')
+            .click();
+        cy.dataCy('branch-label')
+            .should('exist');
+        cy.dataCy('create-branch')
+            .click();
+        // add a user utterance to each open branch
+        addBlock(0);
+        addBlock(1);
+        addBlock(2);
+        // delete one of the branches at index 2
+        getBranchContainer(1)
+            .findCy('branch-label')
+            .last()
+            .click({ force: true });
+        getBranchContainer(1)
+            .findCy('branch-label')
+            .last()
+            .should('have.class', 'active');
+        getBranchContainer(1)
+            .findCy('branch-label')
+            .last()
+            .trigger('mouseover');
+        cy.dataCy('delete-branch')
+            .last()
+            .click({ force: true });
+        cy.dataCy('confirm-yes')
+            .click({ force: true });
+        cy.dataCy('delete-branch')
+            .last()
+            .click({ force: true });
+        cy.dataCy('confirm-yes')
+            .click({ force: true });
+        // the branches at index 2 should have been removed
+        cy.dataCy('branch-menu')
+            .should('not.exist');
     });
 });
