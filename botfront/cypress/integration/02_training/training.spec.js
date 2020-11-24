fa4c7b00a52ed0f6c1ee573def036359fcc53409
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-11-24 16:14:22 -0500

    chore: uncomment tests

--- botfront/cypress/integration/02_training/training.spec.js
+++ botfront/cypress/integration/02_training/training.spec.js
@@ -19,28 +19,28 @@ describe('Training', function() {
         cy.deleteProject('bf');
     });
     
-    // it('Should train and serve a model containing only stories (no NLU) and adding a language should work', function() {
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     cy.testChatInput('/chitchat.greet', 'utter_hi');
-    //     cy.import('bf', 'nlu_sample_en.json', 'en');
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     cy.testChatInput('hi', 'utter_hi'); // nlg returns template name if not defined
-    // });
+    it('Should train and serve a model containing only stories (no NLU) and adding a language should work', function() {
+        cy.train();
+        cy.newChatSesh();
+        cy.testChatInput('/chitchat.greet', 'utter_hi');
+        cy.import('bf', 'nlu_sample_en.json', 'en');
+        cy.train();
+        cy.newChatSesh();
+        cy.testChatInput('hi', 'utter_hi'); // nlg returns template name if not defined
+    });
 
-    // it('Should train and serve a model containing stories + NLU in one language and adding a second language should work too', function() {
-    //     cy.import('bf', 'nlu_sample_en.json', 'en');
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     cy.testChatInput('hi', 'utter_hi');
-    //     cy.createNLUModelProgramatically('bf', '', 'fr'); // first don't import NLU data
-    //     cy.train();
-    //     cy.import('bf', 'nlu_sample_fr.json', 'fr'); // now import the data
-    //     cy.train();
-    //     cy.newChatSesh('fr');
-    //     cy.testChatInput('salut', 'utter_hi');
-    // });
+    it('Should train and serve a model containing stories + NLU in one language and adding a second language should work too', function() {
+        cy.import('bf', 'nlu_sample_en.json', 'en');
+        cy.train();
+        cy.newChatSesh();
+        cy.testChatInput('hi', 'utter_hi');
+        cy.createNLUModelProgramatically('bf', '', 'fr'); // first don't import NLU data
+        cy.train();
+        cy.import('bf', 'nlu_sample_fr.json', 'fr'); // now import the data
+        cy.train();
+        cy.newChatSesh('fr');
+        cy.testChatInput('salut', 'utter_hi');
+    });
 
     it('Should train and serve a model containing stories and NLU in 2 languages', function() {
         cy.import('bf', 'nlu_sample_en.json', 'en');
@@ -53,58 +53,58 @@ describe('Training', function() {
         cy.testChatInput('salut', 'utter_hi');
     });
 
-    // it('Should only train focused stories', function() {
-    //     cy.get('.eye.icon.focused').should('have.length', 0);
-    //     cy.createStoryGroup();
-    //     cy.moveStoryOrGroup({ name: 'Greetings' }, { name: 'Groupo' });
-    //     cy.checkMenuItemAtIndex(1, 'Greetings');
-    //     cy.createStoryGroup({ groupName: 'Intro stories' });
-    //     cy.moveStoryOrGroup({ name: 'Get started' }, { name: 'Intro stories' });
-    //     cy.checkMenuItemAtIndex(1, 'Get started');
-    //     cy.toggleStoryGroupFocused();
-    //     cy.get('.eye.icon.focused').should('have.length', 1);
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     cy.typeChatMessage('/get_started');
-    //     cy.get('.rw-message').should('have.length', 1); // no response
-    //     cy.testChatInput('/chitchat.greet', 'utter_hi');
-    //     cy.toggleStoryGroupFocused();
-    //     cy.get('.eye.icon.focused').should('have.length', 0);
-    //     cy.toggleStoryGroupFocused('Intro stories');
-    //     cy.wait(500);
-    //     cy.get('.eye.icon.focused').should('have.length', 1);
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     cy.typeChatMessage('/chitchat.greet');
-    //     cy.get('.rw-message').should('have.length', 2); // no response
-    //     cy.testChatInput('/get_started', 'utter_get_started');
-    // });
+    it('Should only train focused stories', function() {
+        cy.get('.eye.icon.focused').should('have.length', 0);
+        cy.createStoryGroup();
+        cy.moveStoryOrGroup({ name: 'Greetings' }, { name: 'Groupo' });
+        cy.checkMenuItemAtIndex(1, 'Greetings');
+        cy.createStoryGroup({ groupName: 'Intro stories' });
+        cy.moveStoryOrGroup({ name: 'Get started' }, { name: 'Intro stories' });
+        cy.checkMenuItemAtIndex(1, 'Get started');
+        cy.toggleStoryGroupFocused();
+        cy.get('.eye.icon.focused').should('have.length', 1);
+        cy.train();
+        cy.newChatSesh();
+        cy.typeChatMessage('/get_started');
+        cy.get('.rw-message').should('have.length', 1); // no response
+        cy.testChatInput('/chitchat.greet', 'utter_hi');
+        cy.toggleStoryGroupFocused();
+        cy.get('.eye.icon.focused').should('have.length', 0);
+        cy.toggleStoryGroupFocused('Intro stories');
+        cy.wait(500);
+        cy.get('.eye.icon.focused').should('have.length', 1);
+        cy.train();
+        cy.newChatSesh();
+        cy.typeChatMessage('/chitchat.greet');
+        cy.get('.rw-message').should('have.length', 2); // no response
+        cy.testChatInput('/get_started', 'utter_get_started');
+    });
     
-    // it('Should train and serve a model containing branches and links', function() {
-    //     cy.import('bf', 'branch_link_project.yml');
-    //     cy.train();
-    //     cy.newChatSesh();
-    //     // coffee path
-    //     cy.testChatInput('/hi', 'utter_coffee');
-    //     cy.testChatInput('/yes', 'utter_sugar');
-    //     cy.testChatInput('/yes', 'utter_ok');
-    //     // tea path
-    //     cy.testChatInput('/hi', 'utter_coffee');
-    //     cy.testChatInput('/no', 'utter_tea');
-    //     cy.testChatInput('/yes', 'utter_sugar');
-    //     cy.testChatInput('/no', 'utter_oknosugar');
-    // });
+    it('Should train and serve a model containing branches and links', function() {
+        cy.import('bf', 'branch_link_project.yml');
+        cy.train();
+        cy.newChatSesh();
+        // coffee path
+        cy.testChatInput('/hi', 'utter_coffee');
+        cy.testChatInput('/yes', 'utter_sugar');
+        cy.testChatInput('/yes', 'utter_ok');
+        // tea path
+        cy.testChatInput('/hi', 'utter_coffee');
+        cy.testChatInput('/no', 'utter_tea');
+        cy.testChatInput('/yes', 'utter_sugar');
+        cy.testChatInput('/no', 'utter_oknosugar');
+    });
 
-    // it('Should access bot via sharing link if and only if sharing is enabled', function() {
-    //     cy.visit('/chat/bf/');
-    //     cy.get('body').contains('Sharing not enabled for project').should('exist');
-    //     cy.train();
-    //     cy.dataCy('share-bot').trigger('mouseover');
-    //     cy.dataCy('toggle-bot-sharing').should('exist').should('not.have.class', 'checked')
-    //         .click()
-    //         .should('have.class', 'checked');
-    //     cy.visit('/chat/bf/');
-    //     cy.get('body').contains('Sharing not enabled for project').should('not.exist');
-    //     cy.get('body').contains('utter_get_started').should('exist');
-    // });
+    it('Should access bot via sharing link if and only if sharing is enabled', function() {
+        cy.visit('/chat/bf/');
+        cy.get('body').contains('Sharing not enabled for project').should('exist');
+        cy.train();
+        cy.dataCy('share-bot').trigger('mouseover');
+        cy.dataCy('toggle-bot-sharing').should('exist').should('not.have.class', 'checked')
+            .click()
+            .should('have.class', 'checked');
+        cy.visit('/chat/bf/');
+        cy.get('body').contains('Sharing not enabled for project').should('not.exist');
+        cy.get('body').contains('utter_get_started').should('exist');
+    });
 });
