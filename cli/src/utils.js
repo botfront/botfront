a20850eaabff9daced786f3ed343575baa269aee
pheel <philippe@mrbot.ai>
2021-02-04 14:34:53 -0500

    feat: hot change of BF_PROJECT_ID using cli (#876)
    * feat: hot change of BF_PROJECT_ID using cli
    using botfront set-project, project_id is changed for rasa and actions containers
    
    * ci: ci entrypoint
    
    * chore: bad bin name
    
    * fix: leave mongourl when updating project id

--- cli/src/utils.js
+++ cli/src/utils.js
@@ -21,10 +21,10 @@ export function fixDir(dir) {
 }
 
 export function consoleError(error) {
-    try{
-        console.log(boxen(error))
+    try {
+        console.log(boxen(error));
     } catch (e) {
-        console.log(error)
+        console.log(error);
     }
 }
 
@@ -32,7 +32,7 @@ export function startSpinner(spinner, message) {
     if (spinner) {
         spinner.start(message);
     } else {
-        console.log(message)
+        console.log(message);
     }
 }
 
@@ -40,7 +40,7 @@ export function setSpinnerText(spinner, message) {
     if (spinner) {
         spinner.text = message;
     } else {
-        console.log(message)
+        console.log(message);
     }
 }
 
@@ -48,7 +48,7 @@ export function setSpinnerInfo(spinner, message) {
     if (spinner) {
         spinner.info = message;
     } else {
-        console.log(message)
+        console.log(message);
     }
 }
 
@@ -56,7 +56,7 @@ export function succeedSpinner(spinner, message) {
     if (spinner) {
         spinner.succeed(message);
     } else {
-        console.log(message)
+        console.log(message);
     }
 }
 
@@ -65,7 +65,7 @@ export function failSpinner(spinner, message, params = {}) {
     if (spinner) {
         spinner.fail(message);
     } else {
-        console.log(message)
+        console.log(message);
     }
     if (exit) process.exit(1);
 }
@@ -84,7 +84,7 @@ export async function getLatestVersion() {
         }
         // If the call fails we just return the current version
         return getBotfrontVersion();
-    } catch(e) {
+    } catch (e) {
         return getBotfrontVersion();
     }
 }
@@ -97,15 +97,18 @@ export function getMongoPassword() {
     return getProjectConfig(fixDir(null)).env.mongo_initdb_root_password;
 }
 
-export function isMinorUpdateWithVersion(projectVersion, botfrontVersion){
+export function isMinorUpdateWithVersion(projectVersion, botfrontVersion) {
     const projectMajorVersion = projectVersion.split('.')[1];
     const projectMinorVersion = projectVersion.split('.')[2];
     const botfrontMajorVersion = botfrontVersion.split('.')[1];
     const botfrontMinorVersion = botfrontVersion.split('.')[2];
-    return projectMajorVersion === botfrontMajorVersion && botfrontMinorVersion !== projectMinorVersion;
+    return (
+        projectMajorVersion === botfrontMajorVersion &&
+        botfrontMinorVersion !== projectMinorVersion
+    );
 }
 
-export function isMajorUpdateWithVersion(projectVersion, botfrontVersion){
+export function isMajorUpdateWithVersion(projectVersion, botfrontVersion) {
     const projectMajorVersion = projectVersion.split('.')[1];
     const botfrontMajorVersion = botfrontVersion.split('.')[1];
     return compareVersions(botfrontMajorVersion, projectMajorVersion) == 1;
@@ -139,7 +142,16 @@ export async function displayNpmUpdateMessage() {
     if (shouldUpdate) {
         const currentVersion = getBotfrontVersion();
         const latestVersion = await getLatestVersion();
-        console.log(boxen(`A new version of Botfront is available: ${chalk.blueBright(currentVersion)} -> ${chalk.green(latestVersion)}\nRun ${chalk.cyan.bold('npm install -g botfront')} to update.`,  { padding: 1,  margin: 1 }))
+        console.log(
+            boxen(
+                `A new version of Botfront is available: ${chalk.blueBright(
+                    currentVersion,
+                )} -> ${chalk.green(latestVersion)}\nRun ${chalk.cyan.bold(
+                    'npm install -g botfront',
+                )} to update.`,
+                { padding: 1, margin: 1 },
+            ),
+        );
     }
     return shouldUpdate;
 }
@@ -152,10 +164,28 @@ export async function displayProjectUpdateMessage() {
     const projectVersion = getProjectVersion();
 
     if (isMinorUpdateWithVersion(projectVersion, botfrontVersion)) {
-        console.log(boxen(`Project was made with Botfront ${chalk.blueBright(projectVersion)} and the currently installed version is ${chalk.green(botfrontVersion)}\nRun ${chalk.cyan.bold('botfront update')} to update your project.`));
+        console.log(
+            boxen(
+                `Project was made with Botfront ${chalk.blueBright(
+                    projectVersion,
+                )} and the currently installed version is ${chalk.green(
+                    botfrontVersion,
+                )}\nRun ${chalk.cyan.bold('botfront update')} to update your project.`,
+            ),
+        );
     }
     if (isMajorUpdateWithVersion(projectVersion, botfrontVersion)) {
-        console.log(boxen(`Project was made with Botfront ${chalk.blueBright(projectVersion)} and the currently installed version is ${chalk.green(botfrontVersion)}, which is a major update.\nPlease follow the instructions in the migration guide: ${chalk.cyan.bold('https://botfront.io/docs/installation/migration-guide/')}.`));
+        console.log(
+            boxen(
+                `Project was made with Botfront ${chalk.blueBright(
+                    projectVersion,
+                )} and the currently installed version is ${chalk.green(
+                    botfrontVersion,
+                )}, which is a major update.\nPlease follow the instructions in the migration guide: ${chalk.cyan.bold(
+                    'https://botfront.io/docs/installation/migration-guide/',
+                )}.`,
+            ),
+        );
         isMajorUpdate = true;
     }
     return isMajorUpdate;
@@ -164,27 +194,40 @@ export async function displayProjectUpdateMessage() {
 /*
 Augment the botfront.yml file with version and project specific values
 */
-export async function updateProjectFile(projectAbsPath, images, enableMongoAuth = true, cloud, mongoPassword = randomString()) {
+export function updateProjectFile({
+    projectAbsPath,
+    images,
+    env = {},
+    enableMongoAuth = true,
+    leaveMongoUrl = false,
+    mongoPassword = randomString(),
+}) {
     const config = getProjectConfig(projectAbsPath);
     if (!config.version) {
         config.version = getBotfrontVersion();
     }
 
-    config.images.current = JSON.parse(JSON.stringify(config.images.default)); // deep copy
-    Object.keys(config.images.current).forEach(service => {
-        if (images[service]) config.images.current[service] = images[service];
-    });
-    if (!cloud){
-        if (enableMongoAuth){
+    if (images) {
+        config.images.current = JSON.parse(JSON.stringify(config.images.default)); // deep copy
+        Object.keys(config.images.current).forEach((service) => {
+            if (images[service]) config.images.current[service] = images[service];
+        });
+    }
+    config.env = {
+        ...config.env || {},
+        ...env,
+    };
+    if (!leaveMongoUrl) {
+        if (enableMongoAuth) {
             Object.assign(config.env, {
                 mongo_url: `mongodb://root:${mongoPassword}@mongo:27017/bf?authSource=admin`,
                 mongo_initdb_root_username: 'root',
                 mongo_initdb_root_password: mongoPassword,
-            })
+            });
         } else {
             Object.assign(config.env, {
                 mongo_url: 'mongodb://mongo:27017/bf',
-            })
+            });
         }
     }
 
@@ -193,73 +236,110 @@ export async function updateProjectFile(projectAbsPath, images, enableMongoAuth 
 
 export async function updateEnvFile(projectAbsPath) {
     const config = getProjectConfig(projectAbsPath);
-    let envFileContent = '########################################################################\n';
-    envFileContent +=    '# This file is generated when `botfront up` is invoked.                #\n';
-    envFileContent +=    '# You can change / add environment variables in .botfront/botfront.yml #\n';
-    envFileContent +=    '########################################################################\n\n';
+    let envFileContent =
+        '########################################################################\n';
+    envFileContent +=
+        '# This file is generated when `botfront up` is invoked.                #\n';
+    envFileContent +=
+        '# You can change / add environment variables in .botfront/botfront.yml #\n';
+    envFileContent +=
+        '########################################################################\n\n';
 
     if (!config.env) {
-        config.env = {}
+        config.env = {};
     }
 
-    Object.keys(config.env).forEach(variable => {
+    Object.keys(config.env).forEach((variable) => {
         envFileContent += `${variable.toUpperCase()}=${config.env[variable]}\n`;
     });
-    Object.keys(config.images.default).forEach(variable => {
-        envFileContent += `IMAGES_DEFAULT_${variable.toUpperCase().replace('-','_')}=${config.images.default[variable]}\n`;
+    Object.keys(config.images.default).forEach((variable) => {
+        envFileContent += `IMAGES_DEFAULT_${variable.toUpperCase().replace('-', '_')}=${
+            config.images.default[variable]
+        }\n`;
     });
-    Object.keys(config.images.current).forEach(variable => {
-        envFileContent += `IMAGES_CURRENT_${variable.toUpperCase().replace('-','_')}=${config.images.current[variable]}\n`;
+    Object.keys(config.images.current).forEach((variable) => {
+        envFileContent += `IMAGES_CURRENT_${variable.toUpperCase().replace('-', '_')}=${
+            config.images.current[variable]
+        }\n`;
     });
 
     fs.writeFileSync(getProjectEnvFilePath(projectAbsPath), envFileContent);
 }
 
-export async function generateDockerCompose(exclude = [], dir) {
-    let initContent = '######################################################################################################\n';
-    initContent +=    '# This file is generated when `botfront up` is invoked.                                              #\n';
-    initContent +=    '# Changes in .botfront/botfront.yml and .botfront/docker-compose-template.yml will be reflected here #\n';
-    initContent +=    '######################################################################################################\n\n';
+export const getComposeTemplateFile = dir => getComposeFile(dir, DOCKER_COMPOSE_TEMPLATE_FILENAME);
 
-    const dc = getComposeFile(dir, DOCKER_COMPOSE_TEMPLATE_FILENAME);
-    exclude.forEach(excl => { // remove reference to excluded services
-        if (excl in dc.services) delete dc.services[excl]
-        Object.values(dc.services).forEach(service => {
+export function generateDockerCompose(exclude = [], dir, projectId) {
+    let initContent =
+        '######################################################################################################\n';
+    initContent +=
+        '# This file is generated when `botfront up` is invoked.                                              #\n';
+    initContent +=
+        '# Changes in .botfront/botfront.yml and .botfront/docker-compose-template.yml will be reflected here #\n';
+    initContent +=
+        '######################################################################################################\n\n';
+
+    const dc = getComposeTemplateFile(dir);
+    exclude.forEach((excl) => {
+        // remove reference to excluded services
+        if (excl in dc.services) delete dc.services[excl];
+        Object.values(dc.services).forEach((service) => {
             if ({}.hasOwnProperty.call(service, 'depends_on')) {
-                service.depends_on = service.depends_on.filter(depend => depend !== excl);
+                service.depends_on = service.depends_on.filter(
+                    (depend) => depend !== excl,
+                );
                 if (service.depends_on.length === 0) delete service.depends_on;
             }
-        })
-    })
+        });
+    });
     const config = getProjectConfig(dir);
     const dcCopy = JSON.parse(JSON.stringify(dc));
-    Object.keys(dc.services).filter(service => ['actions', 'rasa'].indexOf(service) < 0).forEach(service => {
-        dcCopy.services[service].image = config.images.current[service]
-    })
-    // for (let key in dcCopy.services) {
-    //     console.log(key, dcCopy.services[key].depends_on)
-    // }
-    fs.writeFileSync(getGeneratedComposeFilePath(dir), `${initContent}${yaml.safeDump(dcCopy)}`);
+    Object.keys(dc.services)
+        .filter((service) => ['actions', 'rasa'].indexOf(service) < 0)
+        .forEach((service) => {
+            dcCopy.services[service].image = config.images.current[service];
+        });
+    if (projectId) { // hot update of projectId
+        ['actions', 'rasa'].forEach(service => {
+            if (!(service in dc.services)) return;
+            dcCopy.services[service].environment = [
+                ...(dcCopy.services[service].environment || []),
+                `BF_PROJECT_ID=${projectId}`,
+            ];
+        })
+    }
+    fs.writeFileSync(
+        getGeneratedComposeFilePath(dir),
+        `${initContent}${yaml.safeDump(dcCopy)}`,
+    );
     return true;
 }
 
 export function getProjectConfig(projectAbsPath) {
-    return yaml.safeLoad(fs.readFileSync(getProjectInfoFilePath(projectAbsPath), 'utf-8'));
+    return yaml.safeLoad(
+        fs.readFileSync(getProjectInfoFilePath(projectAbsPath), 'utf-8'),
+    );
 }
 
 export async function verifySystem() {
     const docker = new Docker();
     const result = await docker.command('info');
     // const version = result.object.server_version;
-    if (!result.object) throw `You must install Docker to use Botfront. Please visit ${chalk.green('https://www.docker.com/products/docker-desktop')}`;
-    const results = await promisify(check)({ node: '>= 8.9'});
+    if (!result.object)
+        throw `You must install Docker to use Botfront. Please visit ${chalk.green(
+            'https://www.docker.com/products/docker-desktop',
+        )}`;
+    const results = await promisify(check)({ node: '>= 8.9' });
     if (!results.versions.node.isSatisfied) {
-        throw `You must upgrade your Node.js installation to use Botfront. Please visit ${chalk.green('https://nodejs.org/en/download/')}`
+        throw `You must upgrade your Node.js installation to use Botfront. Please visit ${chalk.green(
+            'https://nodejs.org/en/download/',
+        )}`;
     }
 }
 
 export function getBotfrontVersion() {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, '../../botfront/package.json'))).version;
+    return JSON.parse(
+        fs.readFileSync(path.join(__dirname, '../../botfront/package.json')),
+    ).version;
 }
 
 export function isPrivate() {
@@ -278,15 +358,17 @@ export function getProjectInfoDirPath(dir) {
 }
 
 export function getProjectInfoFilePath(dir) {
-    return path.join(fixDir(dir), BF_CONFIG_DIR, BF_CONFIG_FILE );
+    return path.join(fixDir(dir), BF_CONFIG_DIR, BF_CONFIG_FILE);
 }
 
 export function getProjectEnvFilePath(dir) {
-    return path.join(fixDir(dir), ENV_FILENAME );
+    return path.join(fixDir(dir), ENV_FILENAME);
 }
 
 export function isProjectDir(dir) {
-    return fs.existsSync(getComposeFilePath(fixDir(dir), DOCKER_COMPOSE_TEMPLATE_FILENAME));
+    return fs.existsSync(
+        getComposeFilePath(fixDir(dir), DOCKER_COMPOSE_TEMPLATE_FILENAME),
+    );
 }
 
 export function getComposeFile(dir, fileName) {
@@ -294,21 +376,25 @@ export function getComposeFile(dir, fileName) {
 }
 
 export function getGeneratedComposeFile(dir, fileName) {
-    return yaml.safeLoad(fs.readFileSync(getGeneratedComposeFilePath(dir, fileName), 'utf-8'));
+    return yaml.safeLoad(
+        fs.readFileSync(getGeneratedComposeFilePath(dir, fileName), 'utf-8'),
+    );
 }
 
 export function getServices(dir) {
     const services = getGeneratedComposeFile(dir).services;
     return Object.keys(services)
-        .filter(s => !!services[s].image)
-        .map(s => services[s].image);
+        .filter((s) => !!services[s].image)
+        .map((s) => services[s].image);
 }
 
 export async function getMissingImgs(dir) {
     const docker = new Docker({});
-    let availableImgs = await docker.command('images --format "{{.Repository}}:{{.Tag}}"');
+    let availableImgs = await docker.command(
+        'images --format "{{.Repository}}:{{.Tag}}"',
+    );
     availableImgs = availableImgs.raw.split('\n');
-    return getServices(dir).filter(service => !availableImgs.includes(service));
+    return getServices(dir).filter((service) => !availableImgs.includes(service));
 }
 
 export function getServiceNames(dir) {
@@ -332,13 +418,13 @@ export function getExternalPort(serviceName, dir) {
 export function getContainerAndImageNames(dir, services) {
     let svcs = services || getComposeFile(dir).services;
     return {
-        containers: Object.keys(svcs).map(s => services[s].container_name),
-        images: Object.keys(svcs).map(s => services[s].image),
+        containers: Object.keys(svcs).map((s) => services[s].container_name),
+        images: Object.keys(svcs).map((s) => services[s].image),
     };
 }
 
 export function getServiceUrl(serviceName) {
-    return `http://localhost:${getExternalPort(serviceName)}`
+    return `http://localhost:${getExternalPort(serviceName)}`;
 }
 
 export function getComposeWorkingDir(workingDirectory) {
@@ -349,9 +435,9 @@ export async function wait(millis) {
     return promisify(setTimeout)(millis);
 }
 
-export function capitalize(s){
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1)
+export function capitalize(s) {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
 export async function shellAsync(command, options) {
@@ -362,9 +448,9 @@ export async function waitForService(serviceName) {
     const serviceUrl = getServiceUrl(serviceName);
     return new Promise((resolve, reject) => {
         const interval = setInterval(async () => {
-            setTimeout(function() {
-                reject()
-            }, 90000)
+            setTimeout(function () {
+                reject();
+            }, 90000);
 
             let response;
             try {
@@ -380,11 +466,11 @@ export async function waitForService(serviceName) {
     });
 }
 
-export function randomString(length=12) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
+export function randomString(length = 12) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
     var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
