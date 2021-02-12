ba05bf4a01a1a64e6fdb2c12a37587a77fb26bef
Kenton Rilea <49006070+kenrilea@users.noreply.github.com>
2021-02-12 08:53:26 -0500

    Refactor: set slot to null (#899)
    * refactor: allow bool slots to be null
    
    * fix: block non eligible slots in forms
    
    * test: form fixes
    
    * refactor: allow exisiting unfeaturized slots

--- botfront/imports/ui/components/forms/graph/SlotChoiceModal.jsx
+++ botfront/imports/ui/components/forms/graph/SlotChoiceModal.jsx
@@ -125,7 +125,7 @@ const SlotChoiceModal = (props) => {
                 onSelect={slot => chooseAddQuestion(slot)}
                 chooseSlotWithoutValue
                 slotsToRemove={slotsUsed}
-                excludeSlotsOfType={['unfeaturized']}
+                excludeSlotsOfType={[]}
             />
         </div>
     );
@@ -173,6 +173,7 @@ const SlotChoiceModal = (props) => {
                 'float',
                 'list',
                 'unfeaturized',
+                'any',
             ]}
         />
     );
